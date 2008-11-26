bf36c682bc1d5cee87f699e23e73f3fe9c53e5d6
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-11-26 13:25:20 +0000

    Item4480: update version number on trunk.foswiki.org so we can see what svn rev is actually running
    git-svn-id: http://svn.foswiki.org/trunk@976 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/tools/develop/cron.sh
+++ core/tools/develop/cron.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
-# Regular cleanup of shipped webs.
+cd /usr/home/trunk.foswiki.org
 
-cd $HOME/twikisvn/core
-
-WEBS="Main TWiki Sandbox _default Trash TestCases";
+#WEBS="Main TWiki Sandbox _default Trash TestCases";
 
 # Cleanup.
 # Delete wrongly created files in shipped webs
 # Revert modified files
-for web in $WEBS; do
-    # Note: ignores changes to properties
-    svn status --no-ignore data/$web pub/$web \
-        | egrep '^(I|\?|M|C)' \
-        | sed 's/^\(I\|\?\)...../rm -rf/' \
-        | sed 's/^\(M\|C\)...../svn revert/' \
-        | sh
-done
+#for web in $WEBS; do
+#    # Note: ignores changes to properties
+#    svn status --no-ignore data/$web pub/$web \
+#        | egrep '^(I|\?|M|C)' \
+#        | sed 's/^\(I\|\?\)...../rm -rf/' \
+#        | sed 's/^\(M\|C\)...../svn revert/' \
+#        | sh
+#done
 
+svn revert core/lib/Foswiki.pm
 svn update
+REV=`svnlook youngest /home/svn/nextwiki`
+cd core
 perl pseudo-install.pl -link default
+cd lib
+sed -e "s/\(RELEASE = '\)/\1SVN $REV: /" Foswiki.pm > Foswiki.pm.new
+mv Foswiki.pm.new Foswiki.pm
+cd ../data
+for f in /home/foswiki.org/data/*; do
+    if [ -d $f -a ! -e `basename $f` ]; then
+        ln -s $f     
+    fi
+done
