219f6310ae6ae21e095e9eb185d63536a444a7f6
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-17 12:29:03 +0000

    Item145: rebranding
    git-svn-id: http://svn.foswiki.org/trunk@1437 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- MailerContrib/lib/Foswiki/Contrib/MailerContrib/TopicContext.pm
+++ MailerContrib/lib/Foswiki/Contrib/MailerContrib/TopicContext.pm
@@ -3,28 +3,28 @@
 
 sub Foswiki::Func::pushTopicContext {
     my( $web, $topic ) = @_;
-    my $twiki = $Foswiki::Plugins::SESSION;
-    my( $web, $topic ) = $twiki->normalizeWebTopicName( @_ );
+    my $session = $Foswiki::Plugins::SESSION;
+    my( $web, $topic ) = $session->normalizeWebTopicName( @_ );
     my $old = {
-        web => $twiki->{webName},
-        topic => $twiki->{topicName},
-        mark => $twiki->{prefs}->mark() };
+        web => $session->{webName},
+        topic => $session->{topicName},
+        mark => $session->{prefs}->mark() };
 
-    push( @{$twiki->{_FUNC_PREFS_STACK}}, $old );
-    $twiki->{webName} = $web;
-    $twiki->{topicName} = $topic;
-    $twiki->{prefs}->pushWebPreferences( $web );
-    $twiki->{prefs}->pushPreferences( $web, $topic, 'TOPIC' );
-    $twiki->{prefs}->pushPreferenceValues(
-        'SESSION', $twiki->{loginManager}->getSessionValues() );
+    push( @{$session->{_FUNC_PREFS_STACK}}, $old );
+    $session->{webName} = $web;
+    $session->{topicName} = $topic;
+    $session->{prefs}->pushWebPreferences( $web );
+    $session->{prefs}->pushPreferences( $web, $topic, 'TOPIC' );
+    $session->{prefs}->pushPreferenceValues(
+        'SESSION', $session->{loginManager}->getSessionValues() );
 }
 
 sub Foswiki::Func::popTopicContext {
-    my $twiki = $Foswiki::Plugins::SESSION;
-    my $old = pop( @{$twiki->{_FUNC_PREFS_STACK}} );
-    $twiki->{prefs}->restore( $old->{mark});
-    $twiki->{webName} = $old->{web};
-    $twiki->{topicName} = $old->{topic};
+    my $session = $Foswiki::Plugins::SESSION;
+    my $old = pop( @{$session->{_FUNC_PREFS_STACK}} );
+    $session->{prefs}->restore( $old->{mark});
+    $session->{webName} = $old->{web};
+    $session->{topicName} = $old->{topic};
 }
 
 1;
