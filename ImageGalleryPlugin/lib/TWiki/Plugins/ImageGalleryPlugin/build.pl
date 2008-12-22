a9f45f9a210fb2a523c20ef36672502b054b3f24
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-22 09:02:53 +0000

    Item491: moved UserListByPhotograph into the ImageGalleryPlugin, as it depends on this plugin (and also makes a nice little demo of it)
    git-svn-id: http://svn.foswiki.org/trunk@1509 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- ImageGalleryPlugin/lib/TWiki/Plugins/ImageGalleryPlugin/build.pl
+++ ImageGalleryPlugin/lib/TWiki/Plugins/ImageGalleryPlugin/build.pl
@@ -8,10 +8,10 @@ BEGIN {
   }
 }
 
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'ImageGalleryPlugin' );
+$build = new Foswiki::Contrib::Build( 'ImageGalleryPlugin' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
