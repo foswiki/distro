fbdcd01b14df6f86e3ca366d22612f99aad35c6b
GilmarSantosJr <GilmarSantosJr@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-26 03:20:23 +0000

    Item6042: improved BuildContrib use
    git-svn-id: http://svn.foswiki.org/trunk/FastCGIEngineContrib@1584 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib/build.pl
+++ FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib/build.pl
@@ -1,52 +1,23 @@
 #!/usr/bin/perl -w
-#
-# Example build class. Copy this file to the equivalent place in your
-# plugin or contrib and edit.
-#
-# Read the comments at the top of lib/TWiki/Contrib/Build.pm for
-# details of how the build process works, and what files you
-# have to provide and where.
-#
-# Requires the environment variable TWIKI_LIBS (a colon-separated path
-# list) to be set to point at the build system and any required dependencies.
-# Usage: ./build.pl [-n] [-v] [target]
-# where [target] is the optional build target (build, test,
-# install, release, uninstall), test is the default.`
-# Two command-line options are supported:
-# -n Don't actually do anything, just print commands
-# -v Be verbose
-#
-
-# Standard preamble
-BEGIN {
-    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
-}
-
+BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
 use Foswiki::Contrib::Build;
 
-# Declare our build package
-package BuildBuild;
-use base qw( Foswiki::Contrib::Build );
-
-sub new {
-    my $class = shift;
-    return bless( $class->SUPER::new( "BuildContrib", "Build" ), $class );
-}
-
-# Example: Override the build target
-sub target_build {
-    my $this = shift;
-
-    $this->SUPER::target_build();
-
-    # Do other build stuff here
-}
-
-package main;
-
 # Create the build object
-$build = new BuildBuild();
+$build = new Foswiki::Contrib::Build('FastCGIEngineContrib');
+
+# (Optional) Set the details of the repository for uploads.
+# This can be any web on any accessible Foswiki installation.
+# These defaults will be used when expanding tokens in .txt
+# files, but be warned, they can be overridden at upload time!
+
+# name of web to upload to
+$build->{UPLOADTARGETWEB} = 'Extensions';
+# Full URL of pub directory
+$build->{UPLOADTARGETPUB} = 'http://foswiki.org/pub';
+# Full URL of bin directory
+$build->{UPLOADTARGETSCRIPT} = 'http://foswiki.org/bin';
+# Script extension
+$build->{UPLOADTARGETSUFFIX} = '';
 
 # Build the target on the command line, or the default target
-$build->build( $build->{target} );
-
+$build->build($build->{target});
