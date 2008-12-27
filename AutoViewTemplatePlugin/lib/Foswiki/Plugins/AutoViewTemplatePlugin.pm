c43c2bf85b2c2955f281130d23366279a4aa193d
OliverKrueger <OliverKrueger@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-27 02:27:10 +0000

    Item196: moved to Foswiki namepspace, updated build.pl, made minor doc changes
    git-svn-id: http://svn.foswiki.org/trunk@1610 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- AutoViewTemplatePlugin/lib/Foswiki/Plugins/AutoViewTemplatePlugin.pm
+++ AutoViewTemplatePlugin/lib/Foswiki/Plugins/AutoViewTemplatePlugin.pm
@@ -1,4 +1,4 @@
-# Plugin for a software formerly known as TWiki<tm> 
+# Plugin for Foswiki
 #
 # Copyright (C) 2008 Oliver Krueger <oliver@wiki-one.net>
 # All Rights Reserved.
@@ -9,15 +9,15 @@
 #
 # This piece of software is licensed under the GPLv2.
 
-package TWiki::Plugins::AutoViewTemplatePlugin;
+package Foswiki::Plugins::AutoViewTemplatePlugin;
 
 use strict;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION 
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION
   $debug $mode $override $isEditAction
   $pluginName $NO_PREFS_IN_TOPIC );
 
-$VERSION = '$Rev$';
-$RELEASE = 'ipo';
+$VERSION = '2008-12-27';
+$RELEASE = '2008-12-27';
 $SHORTDESCRIPTION = 'Automatically sets VIEW_TEMPLATE and EDIT_TEMPLATE';
 $NO_PREFS_IN_TOPIC = 1;
 
@@ -28,78 +28,78 @@ sub initPlugin {
     my( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.026 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
         return 0;
     }
 
     # get configuration
-    $debug = $TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Debug}       || 0;
-    $mode = $TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Mode}         || "exist";
-    $override = $TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Override} || 0;    
+    $debug    = $Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Debug}    || 0;
+    $mode     = $Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Mode}     || "exist";
+    $override = $Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Override} || 0;
 
     # is this an edit action?
-    $isEditAction = TWiki::Func::getContext()->{edit};
+    $isEditAction   = Foswiki::Func::getContext()->{edit};
     my $templateVar = $isEditAction?'EDIT_TEMPLATE':'VIEW_TEMPLATE';
 
     # back off if there is a view template already and we are not in override mode
-    my $currentTemplate = TWiki::Func::getPreferencesValue($templateVar);
+    my $currentTemplate = Foswiki::Func::getPreferencesValue($templateVar);
     return 1 if $currentTemplate && !$override;
 
     # check if this is a new topic and - if so - try to derive the templateName from
     # the WebTopicEditTemplate
-    if (!TWiki::Func::topicExists($web, $topic)) {
-      if (TWiki::Func::topicExists($web, 'WebTopicEditTemplate')) {
+    if (!Foswiki::Func::topicExists($web, $topic)) {
+      if (Foswiki::Func::topicExists($web, 'WebTopicEditTemplate')) {
         $topic = 'WebTopicEditTemplate';
       } else {
         return 1;
       }
     }
-	
+
     # get form-name
-    my ( $meta, $text ) = TWiki::Func::readTopic( $web, $topic );
+    my ( $meta, $text ) = Foswiki::Func::readTopic( $web, $topic );
     my $form = $meta->get("FORM");
     my $formName = $form->{"name"} if $form;
-    
+
     # is it a structured topic?
     return 1 unless $formName;
-    TWiki::Func::writeDebug("- ${pluginName}: formfields detected ($formName)") if $debug;
+    Foswiki::Func::writeDebug("- ${pluginName}: formfields detected ($formName)") if $debug;
 
     # get it
     my $templateName = "";
     MODE: {
       if ( $mode eq "section" ) {
-        $templateName = _getTemplateFromSectionInclude( $formName, $topic, $web );		
-        last MODE;	
+        $templateName = _getTemplateFromSectionInclude( $formName, $topic, $web );
+        last MODE;
       }
       if ( $mode eq "exist" ) {
         $templateName = _getTemplateFromTemplateExistence( $formName, $topic, $web );
-        last MODE;	
+        last MODE;
       }
     }
-    
+
     # only set the view template if there is anything to set
     return 1 unless $templateName;
 
     # in edit mode, try to read the template to check if it exists
-    if ($isEditAction && !TWiki::Func::readTemplate($templateName)) {
-      TWiki::Func::writeDebug("- ${pluginName}: edit tempalte not found") if $debug;
+    if ($isEditAction && !Foswiki::Func::readTemplate($templateName)) {
+      Foswiki::Func::writeDebug("- ${pluginName}: edit tempalte not found") if $debug;
       return 1;
     }
-    
+
     # do it
     if ($debug) {
       if ( $currentTemplate ) {
         if ( $override ) {
-          TWiki::Func::writeDebug("- ${pluginName}: $templateVar already set, overriding with: $templateName");
+          Foswiki::Func::writeDebug("- ${pluginName}: $templateVar already set, overriding with: $templateName");
         } else {
-          TWiki::Func::writeDebug("- ${pluginName}: $templateVar not changed/set.");
+          Foswiki::Func::writeDebug("- ${pluginName}: $templateVar not changed/set.");
         }
       } else {
-        TWiki::Func::writeDebug("- ${pluginName}: $templateVar set to: $templateName");
-      }      	
+        Foswiki::Func::writeDebug("- ${pluginName}: $templateVar set to: $templateName");
+      }
     }
-    $TWiki::Plugins::SESSION->{prefs}->pushPreferenceValues( 'SESSION', { $templateVar => $templateName } );
+    $Foswiki::Plugins::SESSION->{prefs}->pushPreferenceValues( 'SESSION', { $templateVar => $templateName } );
 
     # Plugin correctly initialized
     return 1;
@@ -110,17 +110,15 @@ sub _getTemplateFromSectionInclude {
     my $topic    = $_[1];
     my $web      = $_[2];
 
-    TWiki::Func::writeDebug("- ${pluginName}: called _getTemplateFromSectionInclude($formName, $topic, $web)") if $debug;
-	
-    my ($formweb, $formtopic) = TWiki::Func::normalizeWebTopicName($web, $formName);
+    Foswiki::Func::writeDebug("- ${pluginName}: called _getTemplateFromSectionInclude($formName, $topic, $web)") if $debug;
+
+    my ($formweb, $formtopic) = Foswiki::Func::normalizeWebTopicName($web, $formName);
 
     # SMELL: This can be done much faster, if the formdefinition topic is read directly
     my $sectionName = $isEditAction?'edittemplate':'viewtemplate';
     my $templateName = "%INCLUDE{ \"$formweb.$formtopic\" section=\"$sectionName\"}%";
-    $templateName = TWiki::Func::expandCommonVariables( $templateName, $topic, $web );
-      
-    # TODO: sanatize value
-      
+    $templateName = Foswiki::Func::expandCommonVariables( $templateName, $topic, $web );
+
     return $templateName;
 }
 
@@ -130,10 +128,10 @@ sub _getTemplateFromTemplateExistence {
     my $formName = $_[0];
     my $topic    = $_[1];
     my $web      = $_[2];
-	
-    TWiki::Func::writeDebug("- ${pluginName}: called _getTemplateFromTemplateExistence($formName, $topic, $web)") if $debug;
-    my ($templateWeb, $templateTopic) = TWiki::Func::normalizeWebTopicName($web, $formName);
-    
+
+    Foswiki::Func::writeDebug("- ${pluginName}: called _getTemplateFromTemplateExistence($formName, $topic, $web)") if $debug;
+    my ($templateWeb, $templateTopic) = Foswiki::Func::normalizeWebTopicName($web, $formName);
+
     $templateWeb =~ s/\//\./go;
     my $templateName = $templateWeb.'.'.$templateTopic;
     $templateName =~ s/Form$//;
--- AutoViewTemplatePlugin/lib/Foswiki/Plugins/AutoViewTemplatePlugin/Config.spec
+++ AutoViewTemplatePlugin/lib/Foswiki/Plugins/AutoViewTemplatePlugin/Config.spec
@@ -3,12 +3,12 @@
 
 # **BOOLEAN**
 # Turn on/off debugging in debug.txt
-$TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Debug} = 0;
+$Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Debug} = 0;
 
 # **BOOLEAN**
 # Override existing VIEW_TEMPLATE settings in doubt.
-$TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Override} = 0;
+$Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Override} = 0;
 
 # **SELECT exist,section**
 # Where to find the template to be used.
-$TWiki::cfg{Plugins}{AutoViewTemplatePlugin}{Mode} = 'exist';
+$Foswiki::cfg{Plugins}{AutoViewTemplatePlugin}{Mode} = 'exist';
