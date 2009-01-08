da91caea649e4fe2f8573be5ee5d131db8d31b42
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-08 16:52:16 +0000

    Item597: Ok, I won't touch anything anymore I promise.
    git-svn-id: http://svn.foswiki.org/trunk@1881 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -1882,7 +1882,7 @@ sub validatePerlModule {
     # Remove all non alpha-numeric caracters and :
     # Do not use \w as this is localized, and might be tainted
     my $replacements = $module =~ s/[^a-zA-Z:_0-9]//g;
-    $this->writeWarning( 'validatePerlModule removed '
+    writeWarning( 'validatePerlModule removed '
           . $replacements
           . ' characters, leading to '
           . $module )
