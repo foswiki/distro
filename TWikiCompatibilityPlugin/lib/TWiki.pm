30308e8044394fe5967ee2ef8b04138bdf121f3d
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-08 01:47:13 +0000

    Item689: As all this was started to make SearchEnginKinoSearchAddOn work under TWikiCompatibilityPlugin, almost finishing it... Gilmar shall finish the rest, I can't figure out with writeCompletePage doesn't write anything. Probably because the UIs aren't setup properly
    git-svn-id: http://svn.foswiki.org/trunk@1847 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TWikiCompatibilityPlugin/lib/TWiki.pm
+++ TWikiCompatibilityPlugin/lib/TWiki.pm
@@ -3,14 +3,19 @@
 use Foswiki;
 
 sub TWiki::new {
-    shift;
-    my $fatwilly = new Foswiki(@_);
+    my ( $this, $loginName, $query, $initialContext ) = @_;
+    if( UNIVERSAL::isa( $query, 'CGI' ) ) {
+        # Compatibility: User gave a CGI object
+        # This probably means we're inside a script
+        $query = undef;
+    }
+    my $fatwilly = new Foswiki( $loginName, $query, $initialContext );
     require TWiki::Sandbox;
     $fatwilly->{sandbox} = new TWiki::Sandbox();
     return $fatwilly;
 }
 
-%TWiki::regex = %Foswiki::regex;
-#%TWiki::cfg   = %Foswiki::cfg;
+*TWiki::regex = \%Foswiki::regex;
+*TWiki::cfg   = \%Foswiki::cfg;
 
 1;
