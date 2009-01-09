2f33d81f0d3b624f0872ccaafdd8f1fe1880a602
GilmarSantosJr <GilmarSantosJr@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-09 23:55:15 +0000

    Item716: preparing for new release upload
    git-svn-id: http://svn.foswiki.org/trunk@1904 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- CompareRevisionsAddOn/lib/Foswiki/Plugins/CompareRevisionsAddonPlugin.pm
+++ CompareRevisionsAddOn/lib/Foswiki/Plugins/CompareRevisionsAddonPlugin.pm
@@ -47,7 +47,7 @@ $VERSION = '$Rev: 8713$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '08 Dec 2008';
+$RELEASE = '09 Jan 2009';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'CompareRevisionsAddonPlugin';
