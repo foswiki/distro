959381da5f94f88089ade01a1104bc1f09adc2f8
GilmarSantosJr <GilmarSantosJr@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-12 00:19:32 +0000

    Item740: add copyright and license notes
    git-svn-id: http://svn.foswiki.org/trunk/ModPerlEngineContrib@1937 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- ModPerlEngineContrib/lib/Foswiki/Contrib/ModPerlEngineContrib.pm
+++ ModPerlEngineContrib/lib/Foswiki/Contrib/ModPerlEngineContrib.pm
@@ -1,15 +1,21 @@
-# Contrib for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+# mod_perl Runtime Engine of Foswiki - The Free and Open Source Wiki,
+# http://foswiki.org/
+#
+# Copyright (C) 2009 Gilmar Santos Jr, jgasjr@gmail.com and Foswiki
+# contributors. Foswiki contributors are listed in the AUTHORS file in the root
+# of Foswiki distribution.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at
-# http://www.gnu.org/copyleft/gpl.html
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
 
 package Foswiki::Contrib::ModPerlEngineContrib;
 
@@ -19,6 +25,6 @@ use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
 $RELEASE = '0.9.0';
-$SHORTDESCRIPTION = 'Permits Foswiki to be run under mod_perl';
+$SHORTDESCRIPTION = 'Permits Foswiki to be executed under mod_perl';
 
 1;
--- ModPerlEngineContrib/tools/mod_perl_startup.pl
+++ ModPerlEngineContrib/tools/mod_perl_startup.pl
@@ -1,22 +1,41 @@
 #!/usr/bin/perl -wT
 
+# mod_perl Runtime Engine of Foswiki - The Free and Open Source Wiki,
+# http://foswiki.org/
+#
+# Copyright (C) 2009 Gilmar Santos Jr, jgasjr@gmail.com and Foswiki
+# contributors. Foswiki contributors are listed in the AUTHORS file in the root
+# of Foswiki distribution.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+
 use strict;
 use File::Spec;
 
 BEGIN {
-    my ($vol, @path);
+    my ( $vol, @path );
     foreach my $file ( keys %INC ) {
         next unless $file =~ /mod_perl_startup\.pl$/;
         my $dir;
-        ($vol, $dir) = (File::Spec->splitpath($file))[0,1];
+        ( $vol, $dir ) = ( File::Spec->splitpath($file) )[ 0, 1 ];
         @path = File::Spec->splitdir($dir);
         last;
     }
     pop @path while $path[-1] eq '';
     $path[-1] = 'lib';
-    unshift @INC, File::Spec->catpath($vol, File::Spec->catdir(@path));
+    unshift @INC, File::Spec->catpath( $vol, File::Spec->catdir(@path) );
     push @path, qw(CPAN lib);
-    unshift @INC, File::Spec->catpath($vol, File::Spec->catdir(@path));
+    unshift @INC, File::Spec->catpath( $vol, File::Spec->catdir(@path) );
 }
 
 use Foswiki::Engine::Apache ();
