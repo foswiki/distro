9059200ea61f4a425beeef84fa7d1b4b4f4dd245
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-16 23:20:07 +0000

    Item789: v1.0.0 tag
    git-svn-id: http://svn.foswiki.org/tags/FoswikiRelease01x00x00@2015 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ SmiliesPlugin/lib/Foswiki/Plugins/SmiliesPlugin.pm
@@ -0,0 +1,119 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
+# Copyright (C) 2002-2006 Peter Thoeny, peter@thoeny.org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+#
+# This plugin replaces smilies with small smilies bitmaps
+
+package Foswiki::Plugins::SmiliesPlugin;
+
+use strict;
+
+use Foswiki::Func;
+
+use vars qw( $VERSION $RELEASE
+            %smiliesUrls %smiliesEmotions
+            $smiliesPubUrl $allPattern $smiliesFormat );
+
+# This should always be $Rev$ so that Foswiki can determine the checked-in
+# status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev$';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = '04 Jan 2009';
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between InterwikiPlugin and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin preferences
+    $smiliesFormat =
+      Foswiki::Func::getPreferencesValue( 'SMILIESPLUGIN_FORMAT' ) 
+          || '<img src="$url" alt="$tooltip" title="$tooltip" border="0" />';
+
+    $topic =
+      Foswiki::Func::getPreferencesValue( 'SMILIESPLUGIN_TOPIC' ) 
+          || "$installWeb.SmiliesPlugin";
+
+    $web = $installWeb;
+    if( $topic =~ /(.+)\.(.+)/ ) {
+        $web = $1;
+        $topic = $2;
+    }
+
+    $allPattern = "(";
+    foreach( split( /\n/, Foswiki::Func::readTopicText( $web, $topic, undef, 1 ) ) ) {
+        # smilie       url            emotion
+        if( m/^\s*\|\s*<nop>(?:\&nbsp\;)?([^\s|]+)\s*\|\s*%ATTACHURL%\/([^\s]+)\s*\|\s*"([^"|]+)"\s*\|\s*$/o ) {
+            $allPattern .= "\Q$1\E|";
+            $smiliesUrls{$1}     = $2;
+            $smiliesEmotions{$1} = $3;
+        }
+    }
+    $allPattern =~ s/\|$//o;
+    $allPattern .= ")";
+    $smiliesPubUrl =
+      Foswiki::Func::getPubUrlPath() .
+          "/$web/$topic";
+
+    # Initialization OK
+    return 1;
+}
+
+sub commonTagsHandler {
+    # my ( $text, $topic, $web ) = @_;
+    $_[0] =~ s/%SMILIES%/_allSmiliesTable()/geo;
+}
+
+sub preRenderingHandler {
+#    my ( $text, \%removed ) = @_;
+
+    $_[0] =~ s/(\s|^)$allPattern(?=\s|$)/_renderSmily($1,$2)/geo;
+}
+
+sub _renderSmily {
+    my ( $thePre, $theSmily ) = @_;
+
+    return $thePre unless $theSmily;
+
+    my $text = $thePre.$smiliesFormat;
+    $text =~ s/\$emoticon/$theSmily/go;
+    $text =~ s/\$tooltip/$smiliesEmotions{$theSmily}/go;
+    $text =~ s/\$url/$smiliesPubUrl\/$smiliesUrls{$theSmily}/go;
+
+    return $text;
+}
+
+sub _allSmiliesTable {
+    my $text = "| *What to Type* | *Graphic That Will Appear* | *Emotion* |\n";
+
+    foreach my $k ( sort { $smiliesEmotions{$b} cmp $smiliesEmotions{$a} }
+                 keys %smiliesEmotions ) {
+        $text .= "| <nop>$k | $k | ". $smiliesEmotions{$k} ." |\n";
+    }
+    return $text;
+}
+
+1;
---
+++ SmiliesPlugin/lib/Foswiki/Plugins/SmiliesPlugin/build.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+#
+# Build for SmiliesPlugin
+#
+BEGIN {
+  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
+    unshift @INC, $pc;
+  }
+}
+
+use Foswiki::Contrib::Build;
+
+# Create the build object
+$build = new Foswiki::Contrib::Build( 'SmiliesPlugin' );
+
+# Build the target on the command line, or the default target
+$build->build($build->{target});
