f180e9bdafb271e2ac91593bd97d35b3bec93b97
EugenMayer <EugenMayer@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-22 00:56:09 +0000

    Item833: Fixed broken build script.
    git-svn-id: http://svn.foswiki.org/trunk@2093 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin/build.pl
+++ TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin/build.pl
@@ -1,13 +1,14 @@
-
 #!/usr/bin/perl -w
+
 BEGIN {
-    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
+  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
+    unshift @INC, $pc;
+  }
 }
+
 use Foswiki::Contrib::Build;
 
-# Create the build object
-$build = new Foswiki::Contrib::Build('TinyMCEPlugin');
-
-# Build the target on the command line, or the default target
+$build = new Foswiki::Contrib::Build( "TinyMCEPlugin" );
 $build->build($build->{target});
+
 
