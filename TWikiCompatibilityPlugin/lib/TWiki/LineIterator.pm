40c0c319526242089f28f99ecbd45a4ff34b0efb
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-02 16:36:36 +0000

    Item8055: SkillsPlugin needs TWiki::ListIterator and TWiki::LineIterator, others might also
    git-svn-id: http://svn.foswiki.org/trunk@2303 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ TWikiCompatibilityPlugin/lib/TWiki/LineIterator.pm
@@ -0,0 +1,39 @@
+# See bottom of file for license and copyright information
+
+package TWiki::LineIterator;
+
+# Bridge between TWiki::LineIterator and Foswiki::LineIterator
+
+use strict;
+
+use Foswiki::LineIterator;
+
+sub new { return Foswiki::LineIterator::new(@_); }
+sub _DESTROY { return Foswiki::LineIterator::_DESTROY(@_); }
+sub hasNext { return Foswiki::LineIterator::hasNext(@_); }
+sub next { return Foswiki::LineIterator::next(@_); }
+
+1;
+
+__DATA__
+
+Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/, http://Foswiki.org/
+
+# Copyright (C) 2008 Foswiki Contributors. Foswiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
+
+1;;
