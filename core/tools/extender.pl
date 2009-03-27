b5560cb1e131785630c63c75acf4113e660f9cc0
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-03-27 07:00:26 +0000

    Item1372: Plugin installation fails on Windows - extender.pl can't find Archive::Tar Please remember to merge over bug fixes to Release branch There will for sure be both a 1.0.5 and a 1.0.6 before we see a 1.1.0 so th3 correct sequence for bug fixing should be to fix in Release branch first and then merge over to trunk. Otherwise we get all the testing in trunk and a tiny little regression test in Release01x00 branch.
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@3299 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/tools/extender.pl
+++ core/tools/extender.pl
@@ -619,12 +619,12 @@ sub unpackArchive {
 sub unzip {
     my $archive = shift;
 
-    unless ( 'Archive::Zip'->use ) {
+    if ( 'Archive::Zip'->use ) {
         my $zip           = Archive::Zip->new();
-        my $numberOfFiles = $zip->read($archive);
-        unless ( $numberOfFiles > 0 ) {
+        my $err = $zip->read($archive);
+        if ( $err ) {
             print STDERR "Could not openzip file $archive ("
-              . $zip->error() . "\n";
+              . $err . "\n";
             return 0;
         }
 
@@ -661,7 +661,7 @@ sub untar {
 
     my $compressed = ( $archive =~ /z$/i ) ? 'z' : '';
 
-    unless ( 'Archive::Tar'->use ) {
+    if ( 'Archive::Tar'->use ) {
         my $tar = Archive::Tar->new();
         my $numberOfFiles = $tar->read( $archive, $compressed );
         unless ( $numberOfFiles > 0 ) {
@@ -674,8 +674,8 @@ sub untar {
         foreach my $file (@members) {
             my $target = $file;
 
-            my $err = $tar->extract_file( $file, $target );
-            unless ($err) {
+            my $ok = $tar->extract_file( $file, $target );
+            unless ($ok) {
                 print STDERR 'Failed to extract ', $file, ' from tar file ',
                   $tar, ". Archive may be corrupt.\n";
                 return 0;
