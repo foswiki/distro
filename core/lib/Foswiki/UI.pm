beeb2d8fa05b6ac7e6ffd006c404bb7e238d49aa
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-04-19 15:14:43 +0000

    Item1469: You cannot verify registration by clicking as instructed on the link in the email after the register blocks GET Earlier I reverted the complete block of GET with script register. I have now re-applied the block but excepted the method verify. This way all is blocked except the verification of registration via email link. That open entry should be harmless. I added a new string in the messages. Since it is a geek error message normal people should never see it does no harm. We already have similar error messag$ I added the new string to all the locale files. And I translated those I could myself or with help from people available.
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@3562 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/UI.pm
+++ core/lib/Foswiki/UI.pm
@@ -80,6 +80,8 @@ BEGIN {
         package => 'Foswiki::UI::Register',
         function => 'register_cgi',
         context => { register => 1 },
+        # method verify must allow GET; protect in Foswiki::UI::Register
+        #allow => { POST => 1 },
     };
     $Foswiki::cfg{SwitchBoard}{rename} = {
         package => 'Foswiki::UI::Manage',
