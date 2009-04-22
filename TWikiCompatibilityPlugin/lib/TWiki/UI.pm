c469a83756fce20ae582fbcdac8463db11b0c50b
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-04-22 17:18:31 +0000

    Item1494: execute -> _execute
    git-svn-id: http://svn.foswiki.org/trunk@3625 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TWikiCompatibilityPlugin/lib/TWiki/UI.pm
+++ TWikiCompatibilityPlugin/lib/TWiki/UI.pm
@@ -8,7 +8,7 @@ use Foswiki::UI;
 
 sub TRACE_PASSTHRU    { Foswiki::UI::TRACE_PASSTHRU(@_) }
 sub handleRequest     { Foswiki::UI::handleRequest(@_) }
-sub execute           { Foswiki::UI::execute(@_) }
+sub execute           { Foswiki::UI::_execute(@_) }
 sub logon             { Foswiki::UI::logon(@_) }
 sub checkWebExists    { Foswiki::UI::checkWebExists(@_) }
 sub checkTopicExists  { Foswiki::UI::checkTopicExists(@_) }
