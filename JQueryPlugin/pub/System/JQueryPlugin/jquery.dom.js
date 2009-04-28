dda972a6911c0a6573aa36d9c3fcf4976a928d42
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-04-28 19:18:57 +0000

    Item8144:    * upgraded to jquery-1.3.2, jquery-ui-1.7.1    * reworked directory structure    * added Makefile system to compile and compressed js and cs
    git-svn-id: http://svn.foswiki.org/trunk@3748 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- JQueryPlugin/pub/System/JQueryPlugin/jquery.dom.js
+++
@@ -1,5 +0,0 @@
-$.create=function(){if(arguments.length==0){return[];}
-var first_arg=arguments[0];if(first_arg==null){first_arg="";}
-if(first_arg.constructor==String){if(arguments.length>1){var second_arg=arguments[1];if(second_arg.constructor==String){var elt=document.createTextNode(first_arg);var elts=[];elts.push(elt);var siblings=$.create.apply(null,Array.prototype.slice.call(arguments,1));elts=elts.concat(siblings);return elts;}else{var elt=document.createElement(first_arg);var attributes=arguments[1];for(var attr in attributes){$(elt).attr(attr,attributes[attr]);}
-var children=arguments[2];children=$.create.apply(null,children);$(elt).append(children);if(arguments.length>3){var siblings=$.create.apply(null,Array.prototype.slice.call(arguments,3));return[elt].concat(siblings);}
-return elt;}}else{return document.createTextNode(first_arg);}}else{var elts=[];elts.push(first_arg);var siblings=$.create.apply(null,(Array.prototype.slice.call(arguments,1)));elts=elts.concat(siblings);return elts;}};
