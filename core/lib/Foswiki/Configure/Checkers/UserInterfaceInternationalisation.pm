32246e3a10a2edf53c45697b2c52b944bcdd2317
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-06-30 17:32:55 +0000

    Item1772: check for special perl modules required for some languages (apparently)
    git-svn-id: http://svn.foswiki.org/trunk@4326 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ core/lib/Foswiki/Configure/Checkers/UserInterfaceInternationalisation.pm
@@ -0,0 +1,85 @@
+# See bottom of file for license and copyright information
+package Foswiki::Configure::Checkers::UserInterfaceInternationalisation;
+
+use strict;
+
+use Foswiki::Configure::Checker ();
+our @ISA = ('Foswiki::Configure::Checker');
+
+my @required = (
+    {
+        name  => 'Locale::Maketext::Lexicon',
+        usage => 'I18N translations',
+    },
+);
+
+my @perl56 = (
+    {
+        name            => 'Unicode::String',
+        usage           => 'I18N conversions',
+        requiredVersion => 1,
+    },
+    {
+        name            => 'Unicode::MapUTF8',
+        usage           => "I18N conversions",
+        requiredVersion => 1,
+    },
+    {
+        name            => 'Unicode::Map',
+        usage           => "I18N conversions",
+        requiredVersion => 1,
+    },
+    {
+        name            => 'Unicode::Map8',
+        usage           => "I18N conversions",
+        requiredVersion => 1,
+    },
+    {
+        name            => 'Jcode',
+        usage           => "I18N conversions",
+        requiredVersion => 1,
+    },
+);
+
+my @perl58 = (
+    {
+        name            => 'Encode',
+        usage           => "I18N conversions (core module in Perl 5.8)",
+        requiredVersion => 1,
+    },
+);
+
+sub check {
+    my $this = shift;
+
+    my $n = $this->checkPerlModules( \@required );
+
+    if ( $] >= 5.008 ) {
+        $n .= $this->checkPerlModules( \@perl58 );
+    }
+    else {
+        $n .= $this->checkPerlModules( \@perl56 );
+    }
+    return "<div class='foswikiHelp'>$n</div>";
+}
+
+1;
+__DATA__
+#
+# Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2009 Foswiki Contributors. All Rights Reserved.
+# Foswiki Contributors are listed in the AUTHORS file in the root
+# of this distribution. NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
