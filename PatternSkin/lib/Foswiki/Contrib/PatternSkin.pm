e95e77952c024fde142f1ac0139c60d4548b7d02
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-07 20:29:35 +0000

    Item2011: one more unresolved link...
    git-svn-id: http://svn.foswiki.org/trunk@4791 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -3,6 +3,6 @@ use strict;
 
 our $VERSION = '$Rev$';
 our $RELEASE = '4.0.4';
-our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and W3C-compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
+our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
 
 1;
