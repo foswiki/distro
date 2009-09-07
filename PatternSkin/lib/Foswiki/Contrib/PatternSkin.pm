69cda6f796241e21110579734b053c32f8d9d909
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-07 20:08:57 +0000

    Item2011: consolidate changes trunk and release branch; version for Foswiki 1.0.7
    git-svn-id: http://svn.foswiki.org/trunk@4787 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -2,7 +2,7 @@ package Foswiki::Contrib::PatternSkin;
 use strict;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '4.0.3';
+our $RELEASE = '4.0.4';
 our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and W3C-compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
 
 1;
