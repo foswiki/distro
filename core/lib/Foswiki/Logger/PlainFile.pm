e85f6048b637bd29f68a0a7719f5c57dea07d661
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-14 06:51:32 +0000

    Item2061: only the first instance of vbar was escaped
    git-svn-id: http://svn.foswiki.org/trunk@4898 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Logger/PlainFile.pm
+++ core/lib/Foswiki/Logger/PlainFile.pm
@@ -59,7 +59,7 @@ sub log {
     # to the date; Foswiki::Time::ParseTime can handle it, and it looks
     # OK too.
     unshift( @fields, "$time $level" );
-    my $message = '| ' . join( ' | ', map { s/\|/&vbar;/; $_ } @fields ) . ' |';
+    my $message = '| ' . join( ' | ', map { s/\|/&vbar;/g; $_ } @fields ) . ' |';
     my $file;
     if ( open( $file, '>>', $log ) ) {
         print $file "$message\n";
@@ -208,20 +208,20 @@ sub _getLogForLevel {
 
 1;
 __END__
-# Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-#
-# Copyright (C) 2008-2009 Foswiki Contributors. Foswiki Contributors
-# are listed in the AUTHORS file in the root of this distribution.
-# NOTE: Please extend that file, not this notice.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# As per the GPL, removal of this notice is prohibited.
+Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2009 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
