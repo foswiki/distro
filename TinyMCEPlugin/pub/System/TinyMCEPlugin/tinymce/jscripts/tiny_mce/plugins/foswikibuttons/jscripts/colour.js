6b21d8f462a0598b93404f329c9897a8b8187f8d
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-14 21:28:49 +0000

    Item2070: Merge new TMCE/Wysiwyg pair over to 1.0.7 First we delete the current directories from SVN and then I copy over from trunk That is the safest way with so many files
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@4920 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/pub/System/TinyMCEPlugin/tinymce/jscripts/tiny_mce/plugins/foswikibuttons/jscripts/colour.js
+++
@@ -1,5 +0,0 @@
-function init(){tinyMCEPopup.resizeToInnerSize();}
-function setColour(colour){var inst=tinyMCE.getInstanceById(tinyMCE.getWindowArg('editor_id'));var s=inst.selection.getSelectedHTML();if(s.length>0){tinyMCEPopup.execCommand('mceBeginUndoLevel');s='<font class="WYSIWYG_COLOR" color="'+
-colour
-+'">'+s+'</font>';tinyMCE.execCommand('mceInsertContent',false,s);tinyMCE.triggerNodeChange();tinyMCEPopup.execCommand('mceEndUndoLevel');}
-tinyMCEPopup.close();}
