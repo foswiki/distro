5fe49cc1b6ac2c8c8e5363bb11739b56c7939192
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-18 14:50:32 +0000

    Item2100: Update change history for WysiwygPlugin and TinyMCEPlugin for release
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@4978 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
+++ TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
@@ -18,7 +18,7 @@ use strict;
 use Assert;
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '03 Jul 2009';
+our $RELEASE           = '18 Sep 2009';
 our $SHORTDESCRIPTION  = 'Integration of the Tiny MCE WYSIWYG Editor';
 our $NO_PREFS_IN_TOPIC = 1;
 
