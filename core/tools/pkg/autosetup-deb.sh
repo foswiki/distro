e2a4f671bcb4c9d69c55b6603ab0a5ea592bd465
DrakeDiedrich <DrakeDiedrich@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-22 13:45:04 +0000

    Item2139: BuildContrib now needs Error from liberror-perl
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@5092 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/tools/pkg/autosetup-deb.sh
+++ core/tools/pkg/autosetup-deb.sh
@@ -12,6 +12,7 @@ debdir="$2"
 aptitude update
 aptitude install -y subversion libwww-perl zip fakeroot dpkg-dev \
 	debhelper tardy po-debconf dpatch devscripts build-essential \
-	libcss-minifier-xs-perl libjavascript-minifier-xs-perl
+	libcss-minifier-xs-perl libjavascript-minifier-xs-perl \
+	liberror-perl
 apt-get clean
 aptitude dist-upgrade -y
