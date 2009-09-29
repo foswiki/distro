0812cb6703fe3152ba59fe3351044989fdabda1d
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-29 15:34:36 +0000

    Item2182: Dummy space to change the SVN date of the .pm file to today
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@5183 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -4,5 +4,5 @@ use strict;
 our $VERSION = '$Rev$';
 our $RELEASE = '4.0.5';
 our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
-
+ 
 1;
