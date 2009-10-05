2d1f7f54031f068e7e0efa542cef6ad7b96b74c2
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-10-05 14:09:53 +0000

    Item2210: Home page change implies unit test change...
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@5220 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/FuncTests.pm
+++ UnitTestContrib/test/unit/FuncTests.pm
@@ -599,7 +599,7 @@ sub test_getExternalResource {
     my $response = Foswiki::Func::getExternalResource('http://foswiki.org');
     $this->assert_equals( 200, $response->code() );
     $this->assert_matches(
-        qr/community dedicated to democratic governance, free of commercial influence and trademark issues/s,
+        qr/Foswiki is the open, programmable collaboration platform for the Enterprise/s, 
         $response->content() );
     $this->assert( !$response->is_error() );
     $this->assert( !$response->is_redirect() );
