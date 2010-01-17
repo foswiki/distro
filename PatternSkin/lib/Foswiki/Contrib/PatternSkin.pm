dcfbc0fe647cf74393f5f4062dea772665cb385c
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-01-17 14:06:37 +0000

    Item2649: When changing an extension please remember also to uprev it and add to change history Otherwise people upgrading the extensions will not see there is a newer version and why the date has changed
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@6073 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -2,7 +2,7 @@ package Foswiki::Contrib::PatternSkin;
 use strict;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '4.0.6';
+our $RELEASE = '4.0.7';
 our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
  
 1;
