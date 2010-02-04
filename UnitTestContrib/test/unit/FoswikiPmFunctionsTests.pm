042d6ed0ff7548b66d76c5e6738ed3b4332df529
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-04 07:00:58 +0000

    Item8465: svn mv ManageDotPmTests.pm FoswikiPmFunctionsTests.pm (thats a very old unit test which isn't in Manage anymore)
    git-svn-id: http://svn.foswiki.org/trunk@6213 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ UnitTestContrib/test/unit/FoswikiPmFunctionsTests.pm
@@ -0,0 +1,68 @@
+# NOTE: this is a VERY limited subset of subroutines in Foswiki.pm (um, ok, one - moved from ManageDotPmTests..)
+
+use strict;
+use warnings;
+use diagnostics;
+
+package FoswikiPmFunctionsTests;
+
+use base qw(FoswikiFnTestCase);
+use Foswiki;
+use Foswiki::UI::Manage;
+use Foswiki::UI::Save;
+
+my $debug = 0;
+my $fatwilly;
+
+# Set up the test fixture
+sub set_up {
+    my $this = shift;
+
+    $this->SUPER::set_up();
+    my $query = new Unit::Request();
+    $fatwilly = Foswiki->new( undef, $query );
+    $this->{request}  = $query;
+    $this->{response} = new Unit::Response();
+}
+
+sub test_isValidTopicName_WebHome {
+    my $this = shift;
+
+    my $result = Foswiki::isValidTopicName( 'WebHome', 1 );
+    my $expected = 1;
+    print("result=$result.\n")     if $debug;
+    print("expected=$expected.\n") if $debug;
+    $this->assert( $result eq $expected );
+}
+
+sub test_isValidTopicName_WebHome_NOT_nonwikiword {
+    my $this = shift;
+
+    my $result = Foswiki::isValidTopicName( 'WebHome', 0 );
+    my $expected = 1;
+    print("result=$result.\n")     if $debug;
+    print("expected=$expected.\n") if $debug;
+    $this->assert( $result eq $expected );
+}
+
+sub test_isValidTopicName_Aa_nonwikiword {
+    my $this = shift;
+
+    my $result = Foswiki::isValidTopicName( 'Aa', 1 );
+    my $expected = 1;
+    print("result=$result.\n")     if $debug;
+    print("expected=$expected.\n") if $debug;
+    $this->assert( $result eq $expected );
+}
+
+sub test_isValidTopicName_Aa_NOT_nonwikiword {
+    my $this = shift;
+
+    my $result = Foswiki::isValidTopicName( 'Aa', 0 );
+    my $expected = 0;
+    print("result=$result.\n")     if $debug;
+    print("expected=$expected.\n") if $debug;
+    $this->assert( $result eq $expected );
+}
+
+1;
