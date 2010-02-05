1e82e725e5d1056f4a8ac70983a9c6f88af9f488
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-05 01:12:25 +0000

    Item8470: updated to unit test contrib from release branch
    git-svn-id: http://svn.foswiki.org/trunk@6219 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/lib/Foswiki/Contrib/UnitTestContrib.pm
+++ UnitTestContrib/lib/Foswiki/Contrib/UnitTestContrib.pm
@@ -17,7 +17,7 @@ use strict;
 
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
-$VERSION          = '$Rev$';
-$RELEASE          = '';
-$SHORTDESCRIPTION = '';
+$VERSION = '$Rev$';
+$RELEASE = '1.1';
+$SHORTDESCRIPTION = 'Foswiki Unit-Test Framework';
 
