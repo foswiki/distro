a7be1a0b2e688cc571da8293ed46de6d8284b7e0
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-08 10:38:05 +0000

    Item8481: Add query-object jQuery plugin
    git-svn-id: http://svn.foswiki.org/trunk@6248 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/QUERYOBJECT.pm
@@ -0,0 +1,45 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+# 
+# Copyright (C) 2006-2010 Blair Mitchelmore 
+#   (blair DOT mitchelmore AT gmail DOT com) 
+#   Licensed under the WTFPL (http://sam.zoy.org/wtfpl/).
+#
+# Packaged for Foswiki by Paul.W.Harvey@csiro.au - www.taxonomy.org.au
+
+package Foswiki::Plugins::JQueryPlugin::SUPERSUBS;
+use strict;
+use base 'Foswiki::Plugins::JQueryPlugin::Plugin';
+
+=begin TML
+
+---+ package Foswiki::Plugins::JQueryPlugin::QUERYOBJECT
+
+This is the perl stub for the jquery.queryobject plugin.
+
+=cut
+
+=begin TML
+
+---++ ClassMethod new( $class, $session, ... )
+
+Constructor
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $session = shift || $Foswiki::Plugins::SESSION;
+
+  my $this = bless($class->SUPER::new( 
+    $session,
+    name => 'QueryObject',
+    version => '2.1.7',
+    author => 'Blair Mitchelmore',
+    homepage => 'http://plugins.jquery.com/project/query-object',
+    javascript => ['jquery.queryobject.js'],
+  ), $class);
+
+  return $this;
+}
+
+1;
