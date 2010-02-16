708871283feaa11e1dc77d6d83c6333dc8d0fc92
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-16 23:08:26 +0000

    Item8490: loosen up error code to accomodate legacy broken 400 for login page
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@6382 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- BuildContrib/lib/Foswiki/Contrib/Build.pm
+++ BuildContrib/lib/Foswiki/Contrib/Build.pm
@@ -1868,7 +1868,7 @@ sub _login {
     #Send a login request - to get a validation key for strikeone
     my $response = $userAgent->get("$this->{UPLOADTARGETSCRIPT}/login$this->{UPLOADTARGETSUFFIX}");
     # "(Foswiki login)" or "Login - Foswiki"
-    unless ( $response->code == 400 
+    unless ( ($response->code == 200  || $response->code == 400) 
              and $response->header('title') =~ /login/i ) {
         die 'Failed to GET login form '. $response->request->uri.
           ' -- '. $response->status_line. "\n";
