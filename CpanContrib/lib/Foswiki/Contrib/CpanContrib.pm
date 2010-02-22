fd1c78a0a266d4b405a49c0ccdd915d52beb7736
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-22 01:21:02 +0000

    Item8597: cleanup
    git-svn-id: http://svn.foswiki.org/trunk@6467 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- CpanContrib/lib/Foswiki/Contrib/CpanContrib.pm
+++ CpanContrib/lib/Foswiki/Contrib/CpanContrib.pm
@@ -9,7 +9,7 @@ $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '20100221';
 
 
 1;
