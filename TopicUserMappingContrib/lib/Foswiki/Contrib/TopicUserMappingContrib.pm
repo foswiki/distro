aad594337e57240aa3b39b3737d46932655e0224
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-25 21:37:51 +0000

    Item8619: improve documentation
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@6523 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TopicUserMappingContrib/lib/Foswiki/Contrib/TopicUserMappingContrib.pm
+++ TopicUserMappingContrib/lib/Foswiki/Contrib/TopicUserMappingContrib.pm
@@ -16,6 +16,6 @@ package Foswiki::Contrib::TopicUserMappingContrib;
 use strict;
 
 our $VERSION          = '$Rev$';
-our $RELEASE          = '4.2.3';
+our $RELEASE          = '4.2.4';
 our $SHORTDESCRIPTION = 'User management using data stored in topics';
 
