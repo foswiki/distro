f342f2db5b9a740856ef30a58a5fad5df8369a5e
MichaelTempest <MichaelTempest@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-07 17:31:46 +0000

    Item1326: Add some tests that show the failure reporting works.
    git-svn-id: http://svn.foswiki.org/trunk@6674 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/SeleniumConfigTests.pm
+++ UnitTestContrib/test/unit/SeleniumConfigTests.pm
@@ -29,4 +29,19 @@ sub verify_SeleniumRc_config {
     $this->{selenium}->open_ok( Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'view') );
 }
 
+sub verify_SeleniumRc_ok_failure_reporting {
+    my $this = shift;
+	$this->expect_failure();
+	$this->annotate("Testing that failures from Test::WWW::Selenium find their way to the Unit::TestCase infrastructure");
+    $this->{selenium}->open_ok( Foswiki::Func::getScriptUrl( 'Milkshakes', 'BronzeMoth', 'bananaFlavour') );
+}
+
+sub verify_SeleniumRc_like_failure_reporting {
+    my $this = shift;
+    $this->{selenium}->open_ok( Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'view') );
+	$this->expect_failure();
+	$this->annotate("Testing that failures from Test::WWW::Selenium find their way to the Unit::TestCase infrastructure");
+    $this->{selenium}->title_like( qr/There is no way that this would ever find its way into the title of web page. That would be simply insane!/ );
+}
+
 1;
