e6560aca2ba6968c760042e2aad5c87030ce2583
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-12 22:53:55 +0000

    Item8615: fix printing in Firefox 3.6
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@6731 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -2,7 +2,7 @@ package Foswiki::Contrib::PatternSkin;
 use strict;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '4.0.7.1';
+our $RELEASE = '4.0.7.2';
 our $SHORTDESCRIPTION = 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
  
 1;
