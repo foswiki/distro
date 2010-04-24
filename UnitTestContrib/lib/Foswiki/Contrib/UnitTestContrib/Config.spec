a06be4a4203a883f3552c6bacb3b4e80e63a0364
MichaelTempest <MichaelTempest@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-04-24 21:32:22 +0000

    Item1326: Extend the Selenium RC test infrastructure    * Automate login - this requires a username and password in configure, which required moving the browser config to a more-deeply-nested field in %Foswiki::cfg    * Add some methods for checking the presence or visibility of DOM elements    * Add a generic "wait for condition to be true" method Documentation is still to come, but the API is most definitely not yet stable.
    git-svn-id: http://svn.foswiki.org/trunk@7244 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/lib/Foswiki/Contrib/UnitTestContrib/Config.spec
+++ UnitTestContrib/lib/Foswiki/Contrib/UnitTestContrib/Config.spec
@@ -1,6 +1,14 @@
 # ---+ Extensions
 # ---++ UnitTestContrib
 # Foswiki Unit-Test Framework
+# ---+++ Selenium Remote Control
+# For browser-in-the-loop testing
+# **STRING 30**
+# The UnitTestContrib needs a username to access the testcase web and topic from the browser opened by Selenium RC.
+$Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Username} = '';
+# **PASSWORD 30**
+# The password for the Selenium RC user
+$Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Password} = '';
 # **PERL 40x10**
 # List the browsers accessible via Selenium RC.
 # It is keyed by browser identifier - you choose the identifiers as seems sensible. Browser identifiers must be valid perl identifiers.
@@ -18,5 +26,9 @@
 # </ul>
 # <p />
 # <em><strong>You</strong> are responsible for starting the Selenium RC server on the specified host(s).</em>
-$Foswiki::cfg{UnitTestContrib}{SeleniumRc} = {};
+$Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Browsers} = {};
+# **NUMBER**
+# The base timeout in milliseconds, used when waiting for the browser (and by implication, the server) to respond.
+# You may have to increase this if your test setup is slow.
+$Foswiki::cfg{UnitTestContrib}{SeleniumRc}{BaseTimeout} = 5000;
 
--- UnitTestContrib/test/unit/FoswikiSeleniumTestCase.pm
+++ UnitTestContrib/test/unit/FoswikiSeleniumTestCase.pm
@@ -15,6 +15,28 @@ use Foswiki::UI::Register;
 use Error qw( :try );
 use Encode;
 
+my $startWait;
+my $doze;
+BEGIN {
+    eval "use Time::HiRes qw/usleep time/;";
+    if (not $@) {
+        $startWait = sub{ return time(); };
+        # success
+        $doze = sub {
+            usleep(100_000);
+            return (time() - $_[0]) * 1000;
+        };
+    }
+    else {
+        # use failed
+        $startWait = sub{ return time(); };
+        $doze = sub {
+            sleep(1);
+            return (time() - $_[0]) * 1000;
+        };
+    }
+}
+
 my $useSeleniumError;
 my $browsers;
 
@@ -28,8 +50,11 @@ sub new {
 
     $instance_count++;
 
+    $this->{selenium_timeout} = 30_000; # Same as WWW::Selenium's default value
     $this->{useSeleniumError} = $this->_loadSeleniumInterface;
     $this->{seleniumBrowsers} = $this->_loadSeleniumBrowsers;
+
+    $this->timeout($Foswiki::cfg{UnitTestContrib}{SeleniumRc}{BaseTimeout});
 
     return $this;
 }
@@ -95,9 +120,9 @@ sub _loadSeleniumBrowsers {
     $browsers = {};
 
     unless ($this->{useSeleniumError}) {
-        if ($Foswiki::cfg{UnitTestContrib}{SeleniumRc}) {
-            for my $browser (keys %{ $Foswiki::cfg{UnitTestContrib}{SeleniumRc} }) {
-                my %config = %{ $Foswiki::cfg{UnitTestContrib}{SeleniumRc}{$browser} };
+        if ($Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Browsers}) {
+            for my $browser (keys %{ $Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Browsers} }) {
+                my %config = %{ $Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Browsers}{$browser} };
                 $config{host} ||= 'localhost';
                 $config{port} ||= 4444;
                 $config{browser} ||= '*firefox';
@@ -128,11 +153,90 @@ sub _loadSeleniumBrowsers {
 }
 
 sub _shutDownSeleniumBrowsers {
+    #my $pressEnterToContinue = <STDIN>;
     for my $browser (values %$browsers) {
         print STDERR "Shutting down $browser\n" if $debug;
         $browser->stop();
     }
     undef $browsers;
+}
+
+sub login {
+    my $this = shift;
+    $this->{selenium}->open_ok( Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'login') );
+    my $usernameInputFieldLocator = 'css=input[name="username"]';
+    $this->{selenium}->wait_for_element_present( $usernameInputFieldLocator, $this->{selenium_timeout} );
+    $this->{selenium}->type_ok( $usernameInputFieldLocator, $Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Username} );
+
+    my $passwordInputFieldLocator = 'css=input[name="password"]';
+    $this->assertElementIsPresent($passwordInputFieldLocator);
+    $this->{selenium}->type_ok( $passwordInputFieldLocator, $Foswiki::cfg{UnitTestContrib}{SeleniumRc}{Password} );
+
+    my $loginFormLocator = 'css=form[name="loginform"]';
+    $this->assertElementIsPresent($loginFormLocator);
+    $this->{selenium}->click_ok( 'css=input.foswikiSubmit[type="submit"]' );
+    $this->{selenium}->wait_for_page_to_load( $this->{selenium_timeout} );
+
+    my $postLoginLocation = $this->{selenium}->get_location();
+    my $viewUrl = Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'view');
+    $this->assert_matches(qr/\Q$viewUrl?foswiki_redirect_cache=/, $postLoginLocation);
+}
+
+sub timeout {
+    my $this = shift;
+    my $timeout = shift;
+    $this->{selenium_timeout} = $timeout if $timeout;
+    return $this->{selenium_timeout};
+}
+
+sub waitFor {
+    my $this = shift;
+    my $testFn = shift;
+    my $message = shift;
+    my $args = shift;
+    my $timeout = shift;
+    $timeout ||= $this->{selenium_timeout};
+    $args ||= [];
+    my $result;
+    my $elapsed = 0;
+    my $start = $startWait->();
+    while (not $result and $elapsed < $timeout)
+    {
+        $result = $testFn->($this, @$args);
+        $elapsed = $doze->($start) if not $result;
+    }
+    $this->assert($result, $message || "timeout");
+}
+
+sub assertElementIsPresent {
+    my $this = shift;
+    my $locator = shift;
+    my $message = shift;
+    $message ||= "Element $locator is not present";
+    $this->assert( $this->{selenium}->is_element_present($locator), $message );
+
+    return;
+}
+
+sub assertElementIsVisible {
+    my $this = shift;
+    my $locator = shift;
+    my $message = shift;
+    $message ||= "Element $locator is not visible";
+    $this->assert( $this->{selenium}->is_visible($locator), $message );
+
+    return;
+}
+
+sub assertElementIsNotVisible {
+    my $this = shift;
+    my $locator = shift;
+    my $message = shift;
+    $this->assertElementIsPresent($locator);
+    $message ||= "Element $locator is visible";
+    $this->assert( not $this->{selenium}->is_visible($locator), $message );
+
+    return;
 }
 
 1;
