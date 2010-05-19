2a2f17f3f5ac77c1a9fa53e5dfe68f987bb43ae2
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-05-19 07:12:29 +0000

    Item9001: perltidy
    git-svn-id: http://svn.foswiki.org/trunk@7473 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/HTMLValidationTests.pm
+++ UnitTestContrib/test/unit/HTMLValidationTests.pm
@@ -28,45 +28,51 @@ sub new {
 sub set_up {
     my $this = shift;
 
-	#see http://tidy.sourceforge.net/docs/quickref.html for parameters - be warned that some cause HTML::Tidy to crash
-	$this->{tidy} = HTML::Tidy->new({
-								#turn off warnings until we have fixed errors
-								'show-warnings' => 1,
-								#'accessibility-check'	=> 3,
-								'drop-empty-paras'	=> 0
-									});
-	#print STDERR "HTML::Tidy Version: ".$HTML::Tidy::VERSION."\n";
-	#print STDERR "libtidy Version: ".HTML::Tidy::libtidy_version()."\n";
-    
+#see http://tidy.sourceforge.net/docs/quickref.html for parameters - be warned that some cause HTML::Tidy to crash
+    $this->{tidy} = HTML::Tidy->new(
+        {
+
+            #turn off warnings until we have fixed errors
+            'show-warnings' => 1,
+
+            #'accessibility-check'	=> 3,
+            'drop-empty-paras' => 0
+        }
+    );
+
+    #print STDERR "HTML::Tidy Version: ".$HTML::Tidy::VERSION."\n";
+    #print STDERR "libtidy Version: ".HTML::Tidy::libtidy_version()."\n";
+
     $this->SUPER::set_up();
 }
 
 sub fixture_groups {
-	my @scripts;
-	
-	foreach my $script (keys (%{$Foswiki::cfg{SwitchBoard}})) {
+    my @scripts;
+
+    foreach my $script ( keys( %{ $Foswiki::cfg{SwitchBoard} } ) ) {
         push( @scripts, $script );
         next if ( defined(&$script) );
-        
-		#print STDERR "defining sub $script()\n";
-		my $dispatcher = $Foswiki::cfg{SwitchBoard}{$script};
-		if ( ref($dispatcher) eq 'ARRAY' ) {
 
-		    # Old-style array entry in switchboard from a plugin
-		    my @array = @$dispatcher;
-		    $dispatcher = {
-		        package  => $array[0],
-		        function => $array[1],
-		        context  => $array[2],
-		    };
-		}
+        #print STDERR "defining sub $script()\n";
+        my $dispatcher = $Foswiki::cfg{SwitchBoard}{$script};
+        if ( ref($dispatcher) eq 'ARRAY' ) {
 
-		my $package = $dispatcher->{package} || 'Foswiki::UI';
-		my $function = $dispatcher->{function};
-		my $sub = $package .'::'. $function;
-		#print STDERR "call $sub\n";
+            # Old-style array entry in switchboard from a plugin
+            my @array = @$dispatcher;
+            $dispatcher = {
+                package  => $array[0],
+                function => $array[1],
+                context  => $array[2],
+            };
+        }
 
-		    eval <<SUB;
+        my $package  = $dispatcher->{package} || 'Foswiki::UI';
+        my $function = $dispatcher->{function};
+        my $sub      = $package . '::' . $function;
+
+        #print STDERR "call $sub\n";
+
+        eval <<SUB;
 		sub $script {
 			eval "require \$package" if (defined(\$package));
 			\$UI_FN = \$sub;
@@ -77,24 +83,25 @@ SUB
         die $@ if $@;
     }
 
-	my @skins;
-	#TODO: detect installed skins..
-	foreach my $skin (qw/default pattern plain print/) {
+    my @skins;
+
+    #TODO: detect installed skins..
+    foreach my $skin (qw/default pattern plain print/) {
         push( @skins, $skin );
         next if ( defined(&$skin) );
-        
-		#print STDERR "defining sub $skin()\n";
-		eval <<SUB;
+
+        #print STDERR "defining sub $skin()\n";
+        eval <<SUB;
 		sub $skin {
 			\$SKIN_NAME = \$skin;
 		}
 SUB
-	}
+    }
 
-	my @groups;
-	push(@groups, \@scripts);
-	push(@groups, \@skins);
-	return @groups;
+    my @groups;
+    push( @groups, \@scripts );
+    push( @groups, \@skins );
+    return @groups;
 }
 
 sub call_UI_FN {
@@ -103,43 +110,46 @@ sub call_UI_FN {
         {
             webName   => [$web],
             topicName => [$topic],
-#            template  => [$tmpl],
-            #debugenableplugins => 'TestFixturePlugin,SpreadSheetPlugin,InterwikiPlugin',
-            skin               => $SKIN_NAME
+
+   #            template  => [$tmpl],
+   #debugenableplugins => 'TestFixturePlugin,SpreadSheetPlugin,InterwikiPlugin',
+            skin => $SKIN_NAME
         }
     );
     $query->path_info("/$web/$topic");
     $query->method('GET');
 
-	#turn off ASSERTS so we get less plain text erroring - the user should always see html
+#turn off ASSERTS so we get less plain text erroring - the user should always see html
     $ENV{FOSWIKI_ASSERTS} = 0;
     my $fatwilly = new Foswiki( $this->{test_user_login}, $query );
-    my ($status, $header, $text);
+    my ( $status, $header, $text );
     try {
-		($text) = $this->captureWithKey( switchboard =>
-		    sub {
-		        no strict 'refs';
-		        &${UI_FN}($fatwilly);
-		        use strict 'refs';
-		        $Foswiki::engine->finalize( $fatwilly->{response},
-		            $fatwilly->{request} );
-		    }
-		);
-	} catch Foswiki::OopsException with {
-		my $e = shift;
-		$text = $e->stringify();
-	} catch Foswiki::EngineException with {
-		my $e = shift;
-		$text = $e->stringify();
-	};
+        ($text) = $this->captureWithKey(
+            switchboard => sub {
+                no strict 'refs';
+                &${UI_FN}($fatwilly);
+                use strict 'refs';
+                $Foswiki::engine->finalize( $fatwilly->{response},
+                    $fatwilly->{request} );
+            }
+        );
+    }
+    catch Foswiki::OopsException with {
+        my $e = shift;
+        $text = $e->stringify();
+    }
+    catch Foswiki::EngineException with {
+        my $e = shift;
+        $text = $e->stringify();
+    };
     $fatwilly->finish();
 
     # Remove CGI header
     my $CRLF = "\015\012";    # "\r\n" is not portable
     $text =~ s/^(.*?)$CRLF$CRLF//s;
-    $header = $1;      # untaint is OK, it's a test
+    $header = $1;             # untaint is OK, it's a test
 
-    return ($status, $header, $text);
+    return ( $status, $header, $text );
 }
 
 #TODO: work out why some 'Use of uninitialised vars' don't crash the test (see preview)
@@ -149,43 +159,48 @@ sub call_UI_FN {
 sub verify_switchboard_function {
     my $this = shift;
 
-	my $testcase = 'HTMLValidation_'.$SCRIPT_NAME.'_'.$SKIN_NAME;
-    
-    my ($status, $header, $text) = $this->call_UI_FN('Main', 'WebHome');
+    my $testcase = 'HTMLValidation_' . $SCRIPT_NAME . '_' . $SKIN_NAME;
+
+    my ( $status, $header, $text ) = $this->call_UI_FN( 'Main', 'WebHome' );
+
     #$this->assert_equals('200', $status);
-#    $this->assert_equals('', $header);
-#    $this->assert_equals('', $text);
+    #    $this->assert_equals('', $header);
+    #    $this->assert_equals('', $text);
 
-	$this->{tidy}->parse($testcase, $text);
-	#$this->assert_null($this->{tidy}->messages());
-	my $output = join("\n", $this->{tidy}->messages());
+    $this->{tidy}->parse( $testcase, $text );
 
-	#TODO: disable missing DOCTYPE issues - we've been 
-	if ($output =~ /missing <\!DOCTYPE> declaration/) {
+    #$this->assert_null($this->{tidy}->messages());
+    my $output = join( "\n", $this->{tidy}->messages() );
+
+    #TODO: disable missing DOCTYPE issues - we've been
+    if ( $output =~ /missing <\!DOCTYPE> declaration/ ) {
+
         #$this->expect_failure();
-        $this->annotate("MISSING DOCTYPE - we're returning a messy text error\n$output\n");
-	} else {
-        for( $output ) { # Remove OK warnings
-            # Empty title, no easy fix and harmless
-            s/^$testcase \(\d+:\d+\) Warning: trimming empty <(?:h1|span)>\n?$//gm;
+        $this->annotate(
+            "MISSING DOCTYPE - we're returning a messy text error\n$output\n");
+    }
+    else {
+        for ($output) {    # Remove OK warnings
+                           # Empty title, no easy fix and harmless
+s/^$testcase \(\d+:\d+\) Warning: trimming empty <(?:h1|span)>\n?$//gm;
             s/^\s*$//;
         }
         my $outfile = "${testcase}_run.html";
-		if ($output eq '') {
-            unlink $outfile; # Remove stale output file
+        if ( $output eq '' ) {
+            unlink $outfile;    # Remove stale output file
         }
-        else { # save the output html..
-		    open( my $fh, '>', $outfile ) or die "Can't open $outfile: $!";
-		    print $fh $text;
-		    close $fh;
-		}
-		$this->assert_equals('', $output,
-            "Script $SCRIPT_NAME, skin $SKIN_NAME gave errors, output in $outfile:\n$output");
-	}
-	#clean up messages for next run..
-	$this->{tidy}->clear_messages();
+        else {                  # save the output html..
+            open( my $fh, '>', $outfile ) or die "Can't open $outfile: $!";
+            print $fh $text;
+            close $fh;
+        }
+        $this->assert_equals( '', $output,
+"Script $SCRIPT_NAME, skin $SKIN_NAME gave errors, output in $outfile:\n$output"
+        );
+    }
+
+    #clean up messages for next run..
+    $this->{tidy}->clear_messages();
 }
-
-
 
 1;
