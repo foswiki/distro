46aa225c74662c7b6d4c5a6952c38f3420177a95
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-06-05 22:03:34 +0000

    Item9031: Fix one forgotten file
    git-svn-id: http://svn.foswiki.org/trunk@7645 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- InterwikiPlugin/lib/Foswiki/Plugins/InterwikiPlugin.pm
+++ InterwikiPlugin/lib/Foswiki/Plugins/InterwikiPlugin.pm
@@ -1,27 +1,4 @@
 # See bottom of file for license and copyright information
-# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-Additional copyrights apply to some or all of the code in this
-file as follows:
-Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-Copyright (C) 2001-2007 Peter Thoeny, peter@thoeny.com
-Copyright (C) 2008-2009 Foswiki Contributors
-
-#
-# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 2001-2007 Peter Thoeny, peter@thoeny.org
-# Copyright (C) 2009-2010 Foswiki Contributors
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# As per the GPL, removal of this notice is prohibited.
 
 =begin TML
 
