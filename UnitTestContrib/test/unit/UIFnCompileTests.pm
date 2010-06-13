364439a734882597acba20fe853c23adcaa2cd46
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-06-13 15:44:43 +0000

    Item9147: Perltidy
    git-svn-id: http://svn.foswiki.org/trunk@7764 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/UIFnCompileTests.pm
+++ UnitTestContrib/test/unit/UIFnCompileTests.pm
@@ -12,23 +12,23 @@ use Error qw( :try );
 our $UI_FN;
 our $SCRIPT_NAME;
 our %expected_status = (
-        search  => 302,
-        save  => 302,
+    search => 302,
+    save   => 302,
 );
+
 #TODO: this is beause we're calling the UI::function, not UI:Execute - need to re-write it to use the full engine
 our %expect_non_html = (
-        rest  => 1,
-        viewfile => 1,
-        register => 1,       #TODO: missing action make it throw an exception
-        manage => 1,       #TODO: missing action make it throw an exception
-        upload => 1,         #TODO: zero size upload   
-        resetpasswd => 1,
-        statistics => 1,
+    rest        => 1,
+    viewfile    => 1,
+    register    => 1,    #TODO: missing action make it throw an exception
+    manage      => 1,    #TODO: missing action make it throw an exception
+    upload      => 1,    #TODO: zero size upload
+    resetpasswd => 1,
+    statistics  => 1,
 );
 
-
 sub new {
-    my ($class, @args) = @_;
+    my ( $class, @args ) = @_;
     $Foswiki::cfg{EnableHierarchicalWebs} = 1;
     my $self = $class->SUPER::new( "UIFnCompile", @args );
     return $self;
@@ -42,28 +42,30 @@ sub set_up {
 }
 
 sub fixture_groups {
-	my @groups;
-	
-	foreach my $script (keys (%{$Foswiki::cfg{SwitchBoard}})) {
+    my @groups;
+
+    foreach my $script ( keys( %{ $Foswiki::cfg{SwitchBoard} } ) ) {
         push( @groups, $script );
         next if ( defined(&$script) );
-#print STDERR "defining $script\n";
-	my $dispatcher = $Foswiki::cfg{SwitchBoard}{$script};
-    if ( ref($dispatcher) eq 'ARRAY' ) {
 
-        # Old-style array entry in switchboard from a plugin
-        my @array = @$dispatcher;
-        $dispatcher = {
-            package  => $array[0],
-            function => $array[1],
-            context  => $array[2],
-        };
-    }
+        #print STDERR "defining $script\n";
+        my $dispatcher = $Foswiki::cfg{SwitchBoard}{$script};
+        if ( ref($dispatcher) eq 'ARRAY' ) {
 
-    my $package = $dispatcher->{package} || 'Foswiki::UI';
-    my $function = $dispatcher->{function};
-    my $sub = $package .'::'. $function;
-#print STDERR "call $sub\n";
+            # Old-style array entry in switchboard from a plugin
+            my @array = @$dispatcher;
+            $dispatcher = {
+                package  => $array[0],
+                function => $array[1],
+                context  => $array[2],
+            };
+        }
+
+        my $package  = $dispatcher->{package} || 'Foswiki::UI';
+        my $function = $dispatcher->{function};
+        my $sub      = $package . '::' . $function;
+
+        #print STDERR "call $sub\n";
 
         eval <<"SUB";
 sub $script {
@@ -74,8 +76,8 @@ sub $script {
 SUB
         die $@ if $@;
     }
-	
-	return \@groups;
+
+    return \@groups;
 }
 
 sub call_UI_FN {
@@ -84,58 +86,63 @@ sub call_UI_FN {
         {
             webName   => [$web],
             topicName => [$topic],
-#            template  => [$tmpl],
+
+            #            template  => [$tmpl],
         }
     );
     $query->path_info("/$web/$topic");
     $query->method('POST');
     my $fatwilly = Foswiki->new( $this->{test_user_login}, $query );
-    my ($responseText, $result, $stdout, $stderr);
-    $responseText = "Status: 500";      #errr, boom
+    my ( $responseText, $result, $stdout, $stderr );
+    $responseText = "Status: 500";    #errr, boom
     try {
-		($responseText, $result, $stdout, $stderr) = $this->captureWithKey( switchboard =>
-		    sub {
-		        no strict 'refs';
-		        &${UI_FN}($fatwilly);
-		        use strict 'refs';
-		        $Foswiki::engine->finalize( $fatwilly->{response},
-		            $fatwilly->{request} );
-		    }
-		);
-	} catch Foswiki::OopsException with {
-		my $e = shift;
-		$responseText = $e->stringify();
-	} catch Foswiki::EngineException with {
-		my $e = shift;
-		$responseText = $e->stringify();
-	};
+        ( $responseText, $result, $stdout, $stderr ) = $this->captureWithKey(
+            switchboard => sub {
+                no strict 'refs';
+                &${UI_FN}($fatwilly);
+                use strict 'refs';
+                $Foswiki::engine->finalize( $fatwilly->{response},
+                    $fatwilly->{request} );
+            }
+        );
+    }
+    catch Foswiki::OopsException with {
+        my $e = shift;
+        $responseText = $e->stringify();
+    }
+    catch Foswiki::EngineException with {
+        my $e = shift;
+        $responseText = $e->stringify();
+    };
     $fatwilly->finish();
 
     $this->assert($responseText);
 
     # Remove CGI header
     my $CRLF = "\015\012";    # "\r\n" is not portable
-    my ($header, $body);
-    if ($responseText =~ /^(.*?)$CRLF$CRLF(.*)$/s) {
-        $header = $1;      # untaint is OK, it's a test
-        $body = $2;
-    } else {
+    my ( $header, $body );
+    if ( $responseText =~ /^(.*?)$CRLF$CRLF(.*)$/s ) {
+        $header = $1;         # untaint is OK, it's a test
+        $body   = $2;
+    }
+    else {
         $header = '';
-        $body = $responseText;
+        $body   = $responseText;
     }
 
     my $status = 666;
-    if ($header =~ /Status: (\d*)./) {
+    if ( $header =~ /Status: (\d*)./ ) {
         $status = $1;
     }
-    #aparently we allow the web server to add a 200 status thus risking that an error situation is marked as 200
-    #$this->assert_num_not_equals(666, $status, "no response Status set in probably valid reply\nHEADER: $header\n");
-    if ($status == 666) {
+
+#aparently we allow the web server to add a 200 status thus risking that an error situation is marked as 200
+#$this->assert_num_not_equals(666, $status, "no response Status set in probably valid reply\nHEADER: $header\n");
+    if ( $status == 666 ) {
         $status = 200;
     }
-    $this->assert_num_not_equals(500, $status, 'exception thrown');
+    $this->assert_num_not_equals( 500, $status, 'exception thrown' );
 
-    return ($status, $header, $body, $stdout, $stderr);
+    return ( $status, $header, $body, $stdout, $stderr );
 }
 
 #TODO: work out why some 'Use of uninitialised vars' don't crash the test (see preview)
@@ -144,19 +151,23 @@ sub call_UI_FN {
 #and that the switchboard still works.
 sub verify_switchboard_function {
     my $this = shift;
-    
-    my ($status, $header, $result, $stdout, $stderr) =
-      $this->call_UI_FN($this->{test_web}, $this->{test_topic});
+
+    my ( $status, $header, $result, $stdout, $stderr ) =
+      $this->call_UI_FN( $this->{test_web}, $this->{test_topic} );
 
     $this->assert_num_equals(
-        $expected_status{$SCRIPT_NAME} || 200, $status,
+        $expected_status{$SCRIPT_NAME} || 200,
+        $status,
         "GOT Status : $status\nHEADER: $header\n\nSTDERR: "
-          .($stderr||'')."\n");
-    if (!defined($expect_non_html{$SCRIPT_NAME})) {
-        $this->assert_str_not_equals('', $header);
-        if ($status != 302) {
-            $this->assert_str_not_equals('', $result, "$status: $result");
-        } else {
+          . ( $stderr || '' ) . "\n"
+    );
+    if ( !defined( $expect_non_html{$SCRIPT_NAME} ) ) {
+        $this->assert_str_not_equals( '', $header );
+        if ( $status != 302 ) {
+            $this->assert_str_not_equals( '', $result, "$status: $result" );
+        }
+        else {
+
             #$this->assert_null($result);
         }
     }
@@ -165,6 +176,5 @@ sub verify_switchboard_function {
 
 #TODO: craft specific tests for each script
 #TODO: including timing expectations... (imo statistics takes a long time in this test)
-
 
 1;
