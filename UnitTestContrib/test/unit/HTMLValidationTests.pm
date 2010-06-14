71b112e26e19b9de25db201a87e4531c9042e80f
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-06-14 01:12:24 +0000

    Item9147: Comitted wrong version, thanks to stupid symlink problem.
    git-svn-id: http://svn.foswiki.org/trunk@7778 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/HTMLValidationTests.pm
+++ UnitTestContrib/test/unit/HTMLValidationTests.pm
@@ -416,7 +416,7 @@ sub expected_in_scan {
     if ($got) {
         $_got = 1;
     }
-    if ( $expected ) {
+    if ( $expected or not (defined $expected) ) {
         $_expected = 1;
     }
     if ( $_expected != $_got ) {
