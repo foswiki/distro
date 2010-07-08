641d2130017961c020aab2af058e3cdded9a369f
MichaelTempest <MichaelTempest@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-07-08 20:35:06 +0000

    Item1326: perltidy
    git-svn-id: http://svn.foswiki.org/trunk@8075 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/SeleniumConfigTests.pm
+++ UnitTestContrib/test/unit/SeleniumConfigTests.pm
@@ -14,25 +14,39 @@ sub new {
 
 sub verify_SeleniumRc_config {
     my $this = shift;
-    $this->selenium->open_ok( Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'view') );
+    $this->selenium->open_ok(
+        Foswiki::Func::getScriptUrl(
+            $this->{test_web}, $this->{test_topic}, 'view'
+        )
+    );
     $this->login();
 }
 
 sub verify_SeleniumRc_ok_failure_reporting {
     my $this = shift;
     eval {
-        $this->selenium->open_ok( Foswiki::Func::getScriptUrl( 'Milkshakes', 'BronzeMoth', 'bananaFlavour') );
+        $this->selenium->open_ok(
+            Foswiki::Func::getScriptUrl(
+                'Milkshakes', 'BronzeMoth', 'bananaFlavour'
+            )
+        );
     };
-    $this->assert_matches( "^\nopen, ", $@, "Expected an exception");
+    $this->assert_matches( "^\nopen, ", $@, "Expected an exception" );
 }
 
 sub verify_SeleniumRc_like_failure_reporting {
     my $this = shift;
-    $this->selenium->open_ok( Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic}, 'view') );
+    $this->selenium->open_ok(
+        Foswiki::Func::getScriptUrl(
+            $this->{test_web}, $this->{test_topic}, 'view'
+        )
+    );
     eval {
-        $this->selenium->title_like( qr/There is no way that this would ever find its way into the title of web page. That would be simply insane!/ );
+        $this->selenium->title_like(
+qr/There is no way that this would ever find its way into the title of web page. That would be simply insane!/
+        );
     };
-    $this->assert_matches( "^\nget_title, ", $@, "Expected an exception");
+    $this->assert_matches( "^\nget_title, ", $@, "Expected an exception" );
 }
 
 1;
