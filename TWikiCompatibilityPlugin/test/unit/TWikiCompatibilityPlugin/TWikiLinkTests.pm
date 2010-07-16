5b54280e6bc63b188f6c8c24495a0e597c475b16
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-07-16 09:48:51 +0000

    Item9041: Extend the unit tests to test ALL topics from the specs, instead of just one from each kind
    git-svn-id: http://svn.foswiki.org/trunk@8199 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TWikiCompatibilityPlugin/test/unit/TWikiCompatibilityPlugin/TWikiLinkTests.pm
+++ TWikiCompatibilityPlugin/test/unit/TWikiCompatibilityPlugin/TWikiLinkTests.pm
@@ -3,36 +3,76 @@
 #
 
 package TWikiLinkTests;
-use FoswikiFnTestCase;
-our @ISA = qw( FoswikiFnTestCase );
 
 use strict;
+use warnings;
+
 use Foswiki::Func;
+use FoswikiFnTestCase;
+our @ISA = qw( FoswikiFnTestCase );
 
 sub new {
     my $self = shift()->SUPER::new( "Link", @_ );
     return $self;
 }
 
+sub compareTWikiAndFoswikiLinks {
+    my ( $this, $tlink, $flink ) = @_;
+
+    # Discard removed topics
+    if ( $flink eq 'System._remove_' ) {
+        $flink = $tlink;
+    }
+
+    # When target are not WikiWords, we have to patch them
+    if ( $flink =~ /\.([A-Z][a-z0-9]*)$/ ) {
+        $flink = "[[$flink][$1]]";
+    }
+    my $oldLink = Foswiki::Func::renderText($tlink);
+    my $newLink = Foswiki::Func::renderText($flink);
+    $this->assert_str_equals(
+        $newLink, $oldLink,
+        sprintf(
+            "%4siki link for %s: %s\n%4siki link for %s: %s",
+            TW => $tlink,
+            $oldLink,
+            Fosw => $flink,
+            $newLink
+        )
+    );
+}
+
 sub test_renamedMainTopic {
     my $this = shift;
-    my $goodLink = Foswiki::Func::renderText('Main.SitePreferences');
-    my $oldLink = Foswiki::Func::renderText('Main.TWikiPreferences');
-    $this->assert_str_equals($goodLink, $oldLink);
+    while (
+        my ( $tlink, $flink ) = each %{
+            $Foswiki::cfg{Plugins}{TWikiCompatibilityPlugin}
+              {MainWebTopicNameConversion}
+        }
+      )
+    {
+        $this->compareTWikiAndFoswikiLinks( "Main.$tlink" => "Main.$flink" );
+    }
 }
 
 sub test_renamedSystemTopic {
     my $this = shift;
-    my $goodLink = Foswiki::Func::renderText('System.BeginnersStartHere');
-    my $oldLink = Foswiki::Func::renderText('TWiki.ATasteOfTWiki');
-    $this->assert_str_equals($goodLink, $oldLink);
+    while (
+        my ( $tlink, $flink ) = each %{
+            $Foswiki::cfg{Plugins}{TWikiCompatibilityPlugin}
+              {TWikiWebTopicNameConversion}
+        }
+      )
+    {
+        $this->compareTWikiAndFoswikiLinks( "TWiki.$tlink" => "System.$flink" );
+    }
 }
 
 sub test_renamedSystemTopicWithLinkText {
     my $this = shift;
-    my $goodLink = Foswiki::Func::renderText('[[System.BeginnersStartHere][Link Text]]');
-    my $oldLink = Foswiki::Func::renderText('[[TWiki.ATasteOfTWiki][Link Text]]');
-    $this->assert_str_equals($goodLink, $oldLink);
+    $this->compareTWikiAndFoswikiLinks(
+        '[[System.BeginnersStartHere][Link Text]]' =>
+          '[[TWiki.ATasteOfTWiki][Link Text]]' );
 }
 
 1;
