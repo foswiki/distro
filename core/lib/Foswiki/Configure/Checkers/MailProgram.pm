58fab28bc931d6e3ed1adf12109af477e4e57ff0
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-08-25 02:04:34 +0000

    Item9542: migrate the mailprogram test from trunk, to find that the reason it wasn't working as i expected was a legacy tmwiki cfg ... even so, the extra warning/error will be useful
    git-svn-id: http://svn.foswiki.org/branches/Release01x00@8735 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/Checkers/MailProgram.pm
+++ core/lib/Foswiki/Configure/Checkers/MailProgram.pm
@@ -2,15 +2,15 @@
 package Foswiki::Configure::Checkers::MailProgram;
 
 use strict;
+use warnings;
 
-use Foswiki::Configure::Checker;
-
-use base 'Foswiki::Configure::Checker';
+use Foswiki::Configure::Checker ();
+our @ISA = ('Foswiki::Configure::Checker');
 
 sub check {
     my $this = shift;
 
-    return '' if ( !$Twiki::cfg{EnableEmail} );
+    return '' if ( !$Foswiki::cfg{EnableEmail} );
 
     eval "use Net::SMTP";
     my $n;
@@ -21,50 +21,51 @@ sub check {
     }
     elsif ( !$Foswiki::cfg{SMTP}{MAILHOST} ) {
         $n = $this->WARN(
-'Net::SMTP is installed in this environment, but {SMTP}{MAILHOST} is not defined, so the {MailProgram} <b>will</b> be used..'
+'Net::SMTP is installed in this environment, but {SMTP}{MAILHOST} is not defined, therefore the program listed at {MailProgram} will be used.'
         );
         $useprog = 1;
     }
     else {
         $n = $this->NOTE(
-'<em>Net::SMTP is installed in this environment, so this setting will <b>not</b> be used.</em>'
+'<code>Net::SMTP</code> is installed in this environment, so this setting will <strong>not</strong> be used.'
         );
         $useprog = 0;
     }
-    if ($useprog) {
-        my $val = $Foswiki::cfg{MailProgram} || '';
-        $val =~ s/\s.*$//g;
-        if ( !( -x $val ) ) {
-            $n .= $this->WARN("<tt>$val</tt> was not found. Check the path.");
+    my $val = $Foswiki::cfg{MailProgram} || '';
+    $val =~ s/\s.*$//g;
+    if ( !( -x $val ) ) {
+        if ($useprog) {
+            $n .= $this->ERROR("<tt>$val</tt> was not found (but is required). Check the path, or set the MAILHOST.");
+        } else {
+            $n .= $this->WARN("<tt>$val</tt> was not found.");
         }
     }
     return $n;
 }
 
 1;
-__DATA__
-#
-# Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-#
-# Copyright (C) 2008 Foswiki Contributors. All Rights Reserved.
-# Foswiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
-#
-# Additional copyrights apply to some or all of the code in this
-# file as follows:
-#
-# Copyright (C) 2000-2006 TWiki Contributors. All Rights Reserved.
-# TWiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# As per the GPL, removal of this notice is prohibited.
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2010 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 2000-2006 TWiki Contributors. All Rights Reserved.
+TWiki Contributors are listed in the AUTHORS file in the root
+of this distribution. NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
--- core/lib/Foswiki/Configure/Checkers/WebMasterEmail.pm
+++ core/lib/Foswiki/Configure/Checkers/WebMasterEmail.pm
@@ -2,19 +2,20 @@
 package Foswiki::Configure::Checkers::WebMasterEmail;
 
 use strict;
+use warnings;
 
-use Foswiki::Configure::Checker;
-
-use base 'Foswiki::Configure::Checker';
+use Foswiki::Configure::Checker ();
+our @ISA = ('Foswiki::Configure::Checker');
 
 sub check {
     my $this = shift;
 
     if ( !$Foswiki::cfg{WebMasterEmail} ) {
-        return $this->WARN(
+        return $this->ERROR(
 'Please make sure you enter the e-mail address of the webmaster. This is required for registration to work.'
         );
     }
+
     #    $regex{emailAddrRegex} ...
     if ( $Foswiki::cfg{WebMasterEmail} !~
         /^([a-z0-9!+$%&'*+-\/=?^_`{|}~.]+\@[a-z0-9\.\-]+)$/i )
@@ -25,29 +26,28 @@ sub check {
 }
 
 1;
-__DATA__
-#
-# Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-#
-# Copyright (C) 2008 Foswiki Contributors. All Rights Reserved.
-# Foswiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
-#
-# Additional copyrights apply to some or all of the code in this
-# file as follows:
-#
-# Copyright (C) 2000-2006 TWiki Contributors. All Rights Reserved.
-# TWiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# As per the GPL, removal of this notice is prohibited.
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2010 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 2000-2006 TWiki Contributors. All Rights Reserved.
+TWiki Contributors are listed in the AUTHORS file in the root
+of this distribution. NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
