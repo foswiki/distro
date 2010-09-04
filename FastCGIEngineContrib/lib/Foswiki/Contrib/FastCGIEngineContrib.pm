b0e3c3ad2cce7c52b46b6b1e4c069dd047073ca1
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-04 23:00:18 +0000

    Item9390: preparing for release
    git-svn-id: http://svn.foswiki.org/trunk/FastCGIEngineContrib@8896 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
+++ FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
@@ -18,7 +18,7 @@ use strict;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
-$RELEASE = '0.9.2';
+$RELEASE = '0.9.3';
 $SHORTDESCRIPTION = 'Permits Foswiki to be executed with FastCGI';
 
 1;
