2f670e3ad8076e2dd9e2b37de52047e96d307f3d
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-06 21:13:14 +0000

    Item8841: Prepare release version for 1.1.0 beta 1
    git-svn-id: http://svn.foswiki.org/trunk@8939 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -161,7 +161,7 @@ BEGIN {
     # DO NOT CHANGE THE FORMAT OF $VERSION
     # Automatically expanded on checkin of this module
     $VERSION = '$Date$ $Rev$ ';
-    $RELEASE = 'Foswiki-1.1.0-dev';
+    $RELEASE = 'Foswiki-1.1.0-beta1';
     $VERSION =~ s/^.*?\((.*)\).*: (\d+) .*?$/$RELEASE, $1, build $2/;
 
     # Default handlers for different %TAGS%
