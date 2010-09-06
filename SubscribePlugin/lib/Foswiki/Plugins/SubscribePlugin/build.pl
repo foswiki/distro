1e380c7c2409cf07417736dd4d9deef42721beaf
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-06 22:13:56 +0000

    Item9632: Create Release01x01 branch Removing all the none-default extensions from R11 branch I was a dumb mistake that I copied them in the first place
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@8941 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- SubscribePlugin/lib/Foswiki/Plugins/SubscribePlugin/build.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-
-BEGIN {  unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
-
-use Foswiki::Contrib::Build;
-
-my $build = new Foswiki::Contrib::Build('SubscribePlugin');
-
-$build->build($build->{target});
