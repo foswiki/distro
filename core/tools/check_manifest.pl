13bdb39971f7d83d66dcbed9b64c2b70a28bbb73
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-15 08:15:06 +0000

    Item9679: Fix check_manifest script and fix what it finds That was a lot of files that were still missing
    git-svn-id: http://svn.foswiki.org/trunk@9111 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/tools/check_manifest.pl
+++ core/tools/check_manifest.pl
@@ -37,10 +37,9 @@ while (scalar(@cwd) > 1) {
 
 die "Can't find root" unless (-d "$root/lib" && -d "$root/data");
 
-@skip = qw(tools test working logs);
+my @skip = qw(tools test working logs);
 print <<END;
-Run this script from anywhere (either in core root or in a subdir
-of twikiplugins).
+Run this script from anywhere a directory with a MANIFEST file
 
 The script will find and scan MANIFEST and compare the contents with
 what is checked in under subversion. Any differences are reported.
