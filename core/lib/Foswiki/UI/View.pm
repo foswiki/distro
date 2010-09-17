242418bc87c84cc05e3aebbfad7767156181b46f
DanDascalescu <DanDascalescu@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-17 02:54:29 +0000

    Item9700: add SMELL tag for %QUERYPARAMSTRING%, an undocumented template directive used only once
    git-svn-id: http://svn.foswiki.org/trunk@9185 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/UI/View.pm
+++ core/lib/Foswiki/UI/View.pm
@@ -254,6 +254,12 @@ sub view {
         next if ( $name eq 'topic' );
         push @qparams, $name => $query->param($name);
     }
+    ## SMELL: %QUERYPARAMSTRING% isn't a documented macro, and it's used
+    ## in only one template (viewtopicactionbuttons.tmpl). Perhaps the template
+    ## should instead use ";%QUERYSTRING%" and live with the extra ';' when 
+    ## (most often) the query string is empty? Otherwise, all template
+    ## directives or placeholders should be documented in
+    ## System.Skins#Settings_in_Skins or in System.Macros.
     $tmpl =~ s/%QUERYPARAMSTRING%/Foswiki::_make_params(1,@qparams)/geo;
 
     # extract header and footer from the template, if there is a
