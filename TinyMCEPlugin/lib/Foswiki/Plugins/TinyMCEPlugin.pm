2f84e15fb8af8d9ebfd3e83803d6fd269938328a
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-09-28 01:32:23 +0000

    Item9415: Add missing QuickHelp files.
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@9405 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
+++ TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
@@ -8,7 +8,7 @@ use warnings;
 use Assert;
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '16 Aug 2010';
+our $RELEASE           = '28 Sep 2010';
 our $SHORTDESCRIPTION  = 'Integration of the Tiny MCE WYSIWYG Editor';
 our $NO_PREFS_IN_TOPIC = 1;
 
