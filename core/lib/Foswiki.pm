fec3c7764e4041fa12912c01b017f7a303ee48b8
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-03 11:34:43 +0000

    Item8841: Uprev Foswiki.pm to 1.1.0
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@9484 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -161,7 +161,7 @@ BEGIN {
     # DO NOT CHANGE THE FORMAT OF $VERSION
     # Automatically expanded on checkin of this module
     $VERSION = '$Date$ $Rev$ ';
-    $RELEASE = 'Foswiki-1.1.0-RC1';
+    $RELEASE = 'Foswiki-1.1.0';
     $VERSION =~ s/^.*?\((.*)\).*: (\d+) .*?$/$RELEASE, $1, build $2/;
 
     # Default handlers for different %TAGS%
