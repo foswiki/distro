aea53afe4333acc7d735f7a9e111ee7eec33aa72
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-04 03:56:13 +0000

    Item9785: Add BuildContrib.pm and document requirement
    git-svn-id: http://svn.foswiki.org/trunk@9489 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
@@ -0,0 +1,15 @@
+package Foswiki::Contrib::BuildContrib;
+
+use vars qw( $VERSION $RELEASE );
+# This should always be $Rev: 1340 $ so that Foswiki can determine the
+# checked-in status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev: 1340 $';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = '$Date: (31 Jan 2009)';
+
+
+1;
