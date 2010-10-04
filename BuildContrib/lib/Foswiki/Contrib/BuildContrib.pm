5aa436b72732298ddac00ee74578c5ea59114058
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-04 19:34:06 +0000

    Item9785: BuildContrib should pick up VERSION/RELEASE from BuildContrib.pm
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@9507 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
@@ -0,0 +1,16 @@
+package Foswiki::Contrib::BuildContrib;
+use strict;
+
+# This should always be $Rev: 1340 $ so that Foswiki can determine the
+# checked-in status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+our $VERSION = '$Rev: 1340 $';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+our $RELEASE = '4 Oct 2010';
+
+our $SHORTDESCRIPTION = 'Automates build and packaging process, including installer generation, for extension modules.';
+
+1;
