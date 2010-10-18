9cf629d20bf9b2787f13e98a478020942131656e
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-18 23:26:57 +0000

    Item9842: % NOP % is not expanded when placed in a TOPICS META
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@9607 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -2452,6 +2452,9 @@ sub expandMacrosOnTopicCreation {
         $p->{value} =
           _processMacros( $this, $p->{value}, \&_expandMacroOnTopicCreation,
             $topicObject, 16 );
+        # kill markers used to prevent variable expansion
+        $p->{value} =~  s/%NOP%//g;
+
     }
 }
 
