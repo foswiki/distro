5d9013d4b1cfcb5d87a213c95e34203f8f2477dd
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-05 13:36:33 +0000

    Item9963: Bump release versions
    git-svn-id: http://svn.foswiki.org/trunk@9873 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TipsContrib/lib/Foswiki/Contrib/TipsContrib.pm
+++ TipsContrib/lib/Foswiki/Contrib/TipsContrib.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION          = '$Rev$';
-our $RELEASE          = '31 Jul 2010';
+our $RELEASE          = '06 Nov 2010';
 our $SHORTDESCRIPTION = 'Displays rotating list of "Tip-of-the-day"';
 
 1;
