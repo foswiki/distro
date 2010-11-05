49fe490133f7e9441dd27f54f9ee082b49a2e449
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-05 13:49:56 +0000

    Item9963: Bump release versions
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@9875 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '5.4';
+our $RELEASE = '5.4.1';
 our $SHORTDESCRIPTION =
 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
 
