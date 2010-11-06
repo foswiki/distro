791ab30221f6427f41c4d7f8205e609680b3945e
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-06 01:11:23 +0000

    Item9945: finetuning user feedback
    git-svn-id: http://svn.foswiki.org/trunk@9877 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/UIs/EXTEND.pm
+++ core/lib/Foswiki/Configure/UIs/EXTEND.pm
@@ -249,8 +249,7 @@ HERE
 
     if ( keys(%$plugins) && !$simulate ) {
         $feedback .= $this->NOTE(<<HERE);
-Note: Before you can use newly installed plugins, you must enable them in the
-"Plugins" section in the main page.
+<span class='foswikiAlert'>Note: Before you can use newly installed plugins, you must enable them in the Enabled Plugins section in the main page.</span>
 HERE
         $feedback .= "<pre>";
         foreach my $plu ( sort { lc($a) cmp lc($b) } keys %$plugins ) {
@@ -264,7 +263,7 @@ HERE
 sub _printFeedback {
     my ($feedback) = @_;
 
-    print "<div class='configureMessageBox foswikiAlert'>$feedback</div>";
+    print "<div class='configureMessageBox'>$feedback</div>";
 }
 
 sub _uninstall {
