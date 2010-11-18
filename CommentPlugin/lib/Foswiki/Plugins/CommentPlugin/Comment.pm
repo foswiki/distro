00e4b5bd99c670785591b080439aff55ef47776e
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-18 23:44:21 +0000

    Item10050: CommentPlugin prompt adds newlines that prevent COMMENT inside TML tables Release branch version added many \n probably to beautify HTML output but TML tables barf on these newlines CGI::start_form adds \n so must be removed
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@10009 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- CommentPlugin/lib/Foswiki/Plugins/CommentPlugin/Comment.pm
+++ CommentPlugin/lib/Foswiki/Plugins/CommentPlugin/Comment.pm
@@ -141,6 +141,8 @@ sub _handleInput {
         $url = Foswiki::Func::getScriptUrl( $web, $topic, 'save' );
     }
 
+    # Note: Item10050: If CommentPlugin prompt adds newlines then it prevents
+    # COMMENT inside TML tables so avoid cosmetic \n 
     my $noform = $attrs->remove('noform') || '';
     if ( $input !~ m/^%RED%/ ) {
         $input =~ s/%DISABLED%/$disable/g;
@@ -150,42 +152,36 @@ sub _handleInput {
         if ( $disable eq '' ) {
             my $hiddenFields = "";
             $hiddenFields .=
-              "\n" . CGI::hidden( -name => 'comment_action', -value => 'save' );
+              CGI::hidden( -name => 'comment_action', -value => 'save' );
             $hiddenFields .=
-              "\n" . CGI::hidden( -name => 'comment_type', -value => $type );
+              CGI::hidden( -name => 'comment_type', -value => $type );
             if ( defined($silent) ) {
-                $hiddenFields .=
-                  "\n" . CGI::hidden( -name => 'comment_nonotify', value => 1 );
+                $hiddenFields .= 
+                  CGI::hidden( -name => 'comment_nonotify', value => 1 );
             }
             if ($templatetopic) {
-                $hiddenFields .= "\n"
-                  . CGI::hidden(
+                $hiddenFields .= CGI::hidden(
                     -name  => 'comment_templatetopic',
                     -value => $templatetopic
                   );
             }
             if ($location) {
-                $hiddenFields .= "\n"
-                  . CGI::hidden(
+                $hiddenFields .= CGI::hidden(
                     -name  => 'comment_location',
                     -value => $location
                   );
             }
             elsif ($anchor) {
-                $hiddenFields .= "\n"
-                  . CGI::hidden( -name => 'comment_anchor', -value => $anchor );
+                $hiddenFields .= CGI::hidden( -name => 'comment_anchor', -value => $anchor );
             }
             else {
-                $hiddenFields .= "\n"
-                  . CGI::hidden( -name => 'comment_index', -value => $$pidx );
+                $hiddenFields .= CGI::hidden( -name => 'comment_index', -value => $$pidx );
             }
             if ($nopost) {
-                $hiddenFields .= "\n"
-                  . CGI::hidden( -name => 'comment_nopost', -value => $nopost );
+                $hiddenFields .= CGI::hidden( -name => 'comment_nopost', -value => $nopost );
             }
             if ($remove) {
-                $hiddenFields .= "\n"
-                  . CGI::hidden( -name => 'comment_remove', -value => $$pidx );
+                $hiddenFields .= CGI::hidden( -name => 'comment_remove', -value => $$pidx );
             }
             $input .= $hiddenFields;
         }
@@ -199,14 +195,18 @@ sub _handleInput {
             }
         }
         unless ( $noform eq 'on' ) {
-            $input = CGI::start_form(
+        	my $startform = CGI::start_form(
                 -name   => $type . $n,
                 -id     => $type . $n,
                 -action => $url,
                 -method => 'post'
-              )
-              . $input
-              . CGI::end_form();
+               );
+            
+            # Item10050: CGI may add a trailing new line.
+            # This prevents using COMMENT inside TML tables   
+            $startform =~ s/\n$//;
+            
+            $input = $startform . $input . CGI::end_form(); 
         }
     }
     $$pidx++;
