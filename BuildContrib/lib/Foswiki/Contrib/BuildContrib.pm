59f1ad30e67d88014a30bb401d622daba0b27474
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-27 06:12:00 +0000

    Item10088: Fix %$FOSWIKIAUTHORS% assuming a standard build checkout
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@10085 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
@@ -9,7 +9,7 @@ our $VERSION = '$Rev: 1340 $';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = '4 Oct 2010';
+our $RELEASE = '27 Nov 2010';
 
 our $SHORTDESCRIPTION = 'Automates build and packaging process, including installer generation, for extension modules.';
 
