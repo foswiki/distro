e6a3ed79f570123c302f244e1c5730fbddba6c97
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-12-01 06:12:46 +0000

    Item10098: update forms jq plugin from 2.25 to 2.43
    git-svn-id: http://svn.foswiki.org/trunk@10133 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
@@ -20,7 +20,7 @@ use vars qw(
 );
 
 $VERSION           = '$Rev: 20090710 (2010-08-18) $';
-$RELEASE           = '3.74';
+$RELEASE           = '3.75';
 $SHORTDESCRIPTION  = 'jQuery <nop>JavaScript library for Foswiki';
 $NO_PREFS_IN_TOPIC = 1;
 
