c240bd2f37c4817ed31e959c42b77dfb926bc4a0
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-12-12 00:59:08 +0000

    Item10143: fix trimming and stripping spaces
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@10267 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/pub/System/JavascriptFiles/foswikiString_src.js
+++ core/pub/System/JavascriptFiles/foswikiString_src.js
@@ -72,18 +72,7 @@ foswiki.String = {
      * @return A new string free from spaces.
      */
 	removeSpaces:function(inValue) {
-		if (!inValue) return null;
-		var sIn = inValue;
-		var sOut = '';
-		for ( var i = 0; i < sIn.length; i++ ) {
-			ch = sIn.charAt( i );
-			if( ch==' ' ) {
-				chgUpper = true;
-				continue;
-			}
-			sOut += ch;
-		}
-		return sOut;
+		return inValue.replace(/\s/g, '');
 	},
 	
 	/**
