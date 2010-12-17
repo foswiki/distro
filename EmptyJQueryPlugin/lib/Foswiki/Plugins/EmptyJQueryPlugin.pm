aab6ec618adf757755b8356daa961a40aa0628bc
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-12-17 12:59:20 +0000

    Item10173: make a simple EmptyJQueryPlugin in svn, so its trivial for someone to make new ones
    git-svn-id: http://svn.foswiki.org/trunk@10346 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ EmptyJQueryPlugin/lib/Foswiki/Plugins/EmptyJQueryPlugin.pm
@@ -0,0 +1,86 @@
+# See bottom of file for default license and copyright information
+
+=begin TML
+
+---+ package EmptyJQueryPlugin
+
+=cut
+
+package Foswiki::Plugins::EmptyJQueryPlugin;
+
+# Always use strict to enforce variable scoping
+use strict;
+use warnings;
+
+use Foswiki::Func ();       # The plugins API
+use Foswiki::Plugins ();    # For the API version
+
+our $VERSION = '$Rev$';
+
+our $RELEASE = '03 Mar 2010';
+
+# Short description of this plugin
+# One line description, is shown in the %SYSTEMWEB%.TextFormattingRules topic:
+our $SHORTDESCRIPTION = 'Your New Jquery plugin module..';
+
+our $NO_PREFS_IN_TOPIC = 1;
+
+=begin TML
+
+---++ initPlugin($topic, $web, $user) -> $boolean
+   * =$topic= - the name of the topic in the current CGI query
+   * =$web= - the name of the web in the current CGI query
+   * =$user= - the login name of the user
+   * =$installWeb= - the name of the web the plugin topic is in
+     (usually the same as =$Foswiki::cfg{SystemWebName}=)
+
+=cut
+
+sub initPlugin {
+    my ( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+        Foswiki::Func::writeWarning( 'Version mismatch between ',
+            __PACKAGE__, ' and Plugins.pm' );
+        return 0;
+    }
+    if ($Foswiki::cfg{Plugins}{JQueryPlugin}{Enabled}) {
+        require Foswiki::Plugins::JQueryPlugin;
+        Foswiki::Plugins::JQueryPlugin::registerPlugin("Your",
+        'Foswiki::Plugins::EmptyJQueryPlugin::YOUR');
+    }
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+1;
+__END__
+This copyright information applies to the EmptyJQueryPlugin:
+
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# EmptyJQueryPlugin is Copyright (C) 2010 Foswiki Contributors
+# 
+# The Javascript files packaged with this plugin are Copyright (C) and licensed
+# separately by their respective authors, please review those files separately
+# for more detailed information.
+#
+# Foswiki Contributors are listed in the AUTHORS file in the root
+# of this distribution. NOTE: Please extend that file, not this notice.
+#
+#
+# This license applies to EmptyJQueryPlugin and to any derivatives.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the root of this distribution.
---
+++ EmptyJQueryPlugin/lib/Foswiki/Plugins/EmptyJQueryPlugin/YOUR.pm
@@ -0,0 +1,45 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+# copyright for plugin and for jquery code
+# license..
+
+package Foswiki::Plugins::JQueryExtrasPlugin::YOUR;
+use strict;
+use warnings;
+use Foswiki::Plugins::JQueryPlugin::Plugin;
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+=begin TML
+
+---+ package Foswiki::Plugins::JQueryExtrasPlugin::YOUR
+
+This is the perl stub for the jquery.qtip plugin.
+
+=cut
+
+=begin TML
+
+---++ ClassMethod new( $class, $session, ... )
+
+Constructor
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $session = shift || $Foswiki::Plugins::SESSION;
+
+  my $this = bless($class->SUPER::new( 
+    $session,
+    name => 'your',
+    version => '0.0.1',
+    author => 'The Author of The JS',
+    homepage => 'jq module's URL',
+    documentation => "$Foswiki::cfg{SystemWebName}.JQueryYour",
+    puburl => '%PUBURLPATH%/%SYSTEMWEB%/JQueryExtrasPlugin/your',
+    javascript => ['jquery.your.js']
+  ), $class);
+
+  return $this;
+}
+
+1;
