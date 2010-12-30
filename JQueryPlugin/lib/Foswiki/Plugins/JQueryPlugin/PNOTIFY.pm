cb4f39de7d26e8ba915e53a39d854c6e8af12ee2
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-12-30 22:04:41 +0000

    Item10210: create pnotify plugin for showing temporary messages
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@10446 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/PNOTIFY.pm
@@ -0,0 +1,69 @@
+# See bottom of file for license and copyright information
+package Foswiki::Plugins::JQueryPlugin::PNOTIFY;
+use strict;
+use warnings;
+
+use Foswiki::Plugins::JQueryPlugin::Plugin;
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+=begin TML
+
+---+ package Foswiki::Plugins::JQueryPlugin::PNOTIFY
+
+This is the perl stub for the jquery.pnotify plugin.
+
+=cut
+
+=begin TML
+
+---++ ClassMethod new( $class, $session, ... )
+
+Constructor
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
+
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name         => 'PNotify',
+            version      => '1.0.1',
+            author       => 'Hunter Perrin',
+            homepage     => 'http://pines.sourceforge.net/pnotify/',
+            css          => ['jquery.pnotify.default.css'],
+            javascript   => ['jquery.pnotify.js']
+        ),
+        $class
+    );
+
+    return $this;
+}
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2010 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 2009-2010 Michael Daum http://michaeldaumconsulting.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
