4fb60f1115e8293f69188b08f6022208f0164034
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-03-01 09:37:42 +0000

    Item9986: doc correction
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@10881 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Func.pm
+++ core/lib/Foswiki/Func.pm
@@ -3495,7 +3495,7 @@ sub readTopicText {
 
 =begin TML
 
----+++ saveTopicText( $web, $topic, $text, \%options ) -> $oopsUrl
+---+++ saveTopicText( $web, $topic, $text, $ignorePermissions, $dontNotify ) -> $oopsUrl
 
 Save topic text, typically obtained by readTopicText(). Topic data usually includes meta data; the file attachment meta data is replaced by the meta data from the topic file if it exists.
    * =$web=                - Web name, e.g. ='Main'=, or empty
