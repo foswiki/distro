56b8249a680a4f539043da27734f6e44b8627a16
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-04-10 18:03:45 +0000

    Item10621: Fix the change history and revision of default extensions for 1.1.3
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@11361 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- CommentPlugin/lib/Foswiki/Plugins/CommentPlugin.pm
+++ CommentPlugin/lib/Foswiki/Plugins/CommentPlugin.pm
@@ -11,7 +11,7 @@ use Foswiki::Func    ();
 use Foswiki::Plugins ();
 
 our $VERSION = '$Rev$';
-our $RELEASE = '19 Nov 2010';
+our $RELEASE = '10 Apr 2011';
 our $SHORTDESCRIPTION =
   'Quickly post comments to a page without an edit/save cycle';
 our $NO_PREFS_IN_TOPIC = 1;
