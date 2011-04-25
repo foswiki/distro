0fccaad7725b7609a1153914dc8dce942979343a
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-04-25 15:04:12 +0000

    Item9496: Implement re-usable way to set default text of input fields. Implemented as jquery module.
    git-svn-id: http://svn.foswiki.org/trunk@11551 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/PLACEHOLDER.pm
@@ -0,0 +1,69 @@
+# See bottom of file for license and copyright information
+package Foswiki::Plugins::JQueryPlugin::PLACEHOLDER;
+use strict;
+use warnings;
+
+use Foswiki::Plugins::JQueryPlugin::Plugin;
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+=begin TML
+
+---+ package Foswiki::Plugins::JQueryPlugin::PLACEHOLDER
+
+This is the perl stub for the jquery.placeholder plugin.
+
+=cut
+
+=begin TML
+
+---++ ClassMethod new( $class, $session, ... )
+
+Constructor
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
+
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name    => 'Placeholder',
+            version => 'r1',
+            author  => 'Sam Grossberg',
+            homepage =>
+              'https://github.com/samgro',
+            javascript => ['jquery.placeholder.js'],
+        ),
+        $class
+    );
+
+    return $this;
+}
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2011 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 2006-2011 Michael Daum http://michaeldaumconsulting.com, Arthur Clemens http://visiblearea.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
---
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/placeholder/jquery.placeholder.uncompressed.js
@@ -0,0 +1,48 @@
+/**
+* https://gist.github.com/823300
+* based on
+* http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+* 
+* Usage:
+* 	
+* 	<input type="text" class="foswikiInputField" placeholder="Fill me ...">
+* 
+*/
+jQuery.placeholder = function() {
+  $('[placeholder]').focus(function() {
+    var input = $(this);
+    if (input.hasClass('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+      input.removeClass('foswikiInputFieldBeforeFocus');
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() === '') {
+      input.addClass('placeholder');
+      input.addClass('foswikiInputFieldBeforeFocus');
+      input.val(input.attr('placeholder'));
+    }
+  }).blur().parents('form').submit(function() {
+    $(this).find('[placeholder]').each(function() {
+      var input = $(this);
+      if (input.hasClass('placeholder')) {
+        input.val('');
+      }
+    });
+  });
+  
+  // Clear input on refresh so that the placeholder class gets added back
+  $(window).unload(function() {
+    $('[placeholder]').val('');
+  });
+};
+
+/* when using AJAX */
+$('[placeholder]').livequery(function() {
+    $.placeholder($(this));
+}); 
+    
+$(document).ready(function() {
+	$.placeholder();
+});
