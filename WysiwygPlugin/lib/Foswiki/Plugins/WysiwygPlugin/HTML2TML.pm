9ba61137c8f47c93769a5c7031ea05e8b8e49e6e
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-05-13 05:02:21 +0000

    Item10679: remove the duplicate entry for comment_h as Michael T noted weeks ago, and in the process, un-break the translator tests - sorry
    git-svn-id: http://svn.foswiki.org/trunk@11691 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- WysiwygPlugin/lib/Foswiki/Plugins/WysiwygPlugin/HTML2TML.pm
+++ WysiwygPlugin/lib/Foswiki/Plugins/WysiwygPlugin/HTML2TML.pm
@@ -61,8 +61,7 @@ sub new {
         declaration_h    => [ '_ignore',   'self' ],
         start_document_h => [ '_ignore',   'self' ],
         end_document_h   => [ '_ignore',   'self' ],
-        default_h        => [ '_default',  'self,event,text' ],
-        comment_h         => [ '_ignore',   'self' ]
+        default_h        => [ '_default',  'self,event,text' ]
     );
 
     $this = bless( $this, $class );
