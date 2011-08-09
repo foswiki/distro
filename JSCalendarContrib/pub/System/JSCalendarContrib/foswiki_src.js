88ac37bc4d99da8b363ffa398b6e51a0efec97ac
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-08-09 18:10:59 +0000

    Item11020: Allow calendars with different date formats on one page. Show AM/PM interface if the date format contains AM/PM tokens. Add new calendar style =large= and make it the default.
    git-svn-id: http://svn.foswiki.org/trunk@12290 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- JSCalendarContrib/pub/System/JSCalendarContrib/foswiki_src.js
+++ JSCalendarContrib/pub/System/JSCalendarContrib/foswiki_src.js
@@ -2,49 +2,46 @@ function initCalendar(format) {
 	"use strict";
 	if (_dynarch_popupCalendar !== null) {
 		_dynarch_popupCalendar.hide();
-	} else {
-		var cal = new Calendar(1, null, function (cal, date) {
-				cal.sel.value = date;
-				if (typeof(cal.sel.onchange) === 'function') {
-					cal.sel.onchange();
-				}
-				if (cal.dateClicked) {
-					cal.callCloseHandler();
-				}
-			}, function (cal) {
-				cal.hide();
-				_dynarch_popupCalendar = null;
-			});
-		if (format && format.search(/%H|%I|%k|%l|%M|%p|%P/) !== -1) {
-			cal.showsTime = true;
-		} else {
-			cal.showsTime = false;
-		}
-		cal.showsOtherMonths = true;
-		_dynarch_popupCalendar = cal;
-		cal.setRange(1900, 2070);
-		cal.create();
+	} 
+	var cal = new Calendar(1, null, function (cal, date) {
+			cal.sel.value = date;
+			if (typeof (cal.sel.onchange) === 'function') {
+				cal.sel.onchange();
+			}
+			if (cal.dateClicked) {
+				cal.callCloseHandler();
+			}
+		}, function (cal) {
+			cal.hide();
+			_dynarch_popupCalendar = null;
+		});
+	if (format) {
+		cal.setDateFormat(format);
 	}
+	// show hour/minute interface?
+	cal.showsTime = (format && format.search(/%H|%I|%k|%l|%M|%p|%P/) !== -1) ? true : false;
+	// show am/pm interface?
+	cal.time24 = (format && format.search(/%I|%l|%p|%P/) !== -1) ? false : true;
+	cal.showsOtherMonths = true;
+	_dynarch_popupCalendar = cal;
+	cal.setRange(1900, 2070);
+	cal.create();
+	return cal;
 }
 
-// Reuses the same "calendar" object for all date-type fields on the page
 function showCalendar(id, format) {
 	"use strict";
-	if (format === undefined) {
+	if (format === undefined || format === '') {
 		format = jQuery('#' + id).data('jscalendar-format');
 	}
 	if (format === undefined) {
 		alert("no format passed to calendar with id:" + id);
 		return;
 	}
-	initCalendar(format);
-	if (format) {
-		_dynarch_popupCalendar.setDateFormat(format);
-	}
-	var el = document.getElementById(id);
-	_dynarch_popupCalendar.parseDate(el.value);
-	_dynarch_popupCalendar.sel = el;
-	_dynarch_popupCalendar.showAtElement(el, "Br");
+	var cal = initCalendar(format), el = document.getElementById(id);
+	cal.parseDate(el.value);
+	cal.sel = el;
+	cal.showAtElement(el, "Br");
 	return false;
 }
 
@@ -54,18 +51,13 @@ function formatValue(id, format) {
 		alert("JSCalendarContrib error: No format passed to calendar with id:" + id);
 		return;
 	}
-	initCalendar(format);
-	if (format) {
-		_dynarch_popupCalendar.setDateFormat(format);
-	}
-	var el = document.getElementById(id);
-	_dynarch_popupCalendar.parseDate(el.value);
-	_dynarch_popupCalendar.sel = el;
-	_dynarch_popupCalendar.callHandler();
+	var cal = initCalendar(format), el = document.getElementById(id);
+	cal.sel = el;
+	cal.callHandler();
 	
 	// store format in attribute so it can be used
 	// when the calendar is invoked with showCalendar()
-	jQuery(function() {
+	jQuery(function () {
 		jQuery('#' + id).data('jscalendar-format', format);
 	});
 }
