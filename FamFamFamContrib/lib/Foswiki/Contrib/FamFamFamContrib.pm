05e82cf290f02eec3937637a7286c0526ed53ae3
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-09-04 18:45:15 +0000

    Item11104: add trash-small icon
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@12442 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- FamFamFamContrib/lib/Foswiki/Contrib/FamFamFamContrib.pm
+++ FamFamFamContrib/lib/Foswiki/Contrib/FamFamFamContrib.pm
@@ -7,7 +7,7 @@ use warnings;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
-$RELEASE = '1.4.1';
+$RELEASE = '1.4.2';
 $SHORTDESCRIPTION =
   'FamFamFam Silk is a smooth, free icon set, containing over 2000 icons';
 
