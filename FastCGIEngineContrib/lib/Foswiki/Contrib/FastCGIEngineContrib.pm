613449467829acbfb0eb779937c42e8f271f8c14
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-09-08 20:12:26 +0000

    Item9957: add release info text so we can release a code change Micha commited ~10months ago
    git-svn-id: http://svn.foswiki.org/trunk/FastCGIEngineContrib@12487 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
+++ FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
@@ -18,7 +18,7 @@ use strict;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
-$RELEASE = '0.9.4';
+$RELEASE = '0.9.5';
 $SHORTDESCRIPTION = 'Permits Foswiki to be executed with FastCGI';
 
 1;
