63e185b716cb6acbf7eb4436da3d9bb9c124004c
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-10-06 00:33:11 +0000

    Item11169: Find test groups for FoswikiStoreTests
    git-svn-id: http://svn.foswiki.org/trunk@12715 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/FoswikiStoreTestCase.pm
+++ UnitTestContrib/test/unit/FoswikiStoreTestCase.pm
@@ -10,15 +10,18 @@ our @ISA = qw( FoswikiFnTestCase );
 
 # Determine if RCS is installed. used in tests for RCS functionality.
 our $rcs_installed;
+
 sub rcs_is_installed {
-    if (!defined($rcs_installed)) {
+    if ( !defined($rcs_installed) ) {
         eval {
             `co -V`;    # Check to see if we have co
         };
         if ( $@ || $? ) {
             $rcs_installed = 0;
-            print STDERR "*** CANNOT RUN RcsWrap TESTS - NO COMPATIBLE co: $@\n";
-        } else {
+            print STDERR
+              "*** CANNOT RUN RcsWrap TESTS - NO COMPATIBLE co: $@\n";
+        }
+        else {
             $rcs_installed = 1;
         }
     }
@@ -42,31 +45,35 @@ sub set_up_for_verify {
 }
 
 sub fixture_groups {
-    my @groups;
+    my %groups;
     foreach my $dir (@INC) {
         if ( opendir( D, "$dir/Foswiki/Store" ) ) {
             foreach my $alg ( readdir D ) {
                 next unless $alg =~ s/^(.*)\.pm$/$1/;
-                next if defined &$alg;
-                $ENV{PATH} =~ /^(.*)$/ms;
-                $ENV{PATH} = $1;
-                next if $alg =~ /RcsWrap/ && !rcs_is_installed();
-                ($alg) = $alg =~ /^(.*)$/ms;
-                eval "require Foswiki::Store::$alg";
-                die $@ if $@;
-                no strict 'refs';
-                *$alg = sub {
-                    my $this = shift;
-                    $Foswiki::cfg{Store}{Implementation} =
-                      'Foswiki::Store::'.$alg;
-                    $this->set_up_for_verify();
-                };
-                use strict 'refs';
-                push(@groups, $alg);
+                unless ( defined &$alg ) {
+                    $ENV{PATH} =~ /^(.*)$/ms;
+                    $ENV{PATH} = $1;
+                    next if $alg =~ /RcsWrap/ && !rcs_is_installed();
+                    ($alg) = $alg =~ /^(.*)$/ms;
+                    eval "require Foswiki::Store::$alg";
+                    die $@ if $@;
+                    no strict 'refs';
+                    *$alg = sub {
+                        my $this = shift;
+                        $Foswiki::cfg{Store}{Implementation} =
+                          'Foswiki::Store::' . $alg;
+                        $this->set_up_for_verify();
+                    };
+                    use strict 'refs';
+                }
+                $groups{$alg} = 1;
             }
             closedir(D);
         }
     }
+
+    my @groups;
+    push @groups, ( keys %groups );
     return \@groups;
 }
 
