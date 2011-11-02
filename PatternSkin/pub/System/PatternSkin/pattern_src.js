e65463e1b53cdc335f1e9cc1c309e866ce7ee4a1
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-11-02 22:23:06 +0000

    Item11229: fix attachment count: use QUERY count instead of javascript
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@12989 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/pub/System/PatternSkin/pattern_src.js
+++ PatternSkin/pub/System/PatternSkin/pattern_src.js
@@ -1,55 +1,54 @@
+jQuery(document).ready(function ($) {
 
-jQuery(document).ready(
-    function ($) {
-        var searchResultsCount = 0;
+	"use strict";
 
-		// Create an attachment counter in the attachment table twisty.
-        $('.foswikiAttachments').livequery(function() {
-				var table = $(this).find('table.foswikiTable');
-				if (!table) {
-					return;
-				}
-				var rows = table.attr('rows');
-				if (!rows) {
-					return;
-				}
-				var count = rows.length - 1;
-				var countStr = " <span class='foswikiSmall'>"
-					+ count + "<\/span>";
-				$(this).find('.patternAttachmentHeader').livequery(function() {
-					$(this).append(countStr);
-				});
-			}
-		);
+	var searchResultsCount = 0;
 
-        // Search page handling
-        $('.foswikiSearchResultCount span').livequery(function() {
-                searchResultsCount += parseInt($(this).html());
-            });
-
-        if (searchResultsCount > 0) {
-            $('#foswikiNumberOfResultsContainer').livequery(function() {
-                    // write result count
-                    $(this).html(' ' + foswiki.getMetaTag('TEXT_NUM_TOPICS') + ' <b>' + searchResultsCount + ' </b>');
-                });
-        }
-        
-        $('input.foswikiFocus').livequery(function() {
-				$(this).focus();
-			}
-		);
-        
-        $('input.foswikiChangeFormButton').livequery('click', function() {
-				if (foswiki.Edit) {
-                    foswiki.Edit.validateSuppressed = true;
-				}
-			}
-		);
-
-		$('body.patternEditPage input').livequery('keydown', function(event) {
-				if (event.keyCode == 13) {
-					return false;
-				}
-			}
-		);
+	/*
+	Only for Foswiki 1.0.
+	*/
+	/*
+	// Create an attachment counter in the attachment table twisty.
+	$('.foswikiAttachments').livequery(function () {
+		var rows, count, countStr;
+		rows = $(this).find('table.foswikiTable tr');
+		if (!rows) {
+			return;
+		}
+		count = rows.length - 1;
+		countStr = " <span class='foswikiSmall'>" + count + "<\/span>";
+		$(this).find('.patternAttachmentHeader').livequery(function () {
+			$(this).append(countStr);
+		});
 	});
+	*/
+	
+	// Search page handling
+	$('.foswikiSearchResultCount span').livequery(function () {
+		searchResultsCount += parseInt($(this).html(), 10);
+	});
+
+	if (searchResultsCount > 0) {
+		$('#foswikiNumberOfResultsContainer').livequery(function () {
+			// write result count
+			$(this).html(' ' + foswiki.getMetaTag('TEXT_NUM_TOPICS') + ' <b>' + searchResultsCount + ' </b>');
+		});
+	}
+
+	$('input.foswikiFocus').livequery(function () {
+		$(this).focus();
+	});
+
+	$('input.foswikiChangeFormButton').livequery('click', function () {
+		if (foswiki.Edit) {
+			foswiki.Edit.validateSuppressed = true;
+		}
+	});
+
+	$('body.patternEditPage input').livequery('keydown', function (event) {
+		if (event.keyCode === 13) {
+			return false;
+		}
+	});
+
+});
