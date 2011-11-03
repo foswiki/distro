73f6782f7d00f5fc9c461cee2c5a87c32e9bf916
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-11-03 14:17:23 +0000

    Item11209: merged from trunk. When checking in an inconsistent or no-history topic, use the modification time on the .txt file and force it into the history.
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@12998 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Store/VC/RcsWrapHandler.pm
+++ core/lib/Foswiki/Store/VC/RcsWrapHandler.pm
@@ -106,11 +106,6 @@ sub initText {
 sub ci {
     my ( $this, $isStream, $data, $comment, $user, $date ) = @_;
 
-    #    unless ( -e $this->{rcsFile} ) {    #
-    #                                        # SMELL: what is this for?
-    #        _lock($this);
-    #        _ci( $this, $comment, $user, $date );
-    #    }
     _lock($this);
     if ($isStream) {
         $this->saveStream($data);
@@ -118,7 +113,42 @@ sub ci {
     else {
         $this->saveFile( $this->{file}, $data );
     }
-    _ci( $this, $comment, $user, $date );
+
+    $comment = 'none' unless $comment;
+
+    my ( $cmd, $rcsOutput, $exit );
+    if ( defined($date) ) {
+        require Foswiki::Time;
+        $date = Foswiki::Time::formatTime( $date, '$rcs', 'gmtime' );
+        $cmd = $Foswiki::cfg{RCS}{ciDateCmd};
+        ( $rcsOutput, $exit ) = Foswiki::Sandbox->sysCommand(
+            $cmd,
+            USERNAME => $user,
+            FILENAME => $this->{file},
+            COMMENT  => $comment,
+            DATE     => $date
+        );
+    }
+    else {
+        $cmd = $Foswiki::cfg{RCS}{ciCmd};
+        ( $rcsOutput, $exit ) = Foswiki::Sandbox->sysCommand(
+            $cmd,
+            USERNAME => $user,
+            FILENAME => $this->{file},
+            COMMENT  => $comment
+        );
+    }
+    $rcsOutput ||= '';
+
+    if ($exit) {
+        throw Error::Simple( $cmd . ' of '
+              . $this->hidePath( $this->{file} )
+              . ' failed: '
+              . $exit . ' '
+              . $rcsOutput );
+    }
+
+    chmod( $Foswiki::cfg{RCS}{filePermission}, $this->{file} );
 }
 
 # implements VC::Handler
@@ -444,46 +474,6 @@ sub parseRevisionDiff {
         }
     }
     return \@diffArray;
-}
-
-sub _ci {
-    my ( $this, $comment, $user, $date ) = @_;
-
-    $comment = 'none' unless $comment;
-
-    my ( $cmd, $rcsOutput, $exit );
-    if ( defined($date) ) {
-        require Foswiki::Time;
-        $date = Foswiki::Time::formatTime( $date, '$rcs', 'gmtime' );
-        $cmd = $Foswiki::cfg{RCS}{ciDateCmd};
-        ( $rcsOutput, $exit ) = Foswiki::Sandbox->sysCommand(
-            $cmd,
-            USERNAME => $user,
-            FILENAME => $this->{file},
-            COMMENT  => $comment,
-            DATE     => $date
-        );
-    }
-    else {
-        $cmd = $Foswiki::cfg{RCS}{ciCmd};
-        ( $rcsOutput, $exit ) = Foswiki::Sandbox->sysCommand(
-            $cmd,
-            USERNAME => $user,
-            FILENAME => $this->{file},
-            COMMENT  => $comment
-        );
-    }
-    $rcsOutput ||= '';
-
-    if ($exit) {
-        throw Error::Simple( $cmd . ' of '
-              . $this->hidePath( $this->{file} )
-              . ' failed: '
-              . $exit . ' '
-              . $rcsOutput );
-    }
-
-    chmod( $Foswiki::cfg{RCS}{filePermission}, $this->{file} );
 }
 
 sub _lock {
