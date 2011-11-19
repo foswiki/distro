97effd93dd732c23fa8010c71b030205e9735cf9
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-11-19 18:55:01 +0000

    Item10247: add my very old expandTemplate cache code so I can debug in detail what order the result is constructed in - driven from Foswiki::Template::TRACE - I'll either remove it later, or docco it on the System.SkinTemplates topic
    git-svn-id: http://svn.foswiki.org/trunk@13137 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Templates.pm
+++ core/lib/Foswiki/Templates.pm
@@ -39,7 +39,7 @@ use Foswiki::Attrs ();
 # Enable TRACE to get HTML comments in the output showing where templates
 # (both DEFs and files) open and close. Will probably bork the output, so
 # normally you should use it with a bin/view command-line.
-use constant TRACE => 0;
+use constant TRACE => 1;
 
 my $MAX_EXPANSION_RECURSIONS = 999;
 
@@ -179,7 +179,7 @@ sub tmplP {
     my $val = '';
     if ( exists( $this->{VARS}->{$template} ) ) {
         $val = $this->{VARS}->{$template};
-        $val = "<!--$template-->$val<!--/$template-->" if (TRACE);
+        $val = "<!--$template-->\n$val<!--/$template-->\n" if (TRACE);
         foreach my $p ( keys %$params ) {
             if ( $p eq 'then' || $p eq 'else' ) {
                 $val =~ s/%$p%/$this->expandTemplate($1)/ge;
@@ -337,6 +337,7 @@ sub readTemplate {
     # SMELL: legacy - leading spaces to tabs, should not be required
     $result =~ s|^(( {3})+)|"\t" x (length($1)/3)|geom;
 
+    $this->saveTemplateToCache('_complete', $name, $skins, $web, $result) if (TRACE);
     return $result;
 }
 
@@ -352,8 +353,10 @@ sub _readTemplateFile {
     # if the name ends in .tmpl, then this is an explicit include from
     # the templates directory. No further searching required.
     if ( $name =~ /\.tmpl$/ ) {
-        return _decomment(
+        my$text =  _decomment(
             _readFile( $session, "$Foswiki::cfg{TemplateDir}/$name" ) );
+        $this->saveTemplateToCache('_cache', $name, $skins, $web, $text) if (TRACE);
+        return $text;
     }
 
     my $userdirweb  = $web;
@@ -379,12 +382,14 @@ sub _readTemplateFile {
             $text = '' unless defined $text;
 
             $text =
-                "<!--$userdirweb/$userdirname-->" 
+                "<!--$userdirweb/$userdirname-->\n" 
               . $text
-              . "<!--/$userdirweb/$userdirname-->"
+              . "<!--/$userdirweb/$userdirname-->\n"
               if (TRACE);
-
-            return _decomment($text);
+              
+            $text =  _decomment($text);
+            $this->saveTemplateToCache('_cache', $name, $skins, $web, $text) if (TRACE);
+            return $text;
         }
     }
     else {
@@ -510,10 +515,12 @@ sub _readTemplateFile {
                 my $text = $meta->text();
                 $text = '' unless defined $text;
 
-                $text = "<!--$web1.$name1-->$text<!--/$web1.$name1-->"
+                $text = "<!--$web1.$name1-->\n$text<!--/$web1.$name1-->\n"
                   if (TRACE);
 
-                return _decomment($text);
+              $text =  _decomment($text);
+              $this->saveTemplateToCache('_cache', $name, $skins, $web, $text) if (TRACE);
+              return $text;
             }
         }
         elsif ( -e $file ) {
@@ -522,7 +529,9 @@ sub _readTemplateFile {
             # recursion prevention.
             $this->{files}->{$file} = 1;
 
-            return _decomment( _readFile( $session, $file ) );
+            my $text = _decomment( _readFile( $session, $file ) );
+            $this->saveTemplateToCache('_cache', $name, $skins, $web, $text) if (TRACE);
+            return $text;
         }
     }
 
@@ -539,7 +548,7 @@ sub _readFile {
         my $text = <$F>;
         close($F);
 
-        $text = "<!--$fn-->$text<!--/$fn-->" if (TRACE);
+        $text = "<!--$fn-->\n$text<!--/$fn-->\n" if (TRACE);
 
         return $text;
     }
@@ -559,6 +568,47 @@ sub _decomment {
     $text =~ s/\s*%{.*?}%\s*//sg;
     return $text;
 }
+
+#See http://wikiring.com/Blog/BlogEntry8?cat=WikiRing
+#used for debugging templates, and later maybe for speed.
+sub saveTemplateToCache {
+    my( $this, $cacheName, $name, $skins, $web, $tmplText ) = @_;
+    $skins = '' unless (defined($skins));
+    $web = '' unless (defined($web));
+    
+    my $tmpl_cachedir = $Foswiki::cfg{TemplateDir}.$cacheName;
+    mkdir($tmpl_cachedir) unless (-e $tmpl_cachedir);
+    my $filename =  Foswiki::Sandbox::untaintUnchecked($tmpl_cachedir.'/'.$name.'__'.$skins.'__'.$web.'.tmpl');
+    
+    unless ( open( FILE, ">$filename" ) )  {
+       die "Can't create file $filename - $!\n" if DEBUG;
+        print STDERR "Can't create file $filename - $!\n";
+        
+        return;
+    }
+    print FILE $tmplText;
+    close( FILE);
+}
+#unused, but can be used for a speedup by caching the expanded Template
+sub getTemplateFromCache {
+    my( $this, $name, $skins, $web ) = @_;
+    $skins = '' unless (defined($skins));
+    $web = '' unless (defined($web));
+
+    my $tmpl_cachedir = $Foswiki::cfg{TemplateDir}.'_cache';
+    mkdir($tmpl_cachedir) unless (-e $tmpl_cachedir);
+    my $filename =  Foswiki::Sandbox::untaintUnchecked($tmpl_cachedir.'/'.$name.'__'.$skins.'__'.$web.'.tmpl');
+    
+    if (-e $filename) {
+        open( IN_FILE, "<$filename" ) || return;
+        local $/ = undef; # set to read to EOF
+        my $data = <IN_FILE>;
+        close( IN_FILE );
+        return $data;
+    }
+}
+
+
 
 1;
 __END__
