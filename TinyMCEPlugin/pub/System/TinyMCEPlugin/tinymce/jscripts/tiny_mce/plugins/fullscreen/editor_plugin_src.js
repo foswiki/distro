1f94379d34b42cee0623a0b57bd0e80e10a0cc87
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-11-22 18:53:05 +0000

    Item10288: Revert prior attempt at fix
    Better not to change moxicode source
    
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@13184 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/pub/System/TinyMCEPlugin/tinymce/jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
+++ TinyMCEPlugin/pub/System/TinyMCEPlugin/tinymce/jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
@@ -22,9 +22,6 @@
 				var win, de = DOM.doc.documentElement;
 
 				if (ed.getParam('fullscreen_is_enabled')) {
-					document.getElementById('save').setAttribute('accesskey', document.getElementById('save').getAttribute('title'));
-					document.getElementById('quietsave').setAttribute('accesskey', document.getElementById('quietsave').getAttribute('title'));
-					document.getElementById('checkpoint').setAttribute('accesskey', document.getElementById('checkpoint').getAttribute('title'));
 					if (ed.getParam('fullscreen_new_window'))
 						closeFullscreen(); // Call to close in new window
 					else {
@@ -42,13 +39,6 @@
 
 					return;
 				}
-				document.getElementById('save').setAttribute('title',  document.getElementById('save').getAttribute('accesskey'));
-				document.getElementById('quietsave').setAttribute('title',  document.getElementById('quietsave').getAttribute('accesskey'));
-				document.getElementById('checkpoint').setAttribute('title',  document.getElementById('checkpoint').getAttribute('accesskey'));
-
-				document.getElementById('save').removeAttribute('accesskey');
-				document.getElementById('quietsave').removeAttribute('accesskey');
-				document.getElementById('checkpoint').removeAttribute('accesskey');
 
 				if (ed.getParam('fullscreen_new_window')) {
 					win = DOM.win.open(url + "/fullscreen.htm", "mceFullScreenPopup", "fullscreen=yes,menubar=no,toolbar=no,scrollbars=no,resizable=yes,left=0,top=0,width=" + screen.availWidth + ",height=" + screen.availHeight);
