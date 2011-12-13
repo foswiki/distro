8084e895aab244258abfc8eb8406efcf54f999c8
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-12-13 00:54:51 +0000

    Item10646: Update release numbers for 1.1.4 plugin
    git-svn-id: http://svn.foswiki.org/trunk@13402 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TablePlugin/lib/Foswiki/Plugins/TablePlugin.pm
+++ TablePlugin/lib/Foswiki/Plugins/TablePlugin.pm
@@ -9,7 +9,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '1.137';
+our $RELEASE = '1.138';
 our $SHORTDESCRIPTION =
   'Control attributes of tables and sorting of table columns';
 our $NO_PREFS_IN_TOPIC = 1;
