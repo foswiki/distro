4ec383a43b39e8fbc9b4b29edd0f673974c5da67
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-12-31 00:31:02 +0000

    Item11383: Fix unit tests
    Modernized HTML is good,  but it breaks unit tests. Would be nice if the
    fixes also included the needed changes to unit tests.
    
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@13526 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- EditTablePlugin/test/unit/EditTablePlugin/EditTablePluginTests.pm
+++ EditTablePlugin/test/unit/EditTablePlugin/EditTablePluginTests.pm
@@ -821,7 +821,7 @@ INPUT
 <table border="1" class="foswikiTable" rules="none">
 	<tbody>
 		<tr class="foswikiTableOdd foswikiTableRowdataBgSorted0 foswikiTableRowdataBg0">
-			<td bgcolor="#ffffff" class="foswikiTableCol0 foswikiFirstCol foswikiLast"> <img src="%PUBURLPATH%/%SYSTEMWEB%/DocumentGraphics/choice-yes.png" alt="DONE" title="DONE" width="16" height="16" border="0" /> </td>
+			<td bgcolor="#ffffff" class="foswikiTableCol0 foswikiFirstCol foswikiLast"> <img alt=DONE height=16 src=%PUBURLPATH%/%SYSTEMWEB%/DocumentGraphics/choice-yes.png title=DONE width=16></img> </td>
 			<td bgcolor="#ffffff" class="foswikiTableCol1 foswikiLastCol foswikiLast"> <a href="$viewUrl" class="foswikiCurrentTopicLink">$topicName</a> </td>
 		</tr>
 	</tbody>
