52b7142ce56ad13c5f4510703737c480de8152ef
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-01-03 15:52:00 +0000

    Item11403: fixed cleaning up spurious lease files
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@13548 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Store/VC/Handler.pm
+++ core/lib/Foswiki/Store/VC/Handler.pm
@@ -915,9 +915,10 @@ sub removeSpuriousLeases {
     my $web = $Foswiki::cfg{DataDir} . '/' . $this->{web} . '/';
     if ( opendir( my $W, $web ) ) {
         foreach my $f ( readdir($W) ) {
-            if ( $f =~ /^(.*)\.lease$/ ) {
+            my $file = $web.$f;
+            if ( $file =~ /^(.*)\.lease$/ ) {
                 if ( !-e "$1.txt,v" ) {
-                    unlink($f);
+                    unlink($file);
                 }
             }
         }
