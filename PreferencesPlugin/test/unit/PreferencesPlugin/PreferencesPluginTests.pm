27d311ca134fb1ea6b91eaf24ad1735b4bd1a003
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-01-16 01:45:48 +0000

    Item11431: PreferencesPlugin perlcriticisms
    git-svn-id: http://svn.foswiki.org/trunk@13659 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PreferencesPlugin/test/unit/PreferencesPlugin/PreferencesPluginTests.pm
+++ PreferencesPlugin/test/unit/PreferencesPlugin/PreferencesPluginTests.pm
@@ -1,17 +1,15 @@
 # See bottom of file for license and copyright information
+package PreferencesPluginTests;
 use strict;
 use warnings;
 
-package PreferencesPluginTests;
-
-use FoswikiFnTestCase;
+use FoswikiFnTestCase();
 our @ISA = qw( FoswikiFnTestCase );
 
 use strict;
 use warnings;
-use Unit::Request;
-use Unit::Response;
-use Foswiki;
+use Unit::Request();
+use Foswiki();
 
 sub set_up {
     my $this = shift;
@@ -19,12 +17,14 @@ sub set_up {
     $this->SUPER::set_up();
 
     $Foswiki::cfg{Plugins}{PreferencesPlugin}{Enabled} = 1;
+
+    return;
 }
 
 sub test_edit_simple {
     my $this  = shift;
-    my $query = new Unit::Request( { prefsaction => ['edit'], } );
-    my $text  = <<HERE;
+    my $query = Unit::Request->new( { prefsaction => ['edit'], } );
+    my $text  = <<'HERE';
    * Set FLEEGLE = floon
 %EDITPREFERENCES%
 HERE
@@ -39,7 +39,7 @@ HERE
     my $viewUrl =
       Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic},
         'viewauth' );
-    $this->assert_html_equals( <<HTML, $result );
+    $this->assert_html_equals( <<"HTML", $result );
 <form method="post" action="$viewUrl" enctype="multipart/form-data" name="editpreferences">
  <span style="font-weight:bold;" class="foswikiAlert">FLEEGLE = SHELTER\0070</span>
  <input type="submit" name="prefsaction" value="Save new settings" accesskey="s" class="foswikiSubmit" />
@@ -47,13 +47,15 @@ HERE
  <input type="submit" name="prefsaction" value="Cancel" accesskey="c" class="foswikiButtonCancel" />
 </form>
 HTML
+
+    return;
 }
 
 # Item4816
 sub test_edit_multiple_with_comments {
     my $this  = shift;
-    my $query = new Unit::Request( { prefsaction => ['edit'], } );
-    my $text  = <<HERE;
+    my $query = Unit::Request->new( { prefsaction => ['edit'], } );
+    my $text  = <<'HERE';
 <!-- Comment should be outside form -->
 Normal text outside form
 %EDITPREFERENCES%
@@ -72,7 +74,7 @@ HERE
     my $viewUrl =
       Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic},
         'viewauth' );
-    $this->assert_html_equals( <<HTML, $result );
+    $this->assert_html_equals( <<"HTML", $result );
 <!-- Comment should be outside form -->
 Normal text outside form
 <form method="post" action="$viewUrl" enctype="multipart/form-data" name="editpreferences">
@@ -88,7 +90,7 @@ Normal text outside form
 HTML
 
     Foswiki::Plugins::PreferencesPlugin::postRenderingHandler($result);
-    $this->assert_html_equals( <<HTML, $result );
+    $this->assert_html_equals( <<"HTML", $result );
 <!-- Comment should be outside form -->
 Normal text outside form
 <form method="post" action="$viewUrl" enctype="multipart/form-data" name="editpreferences">
@@ -104,13 +106,14 @@ Normal text outside form
 -->
 HTML
 
+    return;
 }
 
 # Item1117
 sub test_edit_multiple_with_verbatim {
     my $this  = shift;
-    my $query = new Unit::Request( { prefsaction => ['edit'], } );
-    my $text  = <<HERE;
+    my $query = Unit::Request->new( { prefsaction => ['edit'], } );
+    my $text  = <<'HERE';
 Normal text outside form
 <verbatim>
 <!-- Comment-start inside verbatim is ignored
@@ -136,7 +139,7 @@ HERE
     # and it must be encoded here or else the "html_equals" fails
     $result =~ s/<!-- Comment/&lt;!-- Comment/;
     $result =~ s/<(\/?)verbatim>/<$1pre>/g;
-    $this->assert_html_equals( <<HTML, $result );
+    $this->assert_html_equals( <<"HTML", $result );
 Normal text outside form
 <pre>
 &lt;!-- Comment-start inside verbatim is ignored
@@ -153,11 +156,12 @@ Not in the form
    * Set <span style="font-weight:bold;" class="foswikiAlert">FLEEGLE2 = <input type="text" name="FLEEGLE2" value="floontoo" size="80" class="foswikiAlert foswikiInputField" /></span></form>
 HTML
 
+    return;
 }
 
 sub test_save {
     my $this  = shift;
-    my $query = new Unit::Request(
+    my $query = Unit::Request->new(
         {
             prefsaction => ['Save new settings'],
             FLEEGLE     => ['flurb'],
@@ -166,7 +170,7 @@ sub test_save {
             MAKEMEZERO  => ['0'],
         }
     );
-    my $input = <<HERE;
+    my $input = <<'HERE';
    * Set FLEEGLE = floon
 
    * Set Pumpkin = turkey
@@ -203,7 +207,7 @@ HERE
     $this->assert_matches( qr/^Location: $viewUrl\r$/m, $result );
     my ( $meta, $text ) =
       Foswiki::Func::readTopic( $this->{test_web}, $this->{test_topic} );
-    $this->assert_str_equals( <<HERE, $text );
+    $this->assert_str_equals( <<"HERE", $text );
    * Set FLEEGLE = flurb
 
    * Set Pumpkin = turkey
@@ -216,11 +220,12 @@ HERE
 %EDITPREFERENCES%
 HERE
 
+    return;
 }
 
 sub test_view {
     my $this = shift;
-    my $text = <<HERE;
+    my $text = <<"HERE";
    * Set FLEEGLE = floon
 %EDITPREFERENCES%
 HERE
@@ -235,12 +240,14 @@ HERE
     my $viewUrl =
       Foswiki::Func::getScriptUrl( $this->{test_web}, $this->{test_topic},
         'viewauth' );
-    $this->assert_html_equals( <<HTML, $result );
+    $this->assert_html_equals( <<"HTML", $result );
 <form method="post" action="$viewUrl" enctype="multipart/form-data" name="editpreferences">
  <input type="hidden" name="prefsaction" value="edit"  />
  <input type="submit" name="edit" value="Edit Preferences" class="foswikiButton" />
 </form>
 HTML
+
+    return;
 }
 
 1;
