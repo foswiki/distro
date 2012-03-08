e25ef0c55a9e9635eba7c12a954b9f8e55bdcd9a
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-03-08 08:06:19 +0000

    Item9647: now I understand the fix again :(
    git-svn-id: http://svn.foswiki.org/trunk@14248 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Form/Select.pm
+++ core/lib/Foswiki/Form/Select.pm
@@ -126,7 +126,8 @@ sub renderForEdit {
     my $choices = '';
 
     my %isSelected = map { $_ => 1 } split( /\s*,\s*/, $value );
-    foreach my $option ( @{ $this->getOptions() } ) {
+    foreach my $item ( @{ $this->getOptions() } ) {
+        my $option = $item; # Item9647: make a copy not to modify the original value in the array
         my %params = ( class => 'foswikiOption', );
         $params{selected} = 'selected' if $isSelected{$option};
         if ( $this->{_descriptions}{$option} ) {
