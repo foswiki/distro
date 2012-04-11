3c9081e1b4bda0b310d55b4f2e7443a04e5315cb
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-04-11 01:21:37 +0000

    Item000: BUILD Foswiki-1.1.5 at Wed Apr 11 01:21:35 2012 GMT
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@14594 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -169,7 +169,7 @@ BEGIN {
     # DO NOT CHANGE THE FORMAT OF $VERSION
     # Automatically expanded on checkin of this module
     $VERSION = '$Date$ $Rev$ ';
-    $RELEASE = 'Foswiki-1.1.5-RC2';
+    $RELEASE = 'Foswiki-1.1.5';
     $VERSION =~ s/^.*?\((.*)\).*: (\d+) .*?$/$RELEASE, $1, build $2/;
 
     # Default handlers for different %TAGS%
