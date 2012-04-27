ed7402da51f24e45bef88dfb75f862f17ea8332a
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-04-27 14:02:52 +0000

    Item9662: Setting change for initial tables
    git-svn-id: http://svn.foswiki.org/trunk@14672 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
+++ TinyMCEPlugin/lib/Foswiki/Plugins/TinyMCEPlugin.pm
@@ -8,7 +8,7 @@ use warnings;
 use Assert;
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '1.2.6';
+our $RELEASE           = '1.2.7';
 our $SHORTDESCRIPTION  = 'Integration of the Tiny MCE WYSIWYG Editor';
 our $NO_PREFS_IN_TOPIC = 1;
 
