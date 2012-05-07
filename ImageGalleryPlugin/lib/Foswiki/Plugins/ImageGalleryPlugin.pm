633f23b35234b7809900a62b98e39f4c423dbc79
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 10:17:12 +0000

    Item11808: reverting perltidy
    git-svn-id: http://svn.foswiki.org/trunk@14721 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- ImageGalleryPlugin/lib/Foswiki/Plugins/ImageGalleryPlugin.pm
+++ ImageGalleryPlugin/lib/Foswiki/Plugins/ImageGalleryPlugin.pm
@@ -9,7 +9,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at
+# GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -17,104 +17,89 @@ package Foswiki::Plugins::ImageGalleryPlugin;
 use strict;
 
 # =========================
-our $VERSION           = '$Rev$';
-our $RELEASE           = '6.00';
+our $VERSION = '$Rev$';
+our $RELEASE = '6.00';
 our $NO_PREFS_IN_TOPIC = 1;
-our $SHORTDESCRIPTION =
-  'Displays image gallery with auto-generated thumbnails from attachments';
+our $SHORTDESCRIPTION = 'Displays image gallery with auto-generated thumbnails from attachments';
 our $isInitialized;
 our $igpId;
-our $TranslationToken =
-  "\2\3\2";    # SMELL arbitrary but may clash with other plugin's transtoks
+our $TranslationToken = "\2\3\2"; # SMELL arbitrary but may clash with other plugin's transtoks
 our %knownGalleries;
 
 # =========================
 sub initPlugin {
+  #my ($topic, $web, $user, $installWeb) = @_;
 
-    #my ($topic, $web, $user, $installWeb) = @_;
+  if ($Foswiki::Plugins::VERSION < 1) {
+    &Foswiki::Func::writeWarning("Version mismatch between ImageGalleryPlugin and Plugins.pm");
+    return 0;
+  }
+  $igpId = 0;
+  $isInitialized = 0;
+  %knownGalleries = ();
 
-    if ( $Foswiki::Plugins::VERSION < 1 ) {
-        &Foswiki::Func::writeWarning(
-            "Version mismatch between ImageGalleryPlugin and Plugins.pm");
-        return 0;
-    }
-    $igpId          = 0;
-    $isInitialized  = 0;
-    %knownGalleries = ();
+  Foswiki::Func::registerTagHandler('IMAGEGALLERY', \&renderImageGalleryPlaceholder);
+  Foswiki::Func::registerTagHandler('NRIMAGES', \&renderNrImages);
 
-    Foswiki::Func::registerTagHandler( 'IMAGEGALLERY',
-        \&renderImageGalleryPlaceholder );
-    Foswiki::Func::registerTagHandler( 'NRIMAGES', \&renderNrImages );
-
-    return 1;
+  return 1;
 }
 
 # =========================
 sub doInit {
-    return if $isInitialized;
-    $isInitialized = 1;
+  return if $isInitialized;
+  $isInitialized = 1;
 
-    Foswiki::Func::addToHEAD( "IMAGEGALLERYPLUGIN", <<'HERE');
-<link rel="stylesheet" href="%PUBURL%/%SYSTEMWEB%/ImageGalleryPlugin/style.css" type="text/css" media="all" />
+  Foswiki::Func::addToHEAD("IMAGEGALLERYPLUGIN", <<'HERE');
+<link rel="stylesheet" href="%PUBURLPATH%/%SYSTEMWEB%/ImageGalleryPlugin/style.css" type="text/css" media="all" />
 HERE
 
-    require Foswiki::Plugins::ImageGalleryPlugin::Core;
+  require Foswiki::Plugins::ImageGalleryPlugin::Core;
 }
 
 # =========================
 sub renderImageGalleryPlaceholder {
-    my ( $session, $params, $theTopic, $theWeb ) = @_;
+  my ($session, $params, $theTopic, $theWeb) = @_;
 
-    doInit();
+  doInit();
 
-    $igpId++;
-    $knownGalleries{$igpId} = {
-        core => Foswiki::Plugins::ImageGalleryPlugin::Core->new(
-            $igpId, $theTopic, $theWeb
-        ),
-        params => $params
-    };
-    return
-        $TranslationToken
-      . 'IMAGEGALLERY{'
-      . $igpId . '}'
-      . $TranslationToken;
+  $igpId++;
+  $knownGalleries{$igpId} = {
+    core => Foswiki::Plugins::ImageGalleryPlugin::Core->new($igpId, $theTopic, $theWeb),
+    params => $params
+  };
+  return $TranslationToken.'IMAGEGALLERY{'.$igpId.'}'.$TranslationToken;
 }
 
 # =========================
 sub postRenderingHandler {
+  # my $text = shift;
 
-    # my $text = shift;
-
-    $_[0] =~
-s/${TranslationToken}IMAGEGALLERY{(.*?)}$TranslationToken/renderImageGallery($1)/ge;
+  $_[0] =~ s/${TranslationToken}IMAGEGALLERY{(.*?)}$TranslationToken/renderImageGallery($1)/ge;
 }
 
 # =========================
 sub renderImageGallery {
-    my $igpId = shift;
+  my $igpId = shift;
 
-    my $igp = $knownGalleries{$igpId};
-    return '' unless $igp;
+  my $igp = $knownGalleries{$igpId};
+  return '' unless $igp;
 
-    return $igp->{core}->render( $igp->{params} );
+  return $igp->{core}->render($igp->{params});
 }
+
 
 # =========================
 sub renderNrImages {
-    my ( $session, $params, $theTopic, $theWeb ) = @_;
+  my ($session, $params, $theTopic, $theWeb) = @_;
 
-    doInit();
+  doInit();
 
-    my $igp = Foswiki::Plugins::ImageGalleryPlugin::Core->new( undef, $theTopic,
-        $theWeb );
-    if ( $igp->init($params) ) {
-        return scalar @{ $igp->getImages() };
-    }
-    else {
-        return Foswiki::Plugins::ImageGalleryPlugin::Core::renderError(
-            "can't initialize");
-    }
+  my $igp = Foswiki::Plugins::ImageGalleryPlugin::Core->new(undef, $theTopic, $theWeb);
+  if ($igp->init($params)) {
+    return scalar @{$igp->getImages()};
+  } else {
+    return Foswiki::Plugins::ImageGalleryPlugin::Core::renderError("can't initialize");
+  }
 }
 
 1;
