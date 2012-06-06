790686216d95bcd415b3ba2034a3fea96efa9b48
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-06-06 03:02:14 +0000

    Item11927: Permit attachment move / delete
    Only check RENAME auth if the topic itself is being renamed.
    If an attachment is being renamed, then check for change auth.
    
    git-svn-id: http://svn.foswiki.org/trunk@14954 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/RenameTests.pm
+++ UnitTestContrib/test/unit/RenameTests.pm
@@ -1936,6 +1936,149 @@ sub test_rename_attachment {
     return;
 }
 
+sub test_move_attachment_RENAME_Topic_denied {
+    my $this = shift;
+
+    my ($to) = Foswiki::Func::readTopic( $this->{test_web}, 'NewTopic' );
+    $to->text('Wibble');
+    $to->save();
+    $to->finish();
+
+    # returns undef on OSX with 3.15 version of CGI module (works on 3.42)
+    my $stream = File::Temp->new( UNLINK => 0 );
+    print $stream "Blah Blah";
+    $this->assert( $stream->close() );
+    $stream->unlink_on_destroy(1);
+
+    ($to) = Foswiki::Func::readTopic( $this->{test_web}, $this->{test_topic} );
+    $to->text("Wibble\n   * Set ALLOWTOPICRENAME = NotMe\n");
+    $to->attach( name => 'dis.dat', file => $stream->filename );
+    $to->save();
+    $to->finish();
+
+    $this->_reset_session(
+        {
+            attachment    => ['dis.dat'],
+            newattachment => ['dis.dat'],
+            newtopic      => ['NewTopic'],
+            newweb        => $this->{test_web},
+            path_info     => "/$this->{test_web}/$this->{test_topic}"
+        }
+    );
+
+    my ($text) = $this->captureWithKey( rename => $UI_FN, $this->{session} );
+    $this->assert_matches( qr/Status: 302/,                 $text );
+    $this->assert_matches( qr#/$this->{test_web}/NewTopic#, $text );
+    $this->assert(
+        !Foswiki::Func::attachmentExists(
+            $this->{test_web}, $this->{test_topic}, 'dis.dat'
+        )
+    );
+    $this->assert(
+        Foswiki::Func::attachmentExists(
+            $this->{test_web}, 'NewTopic', 'dis.dat'
+        )
+    );
+
+    # Make sure rename back fails if change of target is denied
+    ($to) = Foswiki::Func::readTopic( $this->{test_web}, $this->{test_topic} );
+    $to->text("Wibble\n   * Set ALLOWTOPICCHANGE = NotMe\n");
+    $to->save();
+    $to->finish();
+
+    $this->_reset_session(
+        {
+            attachment    => ['dis.dat'],
+            newattachment => ['dis.dat'],
+            newtopic      => [ $this->{test_topic} ],
+            newweb        => $this->{test_web},
+            path_info     => "/$this->{test_web}/NewTopic"
+        }
+    );
+
+    try {
+        ($text) = $this->captureWithKey( rename => $UI_FN, $this->{session} );
+        $this->assert( 0, $text );
+    }
+    catch Foswiki::AccessControlException with {
+        my $e = shift;
+        $this->assert_equals( $this->{test_topic},           $e->{topic} );
+        $this->assert_equals( 'CHANGE',                      $e->{mode} );
+        $this->assert_equals( 'access not allowed on topic', $e->{reason} );
+    }
+    otherwise {
+        $this->assert( 0, shift );
+    };
+
+    $this->assert_matches( qr/Status: 302/,                 $text );
+    $this->assert_matches( qr#/$this->{test_web}/NewTopic#, $text );
+    $this->assert(
+        !Foswiki::Func::attachmentExists(
+            $this->{test_web}, $this->{test_topic}, 'dis.dat'
+        )
+    );
+    $this->assert(
+        Foswiki::Func::attachmentExists(
+            $this->{test_web}, 'NewTopic', 'dis.dat'
+        )
+    );
+
+    return;
+}
+
+sub test_move_attachment_RENAME_Web_denied {
+    my $this = shift;
+
+    my ($m) = Foswiki::Func::readTopic( "$this->{test_web}", 'WebPreferences' );
+    $m->text("   * Set ALLOWWEBRENAME = NotMe\n");
+    $m->save();
+    $m->finish();
+
+    my ($to) = Foswiki::Func::readTopic( $this->{new_web}, 'NewTopic' );
+    $to->text('Wibble');
+    $to->save();
+    $to->finish();
+
+    # returns undef on OSX with 3.15 version of CGI module (works on 3.42)
+    my $stream = File::Temp->new( UNLINK => 0 );
+    print $stream "Blah Blah";
+    $this->assert( $stream->close() );
+    $stream->unlink_on_destroy(1);
+
+    ($to) = Foswiki::Func::readTopic( $this->{test_web}, $this->{test_topic} );
+    $to->text("Wibble\n");
+    $to->attach( name => 'dis.dat', file => $stream->filename );
+    $to->finish();
+
+    $this->_reset_session(
+        {
+            attachment    => ['dis.dat'],
+            newattachment => ['dis.dat'],
+            newtopic      => ['NewTopic'],
+            newweb        => $this->{new_web},
+            path_info     => "/$this->{test_web}/$this->{test_topic}"
+        }
+    );
+
+    my ($text) = $this->captureWithKey( rename => $UI_FN, $this->{session} );
+    $this->assert_matches( qr/Status: 302/,                $text );
+    $this->assert_matches( qr#/$this->{new_web}/NewTopic#, $text );
+    $this->assert(
+        Foswiki::Func::topicExists( $this->{test_web}, $this->{test_topic} ) );
+    $this->assert(
+        !Foswiki::Func::attachmentExists(
+            $this->{test_web}, $this->{test_topic}, 'dis.dat'
+        )
+    );
+    $this->assert(
+        Foswiki::Func::attachmentExists(
+            $this->{new_web}, 'NewTopic', 'dis.dat'
+        )
+    );
+
+    return;
+}
+
 # Item5464 - Rename of attachment requires change access, not rename access
 sub test_rename_attachment_Rename_Denied_Change_Allowed {
     my $this = shift;
