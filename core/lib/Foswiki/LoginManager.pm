84718c997b0e03a948cae51d6998e1bc24f9e8b3
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-06-11 09:04:26 +0000

    Item11935: set umask explicitly while creating session files
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@14963 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/LoginManager.pm
+++ core/lib/Foswiki/LoginManager.pm
@@ -336,7 +336,7 @@ sub loadSession {
             if ($sid) {
                 $this->{_cgisession} =
                   Foswiki::LoginManager::Session->new( undef, $sid,
-                    { Directory => $sessionDir } );
+                    { Directory => $sessionDir, UMask => 0660 } );
             }
             else {
 
@@ -344,7 +344,7 @@ sub loadSession {
 
                 $this->{_cgisession} =
                   Foswiki::LoginManager::Session->new( undef, undef,
-                    { Directory => $sessionDir } );
+                    { Directory => $sessionDir, UMask => 0660 } );
                 _trace( $this, "New IP2SID session" );
                 $this->_IP2SID( $this->{_cgisession}->id() );
             }
@@ -355,7 +355,7 @@ sub loadSession {
 
             $this->{_cgisession} =
               Foswiki::LoginManager::Session->new( undef, $session->{request},
-                { Directory => $sessionDir } );
+                { Directory => $sessionDir, UMask => 0660 } );
         }
 
         die Foswiki::LoginManager::Session->errstr()
@@ -649,9 +649,14 @@ sub userLoggedIn {
 
         # create new session if necessary
         unless ( $this->{_cgisession} ) {
-            $this->{_cgisession} =
-              Foswiki::LoginManager::Session->new( undef, $session->{request},
-                { Directory => "$Foswiki::cfg{WorkingDir}/tmp" } );
+            $this->{_cgisession} = Foswiki::LoginManager::Session->new(
+                undef,
+                $session->{request},
+                {
+                    Directory => "$Foswiki::cfg{WorkingDir}/tmp",
+                    UMask     => 0660
+                }
+            );
             die Foswiki::LoginManager::Session->errstr()
               unless $this->{_cgisession};
         }
