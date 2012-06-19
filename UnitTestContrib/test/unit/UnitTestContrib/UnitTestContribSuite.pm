8526647c91435bffebc85629fad5d3f6a347ddcc
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-06-19 02:53:44 +0000

    Item11956: Report suites that fail to compile
    git-svn-id: http://svn.foswiki.org/trunk@15042 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/UnitTestContrib/UnitTestContribSuite.pm
+++ UnitTestContrib/test/unit/UnitTestContrib/UnitTestContribSuite.pm
@@ -7,6 +7,14 @@ our @ISA = 'Unit::TestSuite';
 
 sub name { 'UnitTestContribSuite' }
 
-sub include_tests { qw(EavesdropTests UnitTestContribTests) }
+sub include_tests {
+    return (
+        qw(EavesdropTests UnitTestContribTests),
+
+        # Item11956 - used for testing reporting of modules that fail to compile
+        #'UnitTestContribFailsToCompileTest',
+        #'UnitTestContribSuiteMissingTest',
+    );
+}
 
 1;
