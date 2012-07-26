4fafa7bf6e1c1323b64930cfbf25ac9ccaed1aa1
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-07-26 00:03:05 +0000

    Item11740: retry: pattern skin theme
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@15192 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
@@ -20,7 +20,7 @@ use vars qw(
 );
 
 $VERSION           = '$Rev$';
-$RELEASE           = '4.40';
+$RELEASE           = '4.41';
 $SHORTDESCRIPTION  = 'jQuery <nop>JavaScript library for Foswiki';
 $NO_PREFS_IN_TOPIC = 1;
 
