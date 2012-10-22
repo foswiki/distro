cbea62f67a76a2e7fc9cc209e7b469c6c956f4e1
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-10-22 07:14:54 +0000

    Item12175: implement %SET
    git-svn-id: http://svn.foswiki.org/trunk@15657 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ core/lib/Foswiki/Macros/SET.pm
@@ -0,0 +1,54 @@
+# See bottom of file for license and copyright information
+package Foswiki;
+
+use strict;
+use warnings;
+use Assert;
+
+use Foswiki::Plugins ();
+
+sub SET {
+    my ( $this, $params, $topicObject ) = @_;
+
+    my $name = $params->{_DEFAULT} || $params->{name};
+    ASSERT($name) if DEBUG;
+
+    return '' unless defined $name;
+
+    my $value = $params->{value};
+    $value = '' unless defined $value;    # unset
+
+    $Foswiki::Plugins::SESSION->{prefs}
+      ->setSessionPreferences( $name => $value );
+
+    return '';
+}
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2012 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 1999-2007 Peter Thoeny, peter@thoeny.org
+and TWiki Contributors. All Rights Reserved. TWiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+Based on parts of Ward Cunninghams original Wiki and JosWiki.
+Copyright (C) 1998 Markus Peter - SPiN GmbH (warpi@spin.de)
+Some changes by Dave Harris (drh@bhresearch.co.uk) incorporated
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
