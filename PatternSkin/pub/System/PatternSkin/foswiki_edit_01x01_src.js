76d87243d8ec316d29f8f2bff2fd860c7c7e09d6
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-11-01 20:34:10 +0000

    Item12192: use correct links to jquery css images; remove unused edit screen
    git-svn-id: http://svn.foswiki.org/trunk@15826 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- PatternSkin/pub/System/PatternSkin/foswiki_edit_01x01_src.js
+++ PatternSkin/pub/System/PatternSkin/foswiki_edit_01x01_src.js
@@ -25,82 +25,9 @@ As per the GPL, removal of this notice is prohibited.
  * Requires JQUERYPLUGIN::FOSWIKI
  */
 
-var EDITBOX_ID = "topic";
-// edit box rows
-var EDITBOX_PREF_ROWS_ID     = "EditTextareaRows";
-var EDITBOX_CHANGE_STEP_SIZE = 4;
-var EDITBOX_MIN_ROWCOUNT     = 4;
-// edit box font style
-var EDITBOX_PREF_FONTSTYLE_ID            = "EditTextareaFontStyle";
-var EDITBOX_FONTSTYLE_MONO               = "mono";
-var EDITBOX_FONTSTYLE_PROPORTIONAL       = "proportional";
-var EDITBOX_FONTSTYLE_MONO_STYLE         = "foswikiEditboxStyleMono";
-var EDITBOX_FONTSTYLE_PROPORTIONAL_STYLE = "foswikiEditboxStyleProportional";
-
 (function($) {
     foswiki.Edit = {
-        textareaInited: false,
-        fontStyle: null,
         validateSuppressed: false
-    };
-
-    /**
-     * Sets the font style of the edit box and the signature box. The change
-     *  is written to a cookie.
-     * @param inFontStyle: either EDITBOX_FONTSTYLE_MONO or
-     *  EDITBOX_FONTSTYLE_PROPORTIONAL
-     */
-    foswiki.Edit.getFontStyle = function() {
-        if (foswiki.Edit.fontStyle) {
-            return foswiki.Edit.fontStyle;
-        }
-        
-        var pref = foswiki.Pref.getPref(EDITBOX_PREF_FONTSTYLE_ID);
-        
-        if (!pref || (pref !== EDITBOX_FONTSTYLE_PROPORTIONAL
-                      && pref !== EDITBOX_FONTSTYLE_MONO)) {
-            pref = EDITBOX_FONTSTYLE_PROPORTIONAL;
-        }
-        
-        return pref;
-    };
-    
-    /**
-     * Sets the font style of the edit box and the signature box. The change
-     *  is written to a cookie.
-     * @param inFontStyle: either EDITBOX_FONTSTYLE_MONO or
-     *  EDITBOX_FONTSTYLE_PROPORTIONAL
-     */
-    foswiki.Edit.setFontStyle = function(inFontStyle) {
-        if (inFontStyle === EDITBOX_FONTSTYLE_MONO) {
-            $('#' + EDITBOX_ID).removeClass(
-                EDITBOX_FONTSTYLE_PROPORTIONAL_STYLE).addClass(
-                EDITBOX_FONTSTYLE_MONO_STYLE);
-        }
-        if (inFontStyle === EDITBOX_FONTSTYLE_PROPORTIONAL) {
-            $('#' + EDITBOX_ID).removeClass(
-                EDITBOX_FONTSTYLE_MONO_STYLE).addClass(
-                EDITBOX_FONTSTYLE_PROPORTIONAL_STYLE);
-        }
-        foswiki.Edit.fontStyle = inFontStyle;
-        foswiki.Pref.setPref(EDITBOX_PREF_FONTSTYLE_ID, inFontStyle);
-    };
-            
-    /**
-     * Changes the height of the editbox textarea.
-     * param inDirection : -1 (decrease) or 1 (increase).
-     * If the new height is smaller than EDITBOX_MIN_ROWCOUNT the height
-     *  will become EDITBOX_MIN_ROWCOUNT.
-     * Each change is written to a cookie.
-     */
-    foswiki.Edit.changeEditBox = function(inDirection) {
-        var rowCount = $('#' +  EDITBOX_ID).attr('rows');
-        rowCount += (inDirection * EDITBOX_CHANGE_STEP_SIZE);
-        rowCount = (rowCount < EDITBOX_MIN_ROWCOUNT)
-        ? EDITBOX_MIN_ROWCOUNT : rowCount;
-        $('#' + EDITBOX_ID).attr('rows', rowCount);
-                foswiki.Pref.setPref(EDITBOX_PREF_ROWS_ID, rowCount);
-        return false;
     };
 
     foswiki.Edit.validateMandatoryFields = function() {
@@ -150,16 +77,6 @@ var EDITBOX_FONTSTYLE_PROPORTIONAL_STYLE = "foswikiEditboxStyleProportional";
 		} catch (er) {
 			//
 		}
-		
-		var prefRowsId = foswiki.Pref.getPref(EDITBOX_PREF_ROWS_ID);
-		if (prefRowsId) {
-			$('#' + EDITBOX_ID).attr('rows', parseInt(prefRowsId, 10) );
-		}
-		
-		// Set the font style (monospace or proportional space) of the edit
-		// box to the style read from cookie.
-		var prefStyle  = foswiki.Edit.getFontStyle();
-		foswiki.Edit.setFontStyle(prefStyle);
 
 		$(document.forms[name='main']).submit(function(e) {
 			return foswiki.Edit.validateMandatoryFields();
