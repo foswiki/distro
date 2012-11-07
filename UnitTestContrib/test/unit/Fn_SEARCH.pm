b9b675fff63cf9ca2df15f11566f6fa021696ff4
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-11-07 01:26:55 +0000

    Item12166: protect (a little) agains invalid regex's in non-regex search
    git-svn-id: http://svn.foswiki.org/trunk@15926 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- UnitTestContrib/test/unit/Fn_SEARCH.pm
+++ UnitTestContrib/test/unit/Fn_SEARCH.pm
@@ -6743,4 +6743,36 @@ THERE
     return;
 }
 
+#Item12166
+sub verify_search_for_star {
+    my $this = shift;
+
+    my $search = <<'HERE';
+%SEARCH{ 
+    "*" 
+    type="word" 
+    scope="all" 
+    web="all"
+    recurse="" 
+    excludetopic="" 
+    nosearch="" 
+    casesensitive="" 
+    nosummary="" 
+    nototal="" 
+    order="" 
+    reverse="" 
+    pager="on" 
+    limit="50" 
+    pagesize="50" 
+    zeroresults="on" 
+}%
+HERE
+    my $result = $this->{test_topicObject}->expandMacros($search);
+
+    # Should get the default search order (or an error message, perhaps?)
+    $this->assert_matches( 'Number of topics: <span>4</span>', $result );
+
+    return;
+}
+
 1;
