14e3e628f50c3247edba059fa31ae94e98852ae3
TimotheLitt <TimotheLitt@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-11-08 23:53:17 +0000

    Item12231: SELECTCLASS leaves open file descriptors
    git-svn-id: http://svn.foswiki.org/trunk@15961 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/Types/SELECTCLASS.pm
+++ core/lib/Foswiki/Configure/Types/SELECTCLASS.pm
@@ -56,6 +56,7 @@ sub findClasses {
                     #next if ($subplace =~ /^\..*/);
                     push( @newplaces, $place . '/' . $1 );
                 }
+                closedir DIR;
             }
         }
         $places = \@newplaces;
@@ -77,6 +78,7 @@ sub findClasses {
                 push( @list, $1 ) unless $known{$1};
                 $known{$1} = 1;
             }
+            closedir DIR;
         }
     }
 
