b644eecec505bf46988db79425ba742b95a6c162
TimotheLitt <TimotheLitt@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-12-07 21:24:09 +0000

    Item12180: Missing a return value for CSR
    git-svn-id: http://svn.foswiki.org/trunk@16167 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/Checkers/WebMasterName.pm
+++ core/lib/Foswiki/Configure/Checkers/WebMasterName.pm
@@ -313,8 +313,10 @@ $keypass
     print $f $output;
     close $f or return ( 0, $this->ERROR("Close failed saving CSR: $!") );
 
-    return ( 1,
-"Your private key has been created.  Your certificate signing request is displayed below.  Please transmit it to your CA, then proceed to the Install button.  Do NOT click either action button again, as it will over-write the private key, rendering the CSR useless.<pre>$output</pre>"
+    return (
+        1,
+"Your private key has been created.  Your certificate signing request is displayed below.  Please transmit it to your CA, then proceed to the Install button.  Do NOT click either action button again, as it will over-write the private key, rendering the CSR useless.<pre>$output</pre>",
+        $keypass,
     );
 }
 
