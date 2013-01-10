4cfb09f569a80c31b7557b746adecbdc6cc90193
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-01-10 18:26:04 +0000

    Item000: BUILD Foswiki-1.1.7_004-RC3 at Thu Jan 10 17:33:56 2013 GMT
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@16363 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -168,8 +168,8 @@ BEGIN {
 
     # DO NOT CHANGE THE FORMAT OF $VERSION.
     # Use $RELEASE for a descriptive version.
-    use version 0.77; $VERSION = version->declare('v1.1.7_003');
-    $RELEASE = 'Foswiki-1.1.7_003-RC2';
+    use version 0.77; $VERSION = version->declare('v1.1.7_004');
+    $RELEASE = 'Foswiki-1.1.7_004-RC3';
 
     # Default handlers for different %TAGS%
     # Where an entry is set as 'undef', the tag will be demand-loaded
