4bb030f740a21fd99897196e0a5b89acb7f5aac4
TimotheLitt <TimotheLitt@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-01-19 02:07:12 +0000

    Item12180: Support SELECT multiple - will be needed for logfile viewer V2
    git-svn-id: http://svn.foswiki.org/trunk@16410 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/Types/SELECT.pm
+++ core/lib/Foswiki/Configure/Types/SELECT.pm
@@ -8,35 +8,122 @@ use warnings;
 use Foswiki::Configure::Type ();
 our @ISA = ('Foswiki::Configure::Type');
 
+# Require query when input is from CGI
+
+sub new {
+    my $class = shift;
+
+    my $this = $class->SUPER::new(@_);
+
+    $this->{NeedsQuery} = 1;
+    return $this;
+}
+
+# !mult in options enables select-multiple
+# !mult:n sets size (number of lines displayed, default = 5, min = 2)
+#
+# For compatibility, the value of a multiple select is scalar unless
+# more than one item is actually selected, when it becomes an arrayref.
+#
+# Consumers of !mult items must handle both cases.
+
 sub prompt {
     my ( $this, $id, $opts, $value, $class ) = @_;
+
+    my $mult = $opts =~ s/\s*!mult(?::(\d+))?\b//;
+    my $size = ( $1 && $1 > 2 ) ? $1 : $mult ? 5 : 1;
+
     $opts =~ s/^\s+//;
     $opts =~ s/\s+$//;
     my $sopts = '';
+
+    my %sel =
+      ref $value ? map { $_ => 1 }
+      @$value : defined $value ? ( $value => 1 ) : ();
+
     foreach my $opt ( split( /,\s*/, $opts ) ) {
-        if ( !defined($value) || defined($value) && $opt ne $value ) {
+        if ( $sel{$opt} ) {
+            $sopts .= '<option selected="selected">' . $opt . '</option>';
+        }
+        else {
             $sopts .= '<option>' . $opt . '</option>';
         }
-        elsif ( defined($value) && $opt eq $value ) {
-            $sopts .= '<option selected="selected">' . $value . '</option>';
-        }
     }
+    my @mult;
+    @mult = ( multiple => 'multiple' ) if ($mult);
+
     return CGI::Select(
         {
             name     => $id,
-            size     => 1,
+            size     => $size,
             class    => "foswikiSelect $class",
             onchange => 'valueChanged(this)',
+            @mult,
         },
         $sopts
     );
+}
+
+# Item save:
+#
+# Convert ref to log value when multiple items are selected
+
+sub value2string {
+    my $this = shift;
+    my ( $keys, $value, $log ) = @_;
+
+    if ( ref $value && defined $log ) {
+        $_[2] = join( ',', sort @$value );
+    }
+    return $this->SUPER::value2string(@_);
+}
+
+# Convert CGI value for %cfg
+#
+# Compatibility requires that single no no select be stored as a
+# scalar.  Only multiple selects are stored as refs.
+
+sub string2value {
+    my $this = shift;
+    my ( $query, $keys ) = @_;
+
+    my @values = $query->param($keys);
+
+    if ( @values <= 1 ) {
+        return $values[0];
+    }
+    return [@values];
+}
+
+# Compare two values for equality
+
+sub equals {
+    my $this = shift;
+    my ( $val, $def ) = @_;
+
+    if ( !defined $val ) {
+        return 0 if defined $def;
+        return 1;
+    }
+    elsif ( !defined $def ) {
+        return 0;
+    }
+
+    # Convert all values to multiple style
+
+    $val = [$val] unless ( ref $val );
+    $def = [$def] unless ( ref $def );
+    $val = join( ',', sort @$val );
+    $def = join( ',', sort @$def );
+
+    return $val eq $def;
 }
 
 1;
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2008-2010 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2008-2013 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
