4d6d19cc5eb6331404912bfc1257b2748be73cb1
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-02-11 05:03:57 +0000

    Item12389: initial 1.1.7 update work
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@16526 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/tools/pkg/fosiki/build_deb.sh
+++ core/tools/pkg/fosiki/build_deb.sh
@@ -4,21 +4,21 @@ if [ -e /tmp/build_deb ]; then
 	echo '/tmp/build_deb already exists, please move aside'
 	exit -1;
 fi
-if [ ! -e Foswiki-1.1.6.tgz ]; then
-	echo 'need Foswiki-1.1.6.tgz file to build'
+if [ ! -e Foswiki-1.1.7.tgz ]; then
+	echo 'need Foswiki-1.1.7.tgz file to build'
 	exit -1;
 fi
 
 mkdir /tmp/build_deb
 cp -r debian /tmp/build_deb/
-cp Foswiki-1.1.6.tgz /tmp/build_deb/foswiki_1.1.6.orig.tar.gz
+cp Foswiki-1.1.7.tgz /tmp/build_deb/foswiki_1.1.7.orig.tar.gz
 
 cd /tmp/build_deb
-tar zxvf /tmp/build_deb/foswiki_1.1.6.orig.tar.gz
+tar zxvf /tmp/build_deb/foswiki_1.1.7.orig.tar.gz
 
 #add * to allow for -beta, -auto etc
-mv /tmp/build_deb/Foswiki-1.1.6*/ /tmp/build_deb/foswiki-1.1.6/
-cd /tmp/build_deb/foswiki-1.1.6
+mv /tmp/build_deb/Foswiki-1.1.7*/ /tmp/build_deb/foswiki-1.1.7/
+cd /tmp/build_deb/foswiki-1.1.7
 
 mv ../debian .
 
@@ -38,3 +38,13 @@ dpkg-buildpackage -rfakeroot
 #echo ================== upload into experimental debian repos
 #cd /data/home/sven/src/wikiring/projects/Foswiki/FoswikiInstaller/experimental/
 #reprepro --component main --priority normal  --section web --ignore=wrongdistribution   includedeb experimental /tmp/build_deb/*.deb
+
+##############################
+# CREATING PATCHES
+# 1. run a build_deb.sh
+# 2. cd /tmp/build_deb/foswiki-1.1.7
+# 3. run dpatch-edit-patch your-new-patchname
+# 4. once its at a shell, make your change and type 'exit'
+# 4a. if you want to discard, 'exit 260' will work
+# 5. copy the generated dpatch file to the debian/patches dir and add to 00list
+# 6. run a new build_deb.sh :) DONE.
