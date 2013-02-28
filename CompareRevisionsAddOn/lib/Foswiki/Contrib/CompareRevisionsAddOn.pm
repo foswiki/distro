4b3fe9e14512ee0f2bbd98020881b7f11bb2a112
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-02-28 01:59:18 +0000

    Item12399: Forgot to bump revision of CompareRevisionsAddOn
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@16572 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- CompareRevisionsAddOn/lib/Foswiki/Contrib/CompareRevisionsAddOn.pm
+++ CompareRevisionsAddOn/lib/Foswiki/Contrib/CompareRevisionsAddOn.pm
@@ -2,12 +2,12 @@ package Foswiki::Contrib::CompareRevisionsAddOn;
 use strict;
 
 # Also update CompareRevisionsAddOnPlugin
-use version; our $VERSION = version->declare("v1.1.9");
+use version; our $VERSION = version->declare("v1.1.10");
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.  Keep in sync with $VERSION
-our $RELEASE = '1.1.9';
+our $RELEASE = '1.1.10';
 
 our $SHORTDESCRIPTION =
 'Compares two revisions of a document. In contrast to normal _rdiff_, the comparison is done with the rendered HTML output.';
