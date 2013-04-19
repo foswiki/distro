49e707c7e320f35fdf65767876cb0277d1e45d4a
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-04-19 13:23:23 +0000

    Item12446: Use local regex for better compatibility
    Extracted $version::LAX components from version 0.96.
    FlorianSchlichting unit tests note that these were added in 0.78, and
    the minimum version we require is 0.77.
    
    git-svn-id: http://svn.foswiki.org/trunk@16673 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Configure/Dependency.pm
+++ core/lib/Foswiki/Configure/Dependency.pm
@@ -43,6 +43,77 @@ my %STRINGOPMAP = (
 
 my $MAXINT = 0x7FFFFFFF;
 
+#--------------------------------------------------------------------------#
+# LAX Version regexp components TAKEN FROM VERSION 0.96
+#  - version 0.77 requried for core doesn't have the regex
+#  SMELL: Replace this with $version::LAX once version 0.78 or > is required
+#--------------------------------------------------------------------------#
+
+# Fraction part of a decimal version number.  This is a common part of
+# both strict and lax decimal versions
+
+my $FRACTION_PART = qr/\.[0-9]+/;
+
+# First part of either decimal or dotted-decimal lax version number.
+# Unsigned integer, but allowing leading zeros.  Always interpreted
+# as decimal.  However, some forms of the resulting syntax give odd
+# results if used as ordinary Perl expressions, due to how perl treats
+# octals.  E.g.
+#   version->new("010" ) == 10
+#   version->new( 010  ) == 8
+#   version->new( 010.2) == 82  # "8" . "2"
+
+my $LAX_INTEGER_PART = qr/[0-9]+/;
+
+# Second and subsequent part of a lax dotted-decimal version number.
+# Leading zeroes are permitted, and the number is always decimal.  No
+# limit on the numerical value or number of digits, so there is the
+# possibility of overflow when converting to decimal form.
+
+my $LAX_DOTTED_DECIMAL_PART = qr/\.[0-9]+/;
+
+# Alpha suffix part of lax version number syntax.  Acts like a
+# dotted-decimal part.
+
+my $LAX_ALPHA_PART = qr/_[0-9]+/;
+
+#--------------------------------------------------------------------------#
+# Lax version regexp definitions
+#--------------------------------------------------------------------------#
+
+# Lax decimal version number.  Just like the strict one except for
+# allowing an alpha suffix or allowing a leading or trailing
+# decimal-point
+
+my $LAX_DECIMAL_VERSION =
+  qr/ $LAX_INTEGER_PART (?: \. | $FRACTION_PART $LAX_ALPHA_PART? )?
+	|
+	$FRACTION_PART $LAX_ALPHA_PART?
+    /x;
+
+# Lax dotted-decimal version number.  Distinguished by having either
+# leading "v" or at least three non-alpha parts.  Alpha part is only
+# permitted if there are at least two non-alpha parts. Strangely
+# enough, without the leading "v", Perl takes .1.2 to mean v0.1.2,
+# so when there is no "v", the leading part is optional
+
+my $LAX_DOTTED_DECIMAL_VERSION = qr/
+	v $LAX_INTEGER_PART (?: $LAX_DOTTED_DECIMAL_PART+ $LAX_ALPHA_PART? )?
+	|
+	$LAX_INTEGER_PART? $LAX_DOTTED_DECIMAL_PART{2,} $LAX_ALPHA_PART?
+    /x;
+
+# Complete lax version number syntax -- should generally be used
+# anchored: qr/ \A $LAX \z /x
+#
+# REMOVED:
+# The string 'undef' is a special case to make for easier handling
+# of return values from ExtUtils::MM->parse_version
+
+my $LAX = qr/ $LAX_DECIMAL_VERSION | $LAX_DOTTED_DECIMAL_VERSION /x;
+
+#--------------------------------------------------------------------------#
+
 =begin TML
 
 ---++ ClassMethod new( %opts ) 
@@ -360,7 +431,7 @@ s/(\d+)$separator($MNAME)$separator(\d+)/$3.$separator.$M2N{ lc($2) }.$separator
     $b = lc($b);
 
 # See if these are sane perl version strings,  if so we can use CPAN version to compare
-    if ( $a =~ /^$version::LAX$/ && $b =~ /^$version::LAX$/ ) {
+    if ( $a =~ /^$LAX$/ && $b =~ /^$LAX$/ ) {
 
 #print STDERR "$a and $b match LAX version rules, TEST $op ";
 #print STDERR ( compare_using_cpan_version( $a, $op, $b )) ? " - TRUE\n" : " - FALSE \n";
