469e36ddfe065137713b72762e0c69b72f633ed7
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-05-10 18:51:02 +0000

    Item12493: remove deadwood; add tests to JQuery plugins
    git-svn-id: http://svn.foswiki.org/trunk@16715 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ EmptyJQueryPlugin/test/unit/EmptyJQueryPlugin/EmptyJQueryPluginSuite.pm
@@ -0,0 +1,32 @@
+# See bottom of file for license and copyright information
+package EmptyJQueryPluginSuite;
+
+use strict;
+use warnings;
+
+use Unit::TestSuite;
+our @ISA = 'Unit::TestSuite';
+
+sub name { 'EmptyJQueryPluginSuite' }
+
+sub include_tests { qw(EmptyJQueryPluginTests) }
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-%$CREATEDYEAR% Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
---
+++ EmptyJQueryPlugin/test/unit/EmptyJQueryPlugin/EmptyJQueryPluginTests.pm
@@ -0,0 +1,58 @@
+# See bottom of file for license and copyright information
+use strict;
+use warnings;
+
+package EmptyJQueryPluginTests;
+
+use FoswikiTestCase;
+our @ISA = qw( FoswikiTestCase );
+
+use strict;
+use warnings;
+use Foswiki;
+use CGI;
+
+my $foswiki;
+
+sub new {
+    my $self = shift()->SUPER::new(@_);
+    return $self;
+}
+
+# Set up the test fixture
+sub set_up {
+    my $this = shift;
+
+    $this->SUPER::set_up();
+
+    $Foswiki::Plugins::SESSION = $foswiki;
+}
+
+sub tear_down {
+    my $this = shift;
+    $this->SUPER::tear_down();
+}
+
+sub test_self {
+    my $this = shift;
+}
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-%$CREATEDYEAR% Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
