7fb9accd07aada457e2982fd87b36a0aad2bd448
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-10-12 08:47:05 +0000

    Item12577: deprecate jquery-tooltip in favour of jquery-ui-tooltip
    git-svn-id: http://svn.foswiki.org/trunk@16900 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ JQueryPlugin/lib/Foswiki/Configure/Checkers/JQueryPlugin/Plugins/Tooltip/Enabled.pm
@@ -0,0 +1,42 @@
+# See bottom of file for license and copyright information
+package Foswiki::Configure::Checkers::JQueryPlugin::Plugins::Tooltip::Enabled;
+
+use warnings;
+use strict;
+
+use Foswiki::Configure::Checker ();
+our @ISA = qw( Foswiki::Configure::Checker );
+
+sub check {
+    my $this = shift;
+    my $warnings;
+
+    if ( $Foswiki::cfg{JQueryPlugin}{Plugins}{Tooltip}{Enabled} ) {
+        $warnings .= $this->WARN(<<'HERE');
+Tooltip has been deprecated.  Please use the tooltip plugin part of the jQuery-ui package. 
+The new plugin is not backwards compatible. See the JQueryPlugin documentation for further details.
+HERE
+    }
+
+    return $warnings;
+}
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2013 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
