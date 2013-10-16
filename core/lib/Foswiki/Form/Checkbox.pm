1e28851c6cc36d83181c6af8eb92bf7a06f65eea
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-10-16 07:39:39 +0000

    Item12064: improved html
    git-svn-id: http://svn.foswiki.org/branches/Release01x01@16928 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- core/lib/Foswiki/Form/Checkbox.pm
+++ core/lib/Foswiki/Form/Checkbox.pm
@@ -92,7 +92,7 @@ sub renderForEdit {
     my $session = $this->{session};
     my $extra   = '';
     if ( $this->{type} =~ m/\+buttons/ ) {
-        $extra = CGI::br();
+        $extra = "<div class='foswikiButtonBox'>";
         $extra .= CGI::button(
             -class   => 'foswikiButton',
             -value   => $session->i18n->maketext('Set all'),
@@ -100,7 +100,6 @@ sub renderForEdit {
               . $this->{name}
               . ']").prop("checked", true);',
         );
-        $extra .= '&nbsp;';
         $extra .= CGI::button(
             -class   => 'foswikiButton',
             -value   => $session->i18n->maketext('Clear all'),
@@ -108,6 +107,7 @@ sub renderForEdit {
               . $this->{name}
               . ']").prop("checked", true);',
         );
+        $extra .= "</div>";
     }
     $value = '' unless defined($value) && length($value);
     my %isSelected = map { $_ => 1 } split( /\s*,\s*/, $value );
