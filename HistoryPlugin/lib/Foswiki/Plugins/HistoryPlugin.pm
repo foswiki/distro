ca7a1c59eb6b4905ef926ba13e9a48401fb4f60b
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-10-24 04:10:39 +0000

    Item12596: Sync HistoryPlugin from release11
    The Unit Tests are NOT in sync, because trunk renders anchors
    differently.  The rest of the changes are just revision info and
    copyrights.
    
    git-svn-id: http://svn.foswiki.org/trunk@16962 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- HistoryPlugin/lib/Foswiki/Plugins/HistoryPlugin.pm
+++ HistoryPlugin/lib/Foswiki/Plugins/HistoryPlugin.pm
@@ -9,9 +9,9 @@ use Error qw(:try);
 use Foswiki::AccessControlException ();
 
 # =========================
-# Simple decimal version, use parse method, no leading "v"
-use version; our $VERSION = version->parse("1.9");
-our $RELEASE           = '1.9';
+#   Simple decimal version,  no leading "v"
+our $VERSION           = "1.11";
+our $RELEASE           = '1.11';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION  = 'Shows a complete history of a topic';
 
@@ -248,7 +248,7 @@ sub _handleHeadFoot {
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2008-2011 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2008-2012 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
