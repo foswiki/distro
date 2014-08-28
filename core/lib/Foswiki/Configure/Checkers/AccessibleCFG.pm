6904c1518451a89ffdfff006404dd8a5c2244bed
crawford <git@c-dot.co.uk>
2014-08-28 10:46:44 +0100

    Item12952: new checker

---
+++ core/lib/Foswiki/Configure/Checkers/AccessibleCFG.pm
@@ -0,0 +1,48 @@
+# See bottom of file for license and copyright information
+package Foswiki::Configure::Checkers::AccessibleCFG;
+
+use strict;
+use warnings;
+
+use Foswiki::Configure::Checkers::PERL ();
+our @ISA = ('Foswiki::Configure::Checkers::PERL');
+
+sub check_current_value {
+    my ( $this, $reporter ) = @_;
+
+    my $val = $this->getCfg();
+    unless ( ref($val) eq 'ARRAY' ) {
+        $reporter->ERROR("Was expecting this to be an array");
+        return;
+    }
+    my $ec = 0;
+    foreach my $v (@$val) {
+        if ( ref($v) ne 'SCALAR' ) {
+            $reporter->ERROR("Was expecting entry $ec to be a scalar");
+        }
+        if ( $v !~ /^({\w+})+$/ ) {
+            $reporter->ERROR("Was expecting '$v' to be a cfg key");
+        }
+        $ec++;
+    }
+}
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2014 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
