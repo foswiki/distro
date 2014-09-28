a53c13e7a785a80946c26407d71179a2b43a7389
George Clark <geonwiki@fenachrone.com>
2014-09-28 11:25:04 -0400

    Item13041: Improve Htpasswd checker
    No need to report errors if the password file doesn't contain passwords
    with a possible encoding.

--- core/lib/Foswiki/Configure/Checkers/Htpasswd/Encoding.pm
+++ core/lib/Foswiki/Configure/Checkers/Htpasswd/Encoding.pm
@@ -10,47 +10,84 @@ our @ISA = ('Foswiki::Configure::Checker');
 use Foswiki::Configure::Dependency ();
 
 my %methods = (
-    'htdigest-md5' => [ 
-        { name => 'Digest::MD5',
-          usage => 'use or auto-detection of htdigest-md5' } ],
+    'htdigest-md5' => [
+        {
+            name   => 'Digest::MD5',
+            search => ',',
+            usage  => 'use or auto-detection of htdigest-md5'
+        }
+    ],
     'sha1' => [
-        { name => 'Digest::SHA',
-          usage => 'use or auto-detection of sha1' } ],
+        {
+            name   => 'Digest::SHA',
+            search => ':{SHA}',
+            usage  => 'use or auto-detection of sha1'
+        }
+    ],
     'apache-md5' => [
-        { name => 'Crypt::PasswdMD5',
-          usage => 'use or auto-detection of apache-md5' } ],
+        {
+            name   => 'Crypt::PasswdMD5',
+            search => ':$apr1$',
+            usage  => 'use or auto-detection of apache-md5'
+        }
+    ],
     'crypt-md5' => [
-        { name => 'Crypt::PasswdMD5',
-          usage => 'use or auto-detection of crypt-md5' } ],
+        {
+            name   => 'Crypt::PasswdMD5',
+            search => ',',
+            usage  => 'use or auto-detection of crypt-md5'
+        }
+    ],
     'bcrypt' => [
-        { name => 'Crypt::Eksblowfish::Bcrypt',
-          usage => 'use or auto-detection of bcrypt' } ]
-    );
- 
+        {
+            name   => 'Crypt::Eksblowfish::Bcrypt',
+            search => ':$2a$',
+            usage  => 'use or auto-detection of bcrypt'
+        }
+    ]
+);
+
 sub check_current_value {
-    my ($this, $reporter) = @_;
-    my $e    = '';
+    my ( $this, $reporter ) = @_;
+    my $e = '';
+
+    return
+      if ( $Foswiki::cfg{PasswordManager} ne 'Foswiki::Users::HtPasswdUser'
+        && $Foswiki::cfg{PasswordManager} ne
+        'Foswiki::Users::ApacheHtpasswdUser' );
+
+    my $passwordFile = $Foswiki::cfg{Htpasswd}{FileName};
+    Foswiki::Configure::Load::expandValue($passwordFile);
+    ($passwordFile) =
+      $passwordFile =~ m/(.*)/;    # Untaint needed to prevent a failure.
+    my $passwords;
+
+    if ( -e $passwordFile && -r $passwordFile ) {
+        open( my $IN_FILE, '<', $passwordFile );
+        local $/;
+        $passwords = <$IN_FILE>;
+        close $IN_FILE;
+    }
 
     if ( $Foswiki::cfg{Htpasswd}{Encoding} eq 'md5' ) {
         $Foswiki::cfg{Htpasswd}{Encoding} = 'htdigest-md5';
-        $reporter->WARN(
-            Foswiki::Configure::Checker::GUESSED_MESSAGE .
-            'Encoding has been changed from the deprecated =md5= setting to =htdigest-md5=.  Please save your configuration. Note that this does not change how your passwords are stored.'
-          );
+        $reporter->WARN( Foswiki::Configure::Checker::GUESSED_MESSAGE
+              . 'Encoding has been changed from the deprecated =md5= setting to =htdigest-md5=.  Please save your configuration. Note that this does not change how your passwords are stored.'
+        );
     }
 
     my $enc = $Foswiki::cfg{Htpasswd}{Encoding};
     if ( $Foswiki::cfg{Htpasswd}{AutoDetect} || $enc eq 'crypt' ) {
-        my $f = $Foswiki::cfg{Htpasswd}{FileName};
-        Foswiki::Configure::Load::expandValue($f);
+        my $passwordFile = $Foswiki::cfg{Htpasswd}{FileName};
+        Foswiki::Configure::Load::expandValue($passwordFile);
 
         if ( $enc eq 'crypt' ) {
-            if ( $Foswiki::cfg{Htpasswd}{AutoDetect} && -f $f ) {
+            if ( $Foswiki::cfg{Htpasswd}{AutoDetect} && -f $passwordFile ) {
                 $reporter->WARN(
 '<b>Not recommended</b> crypt encoding only uses the first 8 characters of the password and silently ignores the rest.  ={AutoDetect}= is enabled.  Consider changing this to stronger encoding. Passwords will migrate to the new encoding as users change their passwords.'
                 );
             }
-            elsif ( -f $f ) {
+            elsif ( -f $passwordFile ) {
                 $reporter->WARN(
 '<b>Not Recommended:</b> crypt encoding only uses the first 8 characters of the password and silently ignores the rest.  However changing Encoding will invalidate existing passwords unless =AutoDetect= is enabled. See <a href="http://foswiki.org/Support/HtPasswdEncodingSupplement">HtPasswdEncodingSupplement</a> for more information'
                 );
@@ -64,20 +101,36 @@ sub check_current_value {
     }
 
     my @check;
-    if ($Foswiki::cfg{Htpasswd}{AutoDetect}) {
-        foreach my $method (sort keys %methods) {
-            push(@check, @{$methods{$method}});
+    if ( $Foswiki::cfg{Htpasswd}{AutoDetect} ) {
+        foreach my $method ( sort keys %methods ) {
+            push( @check, @{ $methods{$method} } );
+            $check[-1]->{REQ} = 1
+              if ( $Foswiki::cfg{Htpasswd}{Encoding}
+                && $Foswiki::cfg{Htpasswd}{Encoding} eq $method );
         }
-    } elsif ($Foswiki::cfg{Htpasswd}{Encoding} &&
-             $methods{$Foswiki::cfg{Htpasswd}{Encoding}}) {
-        push(@check, @{$methods{$Foswiki::cfg{Htpasswd}{Encoding}}});
     }
-    Foswiki::Configure::Dependency::checkPerlModules( @check );
+    elsif ($Foswiki::cfg{Htpasswd}{Encoding}
+        && $methods{ $Foswiki::cfg{Htpasswd}{Encoding} } )
+    {
+        push( @check, @{ $methods{ $Foswiki::cfg{Htpasswd}{Encoding} } } );
+        $check[-1]->{REQ} = 1;
+    }
+    Foswiki::Configure::Dependency::checkPerlModules(@check);
     foreach my $mod (@check) {
-        if (!$mod->{ok}) {
-            $reporter->ERROR($mod->{check_result});
-        } else {
-            $reporter->NOTE($mod->{check_result});
+        if ( !$mod->{ok} ) {
+            if ( checkEncoding( $passwords, $mod->{search}, $mod->{REQ} ) ) {
+                $reporter->ERROR( $mod->{check_result}
+                      . ": Existing passwords use this encoding, or it's the default encoding."
+                );
+            }
+            else {
+                $reporter->WARN( $mod->{check_result}
+                      . ": No passwords using this encoding were found in $passwordFile ."
+                );
+            }
+        }
+        else {
+            $reporter->NOTE( $mod->{check_result} );
         }
     }
 
@@ -86,14 +139,16 @@ sub check_current_value {
         if ( $Config{myuname} =~ /strawberry/i ) {
             my %mod = (
                 name => 'Crypt::PasswdMD5',
-                usage => 'Required for crypt-md5 encoding on Windows with Strawberry perl',
+                usage =>
+'Required for crypt-md5 encoding on Windows with Strawberry perl',
                 minimumVersion => 0
             );
             Foswiki::Configure::Dependency::checkPerlModules( \%mod );
-            if (!$mod{ok}) {
-                $reporter->ERROR($mod{check_result});
-            } else {
-                $reporter->NOTE($mod{check_result});
+            if ( !$mod{ok} ) {
+                $reporter->ERROR( $mod{check_result} );
+            }
+            else {
+                $reporter->NOTE( $mod{check_result} );
             }
         }
     }
@@ -107,6 +162,17 @@ sub check_current_value {
 "PasswordManager ApacheHtpasswdUser only supports crypt and apache-md5 encryption.  Use HtPasswdUser for other Encoding types."
         );
     }
+}
+
+sub checkEncoding {
+
+    # my ( $passwords, $test, $req ) = @_;
+    return 1 if $_[2];    # Default encoding, so this is required
+    return 0 unless $_[0];    # No password file, so everything is a warning
+    return 1
+      if index( $_[0], $_[1] ) >
+      -1;                     # File contains passwords with this encoding
+    return 0;
 }
 
 1;
