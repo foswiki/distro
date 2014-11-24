f99ea947feb12936ff9741d8a6725dffeb562ca3
MichaelDaum <github@michaeldaumconsulting.com>
2014-11-24 13:52:21 +0100

    Item13109: added sprintf and jquery.mousewheel

---
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/MOUSEWHEEL.pm
@@ -0,0 +1,47 @@
+# See bottom of file for license and copyright information
+
+package Foswiki::Plugins::JQueryPlugin::MOUSEWHEEL;
+use strict;
+use warnings;
+
+use Foswiki::Plugins::JQueryPlugin::Plugin ();
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+sub new {
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
+
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name       => 'MouseWheel',
+            version    => '3.1.12',
+            author     => 'jQuery Foundation and other contributors',
+            homepage   => 'https://github.com/jquery/jquery-mousewheel',
+            javascript => [ 'jquery.mousewheel.js', ],
+        ),
+        $class
+    );
+
+    return $this;
+}
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2014 Michael Daum http://michaeldaumconsulting.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
+
