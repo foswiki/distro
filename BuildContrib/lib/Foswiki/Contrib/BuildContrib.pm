9ac019fba0c4302ca1898e826936e9c65db8c389
George Clark <geonwiki@fenachrone.com>
2014-11-29 21:47:26 -0500

    Item11267: Update BuildContrib docs for git

--- BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
@@ -1,8 +1,8 @@
 package Foswiki::Contrib::BuildContrib;
 use strict;
 
-use version 0.77; our $VERSION = version->declare("v1.8.0");
-our $RELEASE = '13 Feb 2014';
+our $VERSION = '1.81';
+our $RELEASE = '29 Nov 2014';
 
 our $SHORTDESCRIPTION =
 'Automates build and packaging process, including installer generation, for extension modules.';
