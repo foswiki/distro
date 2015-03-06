f8a94ec249a065c63113ce84c6520d2f8109bcd0
MichaelDaum <github@michaeldaumconsulting.com>
2015-03-06 13:34:20 +0100

    Item13292: permissions editor mixes up settings
    ... on second save

--- NatEditPlugin/lib/Foswiki/Plugins/NatEditPlugin.pm
+++ NatEditPlugin/lib/Foswiki/Plugins/NatEditPlugin.pm
@@ -30,8 +30,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '9.01';
-our $RELEASE           = '14 Jan 2015';
+our $VERSION           = '9.02';
+our $RELEASE           = '06 Mar 2015';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION  = 'A Wikiwyg Editor';
 our $baseWeb;
