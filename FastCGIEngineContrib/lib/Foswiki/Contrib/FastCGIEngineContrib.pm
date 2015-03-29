add3af603171c647a35119c8696940e3ada34982
George Clark <geonwiki@fenachrone.com>
2015-03-29 14:37:32 -0400

    Item13342: Don't reinit after every transaction.
    The json requests come in more quickly than the backend can handle when re-init after every transaction.

--- FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
+++ FastCGIEngineContrib/lib/Foswiki/Contrib/FastCGIEngineContrib.pm
@@ -16,8 +16,8 @@ package Foswiki::Contrib::FastCGIEngineContrib;
 use strict;
 use warnings;
 
-our $VERSION          = '1.00';
-our $RELEASE          = '1.00';
+our $VERSION          = '1.01';
+our $RELEASE          = '1.01';
 our $SHORTDESCRIPTION = 'Permits Foswiki to be executed with FastCGI';
 
 1;
