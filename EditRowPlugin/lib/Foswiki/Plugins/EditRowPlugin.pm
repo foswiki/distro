ecb3db5ed3de24d67aa8c688dae93ffb7850bbb9
Comment <git@c-dot.co.uk>
2015-04-11 07:26:44 +0100

    Item13363: minor taint error, only visible on 1.1.9

--- EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin.pm
+++ EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin.pm
@@ -32,8 +32,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '3.300';
-our $RELEASE           = '30 Mar 2015';
+our $VERSION           = '3.310';
+our $RELEASE           = '11 Apr 2015';
 our $SHORTDESCRIPTION  = 'Inline edit for tables';
 our $NO_PREFS_IN_TOPIC = 1;
 
