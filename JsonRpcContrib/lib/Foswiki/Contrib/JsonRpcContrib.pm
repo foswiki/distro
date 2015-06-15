6bd765336b2bc1d84236fb6a9c9385c3a9cca3b6
George Clark <geonwiki@fenachrone.com>
2015-06-14 22:29:35 -0400

    Item9693: Documentation updates

--- JsonRpcContrib/lib/Foswiki/Contrib/JsonRpcContrib.pm
+++ JsonRpcContrib/lib/Foswiki/Contrib/JsonRpcContrib.pm
@@ -1,17 +1,4 @@
-# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-#
-# JsonRpcContrib is Copyright (C) 2011-2015 Michael Daum http://michaeldaumconsulting.com
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details, published at
-# http://www.gnu.org/copyleft/gpl.html
+# See bottom of file for license and copyright information
 
 package Foswiki::Contrib::JsonRpcContrib;
 
@@ -34,8 +21,8 @@ BEGIN {
 
 =cut
 
-our $VERSION           = '2.21';
-our $RELEASE           = '2.21';
+our $VERSION           = '2.22';
+our $RELEASE           = '2.22';
 our $SHORTDESCRIPTION  = 'JSON-RPC interface for Foswiki';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SERVER;
@@ -59,3 +46,18 @@ sub getServer {
 }
 
 1;
+__END__
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# JsonRpcContrib is Copyright (C) 2011-2015 Michael Daum http://michaeldaumconsulting.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
