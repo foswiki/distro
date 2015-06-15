e6f60c698a8fa316acb93c0080bbf4331eb3d3bf
George Clark <geonwiki@fenachrone.com>
2015-06-14 20:41:34 -0400

    Item13423: Perl deprecations,  update copyrights.

--- RenderListPlugin/lib/Foswiki/Plugins/RenderListPlugin.pm
+++ RenderListPlugin/lib/Foswiki/Plugins/RenderListPlugin.pm
@@ -1,22 +1,5 @@
 # See bottom of file for license and copyright information
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
-#
-# Copyright (C) 2001-2007 Peter Thoeny, peter@thoeny.org
-# Copyright (C) 2008 Foswiki Contributors
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# As per the GPL, removal of this notice is prohibited.
-# =========================
-
 # =========================
 package Foswiki::Plugins::RenderListPlugin
   ;    # change the package name and $pluginName!!!
@@ -342,6 +325,7 @@ sub fixImageTag {
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
+Copyright (C) 2001-2007 Peter Thoeny, peter@thoeny.org 
 Copyright (C) 2008-2015 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
