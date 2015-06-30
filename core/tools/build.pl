2c0213c47a47d3d5c8068eaf6418506f6e5699ea
George Clark <geonwiki@fenachrone.com>
2015-06-30 15:38:02 -0400

    Item9693: build.pl building wrong docs

--- core/tools/build.pl
+++ core/tools/build.pl
@@ -443,14 +443,14 @@ sub stage_gendocs {
     print
 `cd $this->{basedir}/bin ; ./view -topic System.ReleaseHistory -skin plain | $this->{basedir}/tools/fix_local_links.pl > $this->{tmpDir}/ReleaseHistory.html`;
     print
-`cd $this->{basedir}/bin ; ./view -topic System.ReleaseNotes01x02 -skin plain | $this->{basedir}/tools/fix_local_links.pl > $this->{tmpDir}/ReleaseNotes01x02.html`;
+`cd $this->{basedir}/bin ; ./view -topic System.ReleaseNotes02x00 -skin plain | $this->{basedir}/tools/fix_local_links.pl > $this->{tmpDir}/ReleaseNotes02x00.html`;
     print
 `cd $this->{basedir}/bin ; ./view -topic System.UpgradeGuide -skin plain | $this->{basedir}/tools/fix_local_links.pl > $this->{tmpDir}/UpgradeGuide.html`;
     print
 `cd $this->{basedir}/bin ; ./view -topic System.InstallationGuide -skin plain | $this->{basedir}/tools/fix_local_links.pl > $this->{tmpDir}/INSTALL.html`;
     $this->filter_txt(
-        "$this->{tmpDir}/ReleaseNotes01x02.html",
-        "$this->{tmpDir}/ReleaseNotes01x02.html"
+        "$this->{tmpDir}/ReleaseNotes02x00.html",
+        "$this->{tmpDir}/ReleaseNotes02x00.html"
     );
     print "Automatic documentation built\n";
 }
