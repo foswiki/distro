69b34ac3f72439a161cf5a51846e505b43f117d5
George Clark <geonwiki@fenachrone.com>
2015-07-07 21:06:10 -0400

    Item13507: Release BuildContrib

--- BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib.pm
@@ -1,8 +1,8 @@
 package Foswiki::Contrib::BuildContrib;
 use strict;
 
-our $VERSION = '1.84';
-our $RELEASE = '11 Mar 2015';
+our $VERSION = '1.85';
+our $RELEASE = '08 Jul 2015';
 
 our $SHORTDESCRIPTION =
 'Automates build and packaging process, including installer generation, for extension modules.';
