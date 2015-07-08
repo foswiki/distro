384cbd1547761826d43031ff0ce1884aa3054410
George Clark <geonwiki@fenachrone.com>
2015-07-08 18:24:34 -0400

    Item13515: Split CpanContrib into a separate repository
    CpanContrib is now in https://github.com/foswiki/CpanContrib

--- CpanContrib/lib/CPAN/lib/Fh.pm
+++
@@ -1,7 +0,0 @@
-# back compatibility package for any code explicitly checking
-# that the filehandle object is a Fh
-package Fh;
-
-$Fh::VERSION = '4.21';
-
-1;
--- CpanContrib/lib/Foswiki/Contrib/CpanContrib/Config.spec
+++
@@ -1,3 +0,0 @@
-# ---+ Extensions
-# ---++ CpanContrib
-1;
