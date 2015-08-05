5628dc613f35fe430c95dbefe48249d32e3a764d
George Clark <geonwiki@fenachrone.com>
2015-08-05 09:19:19 -0400

    Revert "Item13603: Report output from checkers"
    This reverts commit 942193b453d3e6dd1069cb4102de716bd865a2d3.
    
    Fix is not correct.  There are two reporters required.

--- core/lib/Foswiki/Configure/Query.pm
+++ core/lib/Foswiki/Configure/Query.pm
@@ -319,8 +319,7 @@ sub check_current_value {
 
     my @report;
 
-    # If passed a reporter, use it, otherwise create a new one.
-    my $reporter = ($frep) ? $frep : Foswiki::Configure::Reporter->new();
+    my $reporter = Foswiki::Configure::Reporter->new();
 
     # Apply "set" values to $Foswiki::cfg
     eval { _getSetParams( $params, $root, $frep ); };
