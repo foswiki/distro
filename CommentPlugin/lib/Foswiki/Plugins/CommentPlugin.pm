c224fd932ffa12c72bd2fedaac486b96e16a82c5
George Clark <geonwiki@fenachrone.com>
2015-09-09 22:34:10 -0400

    Item13504: Prepare for release

--- CommentPlugin/lib/Foswiki/Plugins/CommentPlugin.pm
+++ CommentPlugin/lib/Foswiki/Plugins/CommentPlugin.pm
@@ -13,8 +13,8 @@ use Foswiki::Func    ();
 use Foswiki::Plugins ();
 
 # Please use major.minor
-our $VERSION = '2.6';
-our $RELEASE = '24 Jul 2015';
+our $VERSION = '2.7';
+our $RELEASE = '10 Sep 2015';
 our $SHORTDESCRIPTION =
   'Quickly post comments to a page without an edit/save cycle';
 our $NO_PREFS_IN_TOPIC = 1;
