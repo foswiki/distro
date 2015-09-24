0d9b34a1bc05d0a47a28b679a2bbfaa244b88f8b
MichaelDaum <daum@michaeldaumconsulting.com>
2015-09-24 09:52:32 +0200

    Item13707: up'ing release number

--- PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
+++ PatternSkin/lib/Foswiki/Contrib/PatternSkin.pm
@@ -3,8 +3,8 @@ package Foswiki::Contrib::PatternSkin;
 use strict;
 use warnings;
 
-our $VERSION = '6.01';
-our $RELEASE = '6.01';
+our $VERSION = '6.02';
+our $RELEASE = '24 Sep 2015';
 our $SHORTDESCRIPTION =
 'Pattern skin provides a CSS based default look and feel - flexible and [[http://www.w3.org/][W3C]] compliant. Its layout and color scheme are designed to provide a nice, clean and productive editing environment';
 
