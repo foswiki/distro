15f87d97ebae951dd29416e5245c16b83cccb797
George Clark <geonwiki@fenachrone.com>
2015-10-04 23:02:38 -0400

    Item13787: Create PatchRelease01x01Contrib
    These are official patches of critical maintenance of Foswiki 1.1.
    Making this part of the "distro" repo, Release01x01 branch.
    
    For the patches,  Item12414, Item12616, Item13775 and Item13777 need to
    be backported to 1.1.0-1.1.7.  Currently only available for 1.1.8/1.1.9

---
+++ PatchRelease01x01Contrib/lib/Foswiki/Contrib/PatchRelease01x01Contrib.pm
@@ -0,0 +1,45 @@
+# See bottom of file for default license and copyright information
+
+=begin TML
+
+---+ package Foswiki::Contrib::PatchRelease01x01Contrib
+
+Install patches on Foswiki.
+
+=cut
+
+package Foswiki::Contrib::PatchRelease01x01Contrib;
+
+# Always use strict to enforce variable scoping
+use strict;
+use warnings;
+
+# Keep in sync with lib/Foswiki/Plugins/PatchRelease01x01ContribPlugin.pm
+our $VERSION = '1.0';
+our $RELEASE = '02 Oct 2015';
+
+our $SHORTDESCRIPTION =
+'Apply critical Extension, Perl and CPAN compatibility fixes onto Release 1.1.x';
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Author: GeorgeClark
+
+Copyright (C) 2008-2014 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
