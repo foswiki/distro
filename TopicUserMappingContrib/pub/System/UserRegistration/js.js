e75d4101952176623bc0dcdab95cb3413bdea8e3
George Clark <geonwiki@fenachrone.com>
2015-11-22 19:21:25 -0500

    Item13864: Restructure the UserRegistration topics.
    Split the include blocks out into System.UserRegistrationParts.
    
    Take advantage of Foswiki 2.0 features:
     - Use %SET macro so that sets can be used in included topics.
     - Use INCLUDE feature that allows a search sequence of topics.
    
    Move the inline CSS into css files.
    Delete the obsolete js.js. It was changed to validate.js

--- TopicUserMappingContrib/pub/System/UserRegistration/js.js
+++
@@ -1,6 +0,0 @@
-function makeWikiName(a, b, c) {
-    $("input[name='Twk1WikiName']").val(
-	foswiki.String.makeCamelCase(
-	    $("input[name='Twk1FirstName']").val(),
-	    $("input[name='Twk1LastName']").val()));
-}
