651eda655e5b75d93d5041d964900ac4bf7a3191
George Clark <geonwiki@fenachrone.com>
2015-11-28 23:45:24 -0500

    Item13870: Update unit test for RCS Checker
     - Add a Reporter and verify the returned note.
     - Also negative test - verify note for bogus command.

--- RCSStoreContrib/test/unit/RCSStoreContrib/RCSConfigureTests.pm
+++ RCSStoreContrib/test/unit/RCSStoreContrib/RCSConfigureTests.pm
@@ -11,20 +11,31 @@ use File::Temp();
 use FindBin;
 use File::Path qw(mkpath rmtree);
 
-use Foswiki::Configure::Checkers::RCSChecker        ();
+use Foswiki::Configure::Checkers::RCSChecker ();
 
 #Item11955
 sub test_checkRCSProgram {
     my ($this) = @_;
     my $checkerObj =
-      Foswiki::Configure::Checkers::RCSChecker->new('Test::Foswiki::Configure::Dummy');
+      Foswiki::Configure::Checkers::RCSChecker->new(
+        'Test::Foswiki::Configure::Dummy');
+    my $reporter = Foswiki::Configure::Reporter->new();
+    $checkerObj->{reporter} = $reporter;
 
     $this->assert( !exists $Foswiki::cfg{RCS}{foo} );
     local $Foswiki::cfg{Store}{Implementation} = 'Foswiki::Store::RcsWrap';
 
     # Don't forget that the cmd is sanitized/untainted...
     local $Foswiki::cfg{RCS}{foo} = 'rcs';
-    $this->assert( !$checkerObj->checkRCSProgram('foo') );
+    $this->assert_matches( qr/^rcs (.*) detected.$/,
+        $checkerObj->checkRCSProgram( 'foo', $reporter ) );
+
+    # Test an invalid command, make sure it fails.
+    local $Foswiki::cfg{RCS}{foo} = 'xxrcs';
+    $this->assert_matches(
+        qr/^RCS command xxrcs failed, may not be installed, or found on path./,
+        $checkerObj->checkRCSProgram( 'foo', $reporter )
+    );
 
     return;
 }
