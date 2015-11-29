78c22f3fad5003608fdbaf372c6ed8db438b805c
George Clark <geonwiki@fenachrone.com>
2015-11-28 21:58:35 -0500

    Item13866: Update script.
     - Move permissions to variables at top.
     - Handle the ,pfv PlainFile structures.

--- core/tools/fix_file_permissions.sh
+++ core/tools/fix_file_permissions.sh
@@ -1,38 +1,71 @@
-#!/bin/sh
+#!/bin/sh # These permissions are set to be permissive enough for Foswiki to run on a wide variety of platforms
+# Note that the bin/configure file directory checkers will report files that are more or less
+# permissive than these settings.  These permissions will work on most systems. On multi-purpose
+# servers, the "world" permissions should be removed, and the corresponding changes made to
+# the configuration variables listed below.
 
-echo "All directories have exec bit for recursive reading"
-find . -type d  -exec chmod -c 755 {} \;
+ROOT=444            # Server root read only
+DIR=755             # Directories need "exec" for directory operations. Matches {Store}{dirPermission}
+TOPICS=644          # Topics: Update by foswiki CGI, read by web server Matches {Store}{filePermission}
+ATTACHMENTS=644     # Attachments:  Same, Matches {Store}{filePermission}.
+RCS=444             # Revision histories are always read only
+EXECUTABLE=555      # Programs called by CGI or Shell
+READONLY=444        # Samples, READMEs, Perl modules, helpers, etc.
+WORKING=644         # workareas, logfiles, etc.
+PASSWORD=640        # .htpasswd Read by web server, writable by Foswiki CGI
+SECURITY=600        # passwords, Configuration. Read/write by Foswiki CGI, nothing else should need them.
+SECURITYRO=440      # Other access control related, not updated by Foswiki.
 
-echo "Everything in root is read only"
-find . -maxdepth 1 -type f  -exec chmod -c 444 {} \;
+echo "Everything in root is read only - $ROOT"
+find . -maxdepth 1 -type f  -exec chmod -c $ROOT {} \;
 
-echo "Files in data & pub writable by server, except for rcs files which are read-only"
-find data -name '*.txt' -type f -exec chmod -c 644 {} \;
-find pub -type f -exec chmod -c 644 {} \;
-find data pub -name '*,v' -type f -exec chmod -c 444 {} \;
+echo
+echo "All directories have exec bit for recursive reading - $DIR"
+find . -type d  -exec chmod -c $DIR {} \;
 
-echo "Everything in data is writable by server."
-find data -maxdepth 1 -type f  -exec chmod -c 644 {} \;
+echo
+echo "Files in data ($TOPICS) & pub ($ATTACHMENTS) writable by server,"
+find data -type f -name "*.txt" -exec chmod -c $TOPICS {} \;
+find data -type f -name "*.lease" -exec chmod -c $TOPICS {} \;
+find data -type f -name ".changes" -exec chmod -c $TOPICS {} \;
+find pub -type f -exec chmod -c $ATTACHMENTS {} \;
 
-echo "bin and tools needs to be executable - with exceptions"
-find bin -type f -exec chmod -c 555 {} \;
-find tools -maxdepth 1 -type f -exec chmod -c 555 {} \;
-echo " ... these are the exceptions:"
-chmod -c 644 bin/LocalLib.cfg.txt
-chmod -c 444 bin/setlib.cfg
-chmod -c 444 tools/extender.pl
+echo
+echo "   except for history files which are read-only ($RCS)"
+find data pub -name '*,v' -type f -exec chmod -c $RCS {} \;
+find data -name "*,pfv" -print0 | xargs -0 -I{{ find {{ -type f -exec chmod 440 {} \;
 
-echo "Everything else is read only"
-find lib -type f -exec chmod -c 444 {} \;
-find locale -type f -exec chmod -c 444 {} \;
-find templates -type f -exec chmod -c 444 {} \;
+echo
+echo "Everything in data top level is writable by server ($TOPICS)."
+find data -maxdepth 1 -type f ! -name .htpasswd  -exec chmod -c $TOPICS {} \;
 
-echo "Working is server writable - with exceptions"
-find working -type f -exec chmod -c 644 {} \;
-find working/configure -type f -exec chmod -c 444 {} \;
-find working -name README -exec chmod -c 444 {} \;
+echo
+echo "bin and tools needs to be executable ($EXECUTABLE) - with exceptions"
+find bin -type f ! -name LocalLib.cfg.txt ! -name setlib.cfg -exec chmod -c $EXECUTABLE {} \;
+find tools -maxdepth 1 -type f ! -name extender.pl -exec chmod -c $EXECUTABLE {} \;
+echo
+echo " ... these are the exceptions: ($READONLY)"
+chmod -c $READONLY bin/LocalLib.cfg.txt
+chmod -c $READONLY bin/setlib.cfg
+chmod -c $READONLY tools/extender.pl
 
-echo "Restrict security related files should not be world readable."
-find . -name .htaccess -exec chmod -c 440 {} \;
-chmod -c 640 data/.htpasswd
-chmod -c 640 lib/LocalSite.cfg 
+echo
+echo "Everything else is read only ($READONLY)"
+find lib -type f ! -name LocalSite.cfg -exec chmod -c $READONLY {} \;
+find locale -type f -exec chmod -c $READONLY {} \;
+find templates -type f -exec chmod -c $READONLY {} \;
+
+echo
+echo "Working is server writable ($WORKING) - with exceptions ($READONLY)"
+find working -type f ! -name README ! -name "cgisess_*" -exec chmod -c $WORKING {} \;
+find working/configure -type f -exec chmod -c $READONLY {} \;
+find working -name README -exec chmod -c $READONLY {} \;
+
+echo
+echo "Security related files should not be world readable - ($SECURITY)."
+find . -name .htaccess -exec chmod -c $SECURITYRO {} \;
+find working -name cgisess_*  -exec chmod -c $SECURITY {} \;
+chmod -c $PASSWORD data/.htpasswd
+chmod -c $SECURITY lib/LocalSite.cfg
+
+echo "Updates completed"
