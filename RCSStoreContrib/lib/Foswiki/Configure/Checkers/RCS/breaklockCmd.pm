6ca72e318a85cd0881d3607c56cd78af95cb6a8c
George Clark <geonwiki@fenachrone.com>
2016-01-17 00:24:39 -0500

    Item13927: Add missing breakLockCmd checker

---
+++ RCSStoreContrib/lib/Foswiki/Configure/Checkers/RCS/breaklockCmd.pm
@@ -0,0 +1,39 @@
+# See bottom of file for license and copyright information
+package Foswiki::Configure::Checkers::RCS::breaklockCmd;
+use Foswiki::Configure::Checkers::RCSChecker ();
+our @ISA = ('Foswiki::Configure::Checkers::RCSChecker');
+
+use strict;
+use warnings;
+
+sub check {
+    my $this = shift;
+    return $this->checkRCSProgram('breaklockCmd');
+}
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2016 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
+
+Copyright (C) 2000-2006 TWiki Contributors. All Rights Reserved.
+TWiki Contributors are listed in the AUTHORS file in the root
+of this distribution. NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
