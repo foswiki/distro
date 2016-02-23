27e4b9824c84da6e3e19ee5667e6321ac8bfc34d
MichaelDaum <daum@michaeldaumconsulting.com>
2016-02-23 12:03:24 +0100

    Item13978: update masonry and
    ... externalize imagesLoaded module

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/IMAGESLOADED.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/IMAGESLOADED.pm
@@ -1,5 +1,5 @@
 # See bottom of file for license and copyright information
-package Foswiki::Plugins::JQueryPlugin::MASONRY;
+package Foswiki::Plugins::JQueryPlugin::IMAGESLOADED;
 use strict;
 use warnings;
 
@@ -8,9 +8,9 @@ our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 =begin TML
 
----+ package Foswiki::Plugins::JQueryPlugin::MASONRY
+---+ package Foswiki::Plugins::JQueryPlugin::IMAGESLOADED
 
-This is the perl stub for the jquery.masonry plugin.
+This is the perl stub for the imagesloaded plugin.
 
 =cut
 
@@ -27,12 +27,11 @@ sub new {
 
     my $this = bless(
         $class->SUPER::new(
-            name         => 'Masonry',
-            version      => '3.3.2',
-            author       => 'David <nop>DeSandro',
-            homepage     => 'http://masonry.desandro.com',
-            javascript   => ['jquery.masonry.js'],
-            dependencies => [ 'metadata', 'livequery', 'imagesloaded' ],
+            name       => 'ImagesLoaded',
+            version    => '4.0.0',
+            author     => 'David <nop>DeSandro',
+            homepage   => 'http://imagesloaded.desandro.com/',
+            javascript => ['imagesloaded.js'],
         ),
         $class
     );
@@ -45,9 +44,12 @@ sub new {
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2010-2016 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2016 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
+
+Additional copyrights apply to some or all of the code in this
+file as follows:
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
