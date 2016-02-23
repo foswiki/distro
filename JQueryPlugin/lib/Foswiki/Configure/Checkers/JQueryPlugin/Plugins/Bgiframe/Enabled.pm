e580cd1451196714ead28195c907dbcffb405056
MichaelDaum <daum@michaeldaumconsulting.com>
2016-02-23 11:34:01 +0100

    Item13973: deprecate bgiframe and media

---
+++ JQueryPlugin/lib/Foswiki/Configure/Checkers/JQueryPlugin/Plugins/Bgiframe/Enabled.pm
@@ -0,0 +1,41 @@
+# See bottom of file for license and copyright information
+package Foswiki::Configure::Checkers::JQueryPlugin::Plugins::Bgiframe::Enabled;
+
+use warnings;
+use strict;
+
+use Foswiki::Configure::Checker ();
+our @ISA = qw( Foswiki::Configure::Checker );
+
+sub check {
+    my $this = shift;
+    my $warnings;
+
+    if ( $Foswiki::cfg{JQueryPlugin}{Plugins}{Bgiframe}{Enabled} ) {
+        $warnings .= $this->WARN(<<'HERE');
+This plugin is deprecated. 
+HERE
+    }
+
+    return $warnings;
+}
+
+1;
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2016 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
