52421669adcb97d177239027e8989f927f041e44
MichaelDaum <daum@michaeldaumconsulting.com>
2016-02-23 10:51:14 +0100

    Item13970: modernize %BUTTON
       * use css gradients instead of images
       * support fontawesome icons
       * remove cyan, red, green images
       * remove documented but non-functional =onmouseout=, =onmouseover=
       * add align parameter
       * remove custom encoding of inline js
       * added =jqButtonDisabled= class for disabled buttons
       * deprecate metadata: use HTML5 data instead
       * add support for =jqButtonSet= to group buttons together easily

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/button/jquery.button.init.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/button/jquery.button.init.uncompressed.js
@@ -1,7 +1,9 @@
 jQuery(function($) {
+'use strict';
+
   $(".jqButton:not(.jqInitedButton)").livequery(function() {
     var $this = $(this), 
-      opts = $.extend({}, $this.metadata()),
+      opts = $.extend({}, $this.data(), $this.metadata()),
       form;
 
     $this.addClass("jqInitedButton")
@@ -11,47 +13,46 @@ jQuery(function($) {
         $this.removeClass("jqButtonHover");
       });
 
-    // submit button
-    if ($this.is(".jqSubmitButton")) {
+    if ($this.is(".jqButtonDisabled")) {
       $this.on("click", function() {
-        $this.closest("form").submit();
         return false;
       });
-    } 
+    } else {
+      // submit button
+      if ($this.is(".jqSubmitButton")) {
+        $this.on("click", function() {
+          $this.closest("form").submit();
+          return false;
+        });
+      } 
 
-    // save button 
-    else if ($this.is(".jqSaveButton")) {
-      $this.on("click", function() {
-        form = $this.closest("form");
-        if(typeof(foswikiStrikeOne) == "function") {
-          foswikiStrikeOne(form[0]); 
-        }
-        form.submit();
-        return false;
-      });
-    } 
+      // save button 
+      else if ($this.is(".jqSaveButton")) {
+        $this.on("click", function() {
+          form = $this.closest("form");
+          if(typeof(foswikiStrikeOne) == "function") {
+            foswikiStrikeOne(form[0]); 
+          }
+          form.submit();
+          return false;
+        });
+      } 
 
-    // reset button
-    else if ($this.is(".jqResetButton")) {
-      $this.on("click", function() {
-        $this.closest("form").resetForm();
-        return false;
-      });
-    }
+      // reset button
+      else if ($this.is(".jqResetButton")) {
+        $this.on("click", function() {
+          $this.closest("form").resetForm();
+          return false;
+        });
+      }
 
-    // clear button
-    else if ($this.is(".jqClearButton")) {
-      $this.on("click", function() {
-        $this.closest("form").clearForm();
-        return false;
-      });
-    }
-
-    // custom click handler
-    else if (opts.onclick) {
-      $this.on("click", function() {
-        return opts.onclick.call(this);
-      });
+      // clear button
+      else if ($this.is(".jqClearButton")) {
+        $this.on("click", function() {
+          $this.closest("form").clearForm();
+          return false;
+        });
+      }
     }
 
   });
