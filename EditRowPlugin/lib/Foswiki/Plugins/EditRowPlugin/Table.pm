a923f73f46b342c82891dbacec6f81e5aea197e2
George Clark <geonwiki@fenachrone.com>
2016-03-31 23:17:34 -0400

    Item14000: EditRowPlugin delete removes incorrect row.
    Thanks to PhilippHoppen for the fix.

--- EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin/Table.pm
+++ EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin/Table.pm
@@ -679,7 +679,7 @@ sub deleteRowCmd {
     my $row =
         $urps->{erp_row} >= 0
       ? $urps->{erp_row}
-      : $this->totalRows() + $urps->{erp_row} - 1;
+      : $this->totalRows() + $urps->{erp_row};
     return unless $this->deleteRow($row);
 
     return if $urps->{erp_row} < 0;    # full table edit?
