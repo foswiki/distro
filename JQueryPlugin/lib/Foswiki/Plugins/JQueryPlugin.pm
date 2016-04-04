819775b5719edd263b1ae6df1c4b6d09d640bf46
Jan Krüger <krueger@modell-aachen.de>
2016-04-04 15:42:54 +0200

    Item14024: Item14025: update changelogs and versions for extensions

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
@@ -24,8 +24,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '7.01';
-our $RELEASE           = '18 Mar 2016';
+our $VERSION           = '7.02';
+our $RELEASE           = '4 Apr 2016';
 our $SHORTDESCRIPTION  = 'jQuery <nop>JavaScript library for Foswiki';
 our $NO_PREFS_IN_TOPIC = 1;
 
