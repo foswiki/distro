a8c240509d4bac60f0f1c3701de2c9fa65fde2b9
MichaelDaum <daum@michaeldaumconsulting.com>
2016-09-06 14:29:42 +0200

    Item13206: Merged Release02x01

--- JQueryPlugin/pub/System/JQueryPlugin/ui/jquery.ui.tabs.init.uncompressed.js
+++
@@ -1,9 +0,0 @@
-// initializer for the ui-tabs plugin
-jQuery(function($) {
-
-  $(".jqUITabs").livequery(function() {
-    var $this = $(this), opts = $.extend({}, $this.data(), $this.metadata());
-    $this.removeClass("jqUITabs").tabs(opts);
-  });
-
-});
