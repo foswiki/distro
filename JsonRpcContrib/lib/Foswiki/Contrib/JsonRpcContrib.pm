ab59747e0de2160321578fa366d343e357929058
George Clark <geonwiki@fenachrone.com>
2016-11-26 14:44:49 -0500

    Item14204: Update release notes, bump version

--- JsonRpcContrib/lib/Foswiki/Contrib/JsonRpcContrib.pm
+++ JsonRpcContrib/lib/Foswiki/Contrib/JsonRpcContrib.pm
@@ -21,8 +21,8 @@ BEGIN {
 
 =cut
 
-our $VERSION           = '2.26';
-our $RELEASE           = '4 Apr 2016';
+our $VERSION           = '2.27';
+our $RELEASE           = '26 Nov 2016';
 our $SHORTDESCRIPTION  = 'JSON-RPC interface for Foswiki';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SERVER;
