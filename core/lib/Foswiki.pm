e4f2c9d4a8856a45e6e3b000df398679680f8425
George Clark <geonwiki@fenachrone.com>
2017-02-12 17:00:57 -0500

    Item000: Build Foswiki Release 2.1.3

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -211,8 +211,8 @@ BEGIN {
 
     # DO NOT CHANGE THE FORMAT OF $VERSION.
     # Use $RELEASE for a descriptive version.
-    use version 0.77; $VERSION = version->declare('v2.1.2_003');
-    $RELEASE = 'Foswiki-2.1.3-RC1';
+    use version 0.77; $VERSION = version->declare('v2.1.3');
+    $RELEASE = 'Foswiki-2.1.3';
 
     # Default handlers for different %TAGS%
     # Where an entry is set as 'undef', the tag will be demand-loaded
