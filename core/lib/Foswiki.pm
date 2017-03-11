221942d39f13d0c9da39b4600aafc30834bf28ed
George Clark <geonwiki@fenachrone.com>
2017-03-10 23:33:12 -0500

    Item13883: Update the Foswiki release
    master should be 2.2 alpha, and a numeric version of 2.1.999_nnn

--- core/lib/Foswiki.pm
+++ core/lib/Foswiki.pm
@@ -211,8 +211,8 @@ BEGIN {
 
     # DO NOT CHANGE THE FORMAT OF $VERSION.
     # Use $RELEASE for a descriptive version.
-    use version 0.77; $VERSION = version->declare('v2.1.3');
-    $RELEASE = 'Foswiki-2.1.3';
+    use version 0.77; $VERSION = version->declare('v2.1.999_001');
+    $RELEASE = 'Foswiki-2.2.0 Alpha';
 
     # Default handlers for different %TAGS%
     # Where an entry is set as 'undef', the tag will be demand-loaded
