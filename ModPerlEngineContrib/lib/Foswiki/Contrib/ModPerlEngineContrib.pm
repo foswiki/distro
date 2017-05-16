12bcd28a7b1f242aaf2b276366c0b263c8467c30
George Clark <geonwiki@fenachrone.com>
2017-05-15 20:40:16 -0400

    Item13883: Preparing for release.

--- ModPerlEngineContrib/lib/Foswiki/Contrib/ModPerlEngineContrib.pm
+++ ModPerlEngineContrib/lib/Foswiki/Contrib/ModPerlEngineContrib.pm
@@ -23,8 +23,8 @@ use strict;
 
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
-$VERSION          = '1.05';
-$RELEASE          = '30 Apr 2017';
+$VERSION          = '1.06';
+$RELEASE          = '15 May 2017';
 $SHORTDESCRIPTION = 'Permits Foswiki to be executed under mod_perl';
 
 1;
