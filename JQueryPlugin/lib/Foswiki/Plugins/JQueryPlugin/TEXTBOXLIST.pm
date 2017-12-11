658cf8ea8a84af132bd16624b2c43b3574224ef9
MichaelDaum <daum@michaeldaumconsulting.com>
2017-12-11 13:59:47 +0100

    Item14571: add manual sorting mode to textboxlist

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
@@ -28,7 +28,7 @@ sub new {
     my $this = bless(
         $class->SUPER::new(
             name     => 'TextboxList',
-            version  => '2.11',
+            version  => '2.20',
             author   => 'Michael Daum',
             homepage => 'http://foswiki.org/Extensions/JQueryPlugin',
             css      => ['jquery.textboxlist.css'],
