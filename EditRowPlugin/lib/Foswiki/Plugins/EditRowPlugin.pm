ecbea86679d3e900b7101e9bc5caf9b3d73481c6
MichaelDaum <daum@michaeldaumconsulting.com>
2017-12-12 11:14:10 +0100

    Item14490: fix css for edit table button

--- EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin.pm
+++ EditRowPlugin/lib/Foswiki/Plugins/EditRowPlugin.pm
@@ -32,8 +32,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '3.319';
-our $RELEASE           = '08 Dec 2017';
+our $VERSION           = '3.320';
+our $RELEASE           = '12 Dec 2017';
 our $SHORTDESCRIPTION  = 'Inline edit for tables';
 our $NO_PREFS_IN_TOPIC = 1;
 
