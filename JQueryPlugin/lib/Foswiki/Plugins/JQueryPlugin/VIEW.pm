f6afb02af39c09244a54440cc4c01975cbd8979c
Chris Hoefler <hoeflerb@gmail.com>
2017-12-15 14:21:02 +0000

    Item14454: Merge commit of feature branch Item14454.
    The following commits were squashed:
        Item14454: More docu fixes for jsviews
        commit 3d86c6ac2311dd815311d3925577ec0fc2514dc3
    
        Item14454: Update JQueryPlugin MANIFEST and Config.spec to include jsviews
        commit 16dc8dbcd75eb0ef7a5f1e8fe359d972c7872bd5
    
        Item14454: Fix minor docu error
        commit 7f37405c9cac0404c6ab7fa6ded1a6a47d987667
    
        Item14454: Update jsrender/jsviews version strings in .pm
        commit 27074b159f4325d3e5fa4a3a8c73d4cbb8203476
    
        Item14454: Update jsrender to latest master (v0.9.90)
        commit 562e752540e79fdaa33ec9b69212040870c2ccb3
    
        Item14454: Update jsviews to latest master (v0.9.90)
        commit cfb113cf7fefbc7f8008688a254d0a3b3be1b75c
    
        Item14454: Update jsviews to latest master (v0.9.90)
        commit b27be897ca72835f439cb7c1a4a0513613b25952
    
        Item14454: Add Makefile to update jsviews from git repo
        commit 931187e792b22a21881e04d7a3b41036c634b1bd
    
        Item14454: Merge with branch 'master'
        commit 611e981a276ace0cac4d6a84b7750b39131c1b55
    
        Item14454: Bundle JsViews as an option with JsRender
        commit 23fe3294eb363760755a9cb3a2766081510c72d6
    
        Item14454 (#16)
        commit f23d1f09997fcf8c899f45187577ace75b1596be
    
    Changes to be committed:
        new file:   JQueryPlugin/data/System/JQueryView.txt
        modified:   JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/Config.spec
        modified:   JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/MANIFEST
        modified:   JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/RENDER.pm
        new file:   JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VIEW.pm
        modified:   JQueryPlugin/pub/System/JQueryPlugin/plugins/render/jquery.render.uncompressed.js
        new file:   JQueryPlugin/pub/System/JQueryPlugin/plugins/view/Makefile
        new file:   JQueryPlugin/pub/System/JQueryPlugin/plugins/view/jquery.observable.js
        new file:   JQueryPlugin/pub/System/JQueryPlugin/plugins/view/jquery.observable.uncompressed.js
        new file:   JQueryPlugin/pub/System/JQueryPlugin/plugins/view/jquery.views.js
        new file:   JQueryPlugin/pub/System/JQueryPlugin/plugins/view/jquery.views.uncompressed.js

---
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VIEW.pm
@@ -0,0 +1,52 @@
+# See bottom of file for license and copyright information
+
+package Foswiki::Plugins::JQueryPlugin::VIEW;
+use strict;
+use warnings;
+
+use Foswiki                                ();
+use Foswiki::Plugins::JQueryPlugin::Plugin ();
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+sub new {
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
+
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name         => 'View',
+            version      => '0.9.90',
+            author       => 'Boris Moore',
+            homepage     => 'http://www.jsviews.com',
+            dependencies => ['render'],
+            javascript   => [
+                'jquery.observable.uncompressed.js',
+                'jquery.views.uncompressed.js'
+            ],
+        ),
+        $class
+    );
+
+    return $this;
+}
+
+1;
+
+__END__
+
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2010-2016 Foswiki Contributors. Foswiki Contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
