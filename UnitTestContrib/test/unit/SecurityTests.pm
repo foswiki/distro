b99be21a2b03c0fc1720ab3bc631de27925712eb
Vadim Belman <vrurg@lflat.org>
2018-05-13 21:24:46 -0400

    Item14237: Adapted missed SecurityTests to the v3 test framework

--- UnitTestContrib/test/unit/SecurityTests.pm
+++ UnitTestContrib/test/unit/SecurityTests.pm
@@ -1,51 +1,28 @@
 package SecurityTests;
-use FoswikiFnTestCase();
-use Foswiki::UI::Attach();
+
+#use Foswiki::UI::Attach();
+
+use Foswiki::Class;
+extends qw(FoswikiFnTestCase);
 our @ISA = qw( FoswikiFnTestCase );
 
 # use strict;
 
-my $session;    # Foswiki instance
-
-sub new {
-    my $self = shift()->SUPER::new(@_);
-
-    return $self;
-}
-
-sub set_up {
-    my $this = shift;
-    $this->SUPER::set_up();
-    $session = undef;
-}
-
 sub create_session {
-    my $this       = shift;
-    my $query_opts = shift;
+    my $this    = shift;
+    my $reqOpts = shift;
 
-    # a simple query using attach
-    my $query = new Unit::Request($query_opts);
-    $query->path_info("/$this->{test_web}/$this->{test_topic}");
-    $query->action("attach");
-
-    # Create a Foswiki instance
-    $session =
-      $this->createNewFoswikiSession( $this->{test_user_login}, $query );
-
-    return $session;
-}
-
-sub tear_down {
-    my $this = shift;    # the Test::Unit::TestCase object
-
-    if ($session) {
-
-        # FoswikiFnTestCase does most of this
-        1;
-    }
-
-    # This will automatically restore the state of $Foswiki::cfg
-    $this->SUPER::tear_down();
+    # Create a Foswiki app
+    $this->createNewFoswikiApp(
+        requestParams => { initializer => $reqOpts, },
+        engineParams  => {
+            initialAttributes => {
+                path_info => "/" . $this->test_web . "/" . $this->test_topic,
+                action    => "attach",
+                user      => $this->test_user_login,
+            },
+        }
+    );
 }
 
 sub test_setup {
@@ -56,23 +33,16 @@ sub test_setup {
     my $this = shift;
 
     $this->create_session( { filename => ["goober"] } );
-    my $query = $this->{request};
+    my $query = $this->app->request;
 
-    $this->assert_str_equals( "attach",          $query->action() );
-    $this->assert_str_equals( "filename=goober", $query->queryString() );
+    $this->assert_str_equals( "attach",          $query->action );
+    $this->assert_str_equals( "filename=goober", $query->queryString );
     $this->assert_str_equals( "goober", scalar( $query->param('filename') ) );
 
     # print $query->url(-query => 1), "\n";
 
-    my ( $respText, $result, $stdout, $stderr ) = $this->captureWithKey(
-        attach => sub {
-            no strict 'refs';
-            Foswiki::UI::Attach::attach( $this->{session} );
-            use strict 'refs';
-            $Foswiki::engine->finalize( $this->{session}{response},
-                $this->{session}{request} );
-        }
-    );
+    my ( $respText, $result, $stdout, $stderr ) =
+      $this->captureWithKey( attach => sub { $this->app->handleRequest }, );
 
     # print $respText, "\n";
 
@@ -87,19 +57,12 @@ sub test_attach_filename_xss {
     # send filename="><sCrIpT>alert(66562)</sCrIpT>
     $this->create_session(
         { filename => ['"><sCrIpT>alert(66562)</sCrIpT>'] } );
-    my $query = $this->{request};
+    my $query = $this->app->request;
 
     # print $query->url(-query => 1), "\n";
 
-    my ( $respText, $result, $stdout, $stderr ) = $this->captureWithKey(
-        attach => sub {
-            no strict 'refs';
-            Foswiki::UI::Attach::attach( $this->{session} );
-            use strict 'refs';
-            $Foswiki::engine->finalize( $this->{session}{response},
-                $this->{session}{request} );
-        }
-    );
+    my ( $respText, $result, $stdout, $stderr ) =
+      $this->captureWithKey( attach => sub { $this->app->handleRequest; } );
 
     # print $respText, "\n";
 
