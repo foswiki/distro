01ff952e24cc023b8030bcd994f7817c89c4adce
MichaelDaum <daum@michaeldaumconsulting.com>
2019-11-12 13:17:06 +0100

    Item14873: rewrite and simplify

--- UpdatesPlugin/lib/Foswiki/Plugins/UpdatesPlugin/Core.pm
+++ UpdatesPlugin/lib/Foswiki/Plugins/UpdatesPlugin/Core.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# UpdatesPlugin is Copyright (C) 2011-2014 Foswiki Contributors
+# UpdatesPlugin is Copyright (C) 2011-2019 Foswiki Contributors
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,96 +20,179 @@ use warnings;
 
 use JSON ();
 use Error qw(:try);
-use Foswiki::Plugins::JQueryPlugin ();
+use Foswiki::Plugins ();
 use Foswiki::Configure::Dependency ();    # for _compare_extension_versions()
 
 sub new {
-    my $class   = shift;
-    my $session = shift;
+    my $class = shift;
 
     my $this = bless(
         {
-            session   => $session,
-            debug     => 0,
+            debug => $Foswiki::cfg{Plugins}{UpdatesPlugin}{Debug} || 0,
             reportUrl => $Foswiki::cfg{Plugins}{UpdatesPlugin}{ReportUrl}
               || "https://foswiki.org/Extensions/UpdatesPluginReport",
-            timeout => $Foswiki::cfg{Plugins}{UpdatesPlugin}{CacheTimeout},
+            timeout => $Foswiki::cfg{Plugins}{UpdatesPlugin}{CacheTimeout}
+              // 86400,
             exclude => $Foswiki::cfg{Plugins}{UpdatesPlugin}{ExcludeExtensions}
               || '',
-
+            messageVar => $Foswiki::cfg{Plugins}{UpdatesPlugin}{MessageVariable}
+              || 'BROADCASTMESSAGE',
             @_
         },
         $class
     );
 
     # fix params
-    $this->{timeout} = 86400 unless defined $this->{timeout};
     if ( $this->{exclude} ) {
-        $this->{excludePattern} =
+        $this->{_excludePattern} =
           '^(' . join( '|', split( /\s*,\s*/, $this->{exclude} ) ) . ')$';
 
-#print STDERR "exclude pattern = ".$this->{excludePattern}."\n" if $this->{debug};
+        $this->writeDebug( "exclude pattern = " . $this->{_excludePattern} );
     }
+
+    my $request = Foswiki::Func::getRequestObject();
+    $this->{_refresh} =
+      ( $request->param("refresh") || '' ) =~ /^(on|true|yes|updates)$/ ? 1 : 0;
+
+    Foswiki::Func::readTemplate("updatesplugin");
 
     return $this;
 }
 
-# the plugin can be configured to proxy the request for data. In this case the local server
-# publishes a REST method that responds with the data from f.o - either cached locally or
-# freshly mined.
-sub handleRESTCheck {
-    my ( $this, $plugin, $verb, $response ) = @_;
+sub DESTROY {
+    my $this = shift;
 
-    #print STDERR "called handleRESTCheck()\n";
+    undef $this->{_json};
+    undef $this->{_available};
+    undef $this->{_installed};
+    undef $this->{_outdated};
+    undef $this->{_excludePattern};
+}
 
-    my $availablePlugins;
+sub json {
+    my $this = shift;
+
+    unless ( defined $this->{_json} ) {
+        $this->{_json} = JSON->new->allow_nonref;
+    }
+
+    return $this->{_json};
+}
+
+sub writeDebug {
+    my ( $this, $msg ) = @_;
+
+    return unless $this->{debug};
+
+    #Foswiki::Func::writeDebug("UpdatesPlugin - $msg");
+    print STDERR "UpdatesPlugin - $msg\n";
+}
+
+sub setMessage {
+    my ( $this, $text, $params ) = @_;
+
+    if ($params) {
+        while ( my ( $key, $val ) = each %$params ) {
+            $text =~ s/\$$key\b/$val/g;
+        }
+    }
+
+    $this->writeDebug( $this->{messageVar} . ": " . $text );
+
+    Foswiki::Func::setPreferencesValue( $this->{messageVar}, $text );
+}
+
+sub check {
+    my $this = shift;
+
     my $error;
+    my $outdated;
 
     try {
-        $availablePlugins = $this->getAvailable();
+        $outdated = $this->getOutdated();
     }
     catch Error::Simple with {
         $error = shift;
     };
 
     if ( defined $error ) {
-        printRESTResponse( $response, 500, $error );
+        print STDERR "ERROR: $error\n";
+        $this->setMessage($error);
         return;
     }
 
-#print STDERR "available=".JSON::to_json($availablePlugins, {pretty=>1})."\n" if $this->{debug};
-#print STDERR "installed=".JSON::to_json($this->{_installed}, {pretty=>1})."\n" if $this->{debug};
-
-    my @outdatedPlugins = ();
-
-    foreach my $ext ( @{ $this->{_available} } ) {
-        my $extName   = $ext->{topic};
-        my $installed = $this->{_installed}{$extName};
-
-        my $release = $ext->{release};
-        my $version = $ext->{version} || '';
-
-        my $fakeObj = {
-            installedRelease => $release,
-            installedVersion => $version
-        };
-
-        # SMELL: breaks encapsulation of F::Configure::Dependency
-        my $result =
-          Foswiki::Configure::Dependency::_compare_extension_versions( $fakeObj,
-            ">", $installed );
-        if ($result) {
-            push @outdatedPlugins, $extName;
-            print STDERR
-"UPDATE: $extName: installed: $installed, available release=$release, available version=$version\n"
-              if $this->{debug};
-        }
+    if ( $outdated && @$outdated ) {
+        my $text = Foswiki::Func::expandTemplate("updates::message");
+        $this->setMessage(
+            $text,
+            {
+                nrPlugins       => scalar(@$outdated),
+                outdatedPlugins => join( "&#44; ", sort @$outdated ),
+            }
+        );
     }
 
-    printRESTResponse( $response, 200,
-        JSON::to_json( \@outdatedPlugins, { pretty => 1 } ) );
-
     return;
+}
+
+sub getOutdated {
+    my $this = shift;
+
+    unless ( defined $this->{_outdated} ) {
+
+        # fetch from session store
+        my $outdated =
+          $this->{_refresh}
+          ? undef
+          : Foswiki::Func::getSessionValue("UPDATESPLUGIN::OUTDATED");
+
+        if ( defined $outdated ) {
+
+            $this->writeDebug("found in session");
+            $this->{_outdated} = [ split( /\s*,\s*/, $outdated ) ];
+            return $this->{_outdated};
+        }
+        else {
+            $this->writeDebug("NOT found in session");
+        }
+
+        $this->getInstalled();
+        $this->getAvailable();
+
+        my @outdated = ();
+
+        foreach my $ext ( @{ $this->{_available} } ) {
+            my $extName   = $ext->{topic};
+            my $installed = $this->{_installed}{$extName};
+
+            my $release = $ext->{release};
+            my $version = $ext->{version} || '';
+
+            my $fakeObj = {
+                installedRelease => $release,
+                installedVersion => $version
+            };
+
+            # SMELL: breaks encapsulation of F::Configure::Dependency
+            my $result =
+              Foswiki::Configure::Dependency::_compare_extension_versions(
+                $fakeObj, ">", $installed );
+            if ($result) {
+                push @outdated, $extName;
+                $this->writeDebug(
+"UPDATE: $extName: installed: $installed, available release=$release, available version=$version"
+                );
+            }
+        }
+
+        # set to session store
+        Foswiki::Func::setSessionValue( "UPDATESPLUGIN::OUTDATED",
+            join( ", ", sort @outdated ) );
+
+        $this->{_outdated} = \@outdated;
+    }
+
+    return $this->{_outdated};
 }
 
 sub getAvailable {
@@ -117,32 +200,32 @@ sub getAvailable {
 
     unless ( defined $this->{_available} ) {
 
-        my $installedPlugins = $this->getInstalled();
-
         my $works = Foswiki::Func::getWorkArea('UpdatesPlugin');
         my $fh;
         my @result = ();
-
         my @unfound;
-        foreach my $ext ( keys %$installedPlugins ) {
+
+        my $installed = $this->getInstalled();
+        foreach my $ext ( keys %$installed ) {
 
             # if the cache is fresh, use it
-            if ( -e "$works/$ext"
+            if (   -e "$works/$ext"
+                && !$this->{_refresh}
                 && ( stat("$works/$ext") )[9] > time() - $this->{timeout} )
             {
 
                 # Use the cache
                 local $/ = undef;
-                if ( open( $fh, '<:encoding(UTF-8)', "$works/$ext" ) ) {
-                    my $data = JSON::from_json(<$fh>);
-                    push( @result, $data );
+                if ( open( $fh, '<:encoding(utf-8)', "$works/$ext" ) ) {
+                    my $data = $this->json->decode(<$fh>);
+                    push @result, $data;
                     close($fh);
                     next;
                 }
             }
 
             # Not found in cache, or cache out of date. Update cache.
-            push( @unfound, $ext );
+            push @unfound, $ext;
         }
 
         if ( scalar(@unfound) ) {
@@ -152,53 +235,52 @@ sub getAvailable {
               . join( ',', @unfound )
               . ";contenttype=text/plain;skin=text";
 
-            print STDERR "calling $reportUrl\n" if $this->{debug};
+            $this->writeDebug("calling $reportUrl");
 
             my $resource = Foswiki::Func::getExternalResource($reportUrl);
 
             if ( !$resource->is_error() && $resource->isa('HTTP::Response') ) {
                 my $content = $resource->decoded_content();
 
-                #print STDERR "content=$content\n" if $this->{debug};
+                #$this->writeDebug("content=$content");
 
                 # "Verify" the format of the resource and reduce to a perl array
                 if ( $content =~
                     /^foswikiUpdates.handleResponse\((\[.*\])\);$/s )
                 {
-                    my $data  = JSON::from_json($1);
+                    my $data  = $this->json->decode($1);
                     my %found = ();
 
                     # Refresh the cache
                     foreach my $ext (@$data) {
 
                         #print STDERR Data::Dumper::Dumper( \$ext );
-                        my $text = JSON::to_json($ext);
+                        my $text = $this->json->encode($ext);
                         $found{ $ext->{topic} } = 1;
                         if (
                             open(
-                                $fh, '>:encoding(UTF-8)',
+                                $fh, '>:encoding(utf-8)',
                                 "$works/$ext->{topic}"
                             )
                           )
                         {
                             print $fh $text;
                         }
-                        push( @result, $ext );
+                        push @result, $ext;
                     }
 
 # remember null-result for unfound extensions not in the rest result provided by f.o
                     foreach my $ext (@unfound) {
                         next if $found{$ext};
-                        print STDERR "no info about $ext in report\n"
-                          if $this->{debug};
+                        $this->writeDebug("no info about $ext in report");
 
                         # generate a report based on the installed release
                         my $data = {
                             topic   => $ext,
                             release => $this->{_installed}{$ext}
                         };
-                        my $text = JSON::to_json($data);
-                        if ( open( $fh, '>:encoding(UTF-8)', "$works/$ext" ) ) {
+                        my $text = $this->json->encode($data);
+                        if ( open( $fh, '>:encoding(utf-8)', "$works/$ext" ) ) {
                             print $fh $text;
                         }
                     }
@@ -227,7 +309,8 @@ sub getInstalled {
 
 # First get contribs and skins by poking into the System web. The versions returned
 # may include %$RELEASE% if this is a pseudo-install
-# print STDERR "performing local SEARCH in $Foswiki::cfg{SystemWebName}\n" if $this->{debug};
+        $this->writeDebug(
+            "performing local SEARCH in $Foswiki::cfg{SystemWebName}");
 
         my $list = Foswiki::Func::expandCommonVariables(
                 '{%SEARCH{"1" nosearch="on" nototal="on" web="'
@@ -239,8 +322,8 @@ sub getInstalled {
         foreach my $thing ( split( /\s*,\s*/, $list ) ) {
             next unless $thing =~ /^([a-zA-Z0-9_]+)$/;
             next
-              if defined $this->{excludePattern}
-              && $thing =~ /$this->{excludePattern}/;
+              if defined $this->{_excludePattern}
+              && $thing =~ /$this->{_excludePattern}/;
 
             my $mn  = $1;
             my $mod = "Foswiki::Contrib::$mn";
@@ -257,7 +340,7 @@ sub getInstalled {
                     $release = $release->stringify();
                 }
 
-    #print STDERR "found extension $mn, release = $release\n" if $this->{debug};
+                #$this->writeDebug("found extension $mn, release = $release");
 
                 $data->{$mn} = $release;
             }
@@ -268,12 +351,13 @@ sub getInstalled {
         my $controller = $this->{session}{plugins};
         foreach my $plugin ( @{ $controller->{plugins} } ) {
             next
-              if defined $this->{excludePattern}
-              && $plugin->{name} =~ /$this->{excludePattern}/;
+              if defined $this->{_excludePattern}
+              && $plugin->{name} =~ /$this->{_excludePattern}/;
 
             my $release = eval "\$$plugin->{module}::VERSION" || '%$VERSION';
 
-#print STDERR "found plugin $plugin->{name}, release = $release\n" if $this->{debug};
+            $this->writeDebug(
+                "found plugin $plugin->{name}, release = $release");
 
             $data->{ $plugin->{name} } = $release;
         }
@@ -282,16 +366,6 @@ sub getInstalled {
     }
 
     return $this->{_installed};
-}
-
-sub printRESTResponse {
-    my ( $response, $status, $content ) = @_;
-    $response->header(
-        -status  => 200,
-        -type    => 'text/plain',
-        -charset => 'utf-8'
-    );
-    $response->print($content);
 }
 
 1;
