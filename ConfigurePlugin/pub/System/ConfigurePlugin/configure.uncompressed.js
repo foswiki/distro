e8432ea54f3f67b5ae64a3245317336ab6235e0d
Timothy Legge <timlegge@gmail.com>
2020-04-17 10:02:13 -0300

    Item14901: ConfigurePlugin update Copyright and version

--- ConfigurePlugin/pub/System/ConfigurePlugin/configure.uncompressed.js
+++ ConfigurePlugin/pub/System/ConfigurePlugin/configure.uncompressed.js
@@ -1,7 +1,7 @@
 /*
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2013-2015 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2013-2020 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
