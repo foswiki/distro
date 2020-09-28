e9d7b57b311ffa44904946557f08a3c39f42907e
MichaelDaum <daum@michaeldaumconsulting.com>
2020-09-28 17:48:22 +0200

    Item14918: preparing release

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/shrinkurls/jquery.shrinkurls.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/shrinkurls/jquery.shrinkurls.uncompressed.js
@@ -1,7 +1,7 @@
 /*
  * jQuery Shrinkurls plugin 2.01
  *
- * Copyright (c) 2007-2019 Foswiki Contributors http://foswiki.org
+ * Copyright (c) 2007-2020 Foswiki Contributors http://foswiki.org
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
