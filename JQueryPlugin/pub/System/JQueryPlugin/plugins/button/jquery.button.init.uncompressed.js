e579a1e06c3f48e988ecb965fb6a24ff41dc4dde
MichaelDaum <daum@michaeldaumconsulting.com>
2021-01-04 18:47:20 +0100

    Item15000: fix button's behavior in disabled state

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/button/jquery.button.init.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/button/jquery.button.init.uncompressed.js
@@ -2,57 +2,47 @@
 jQuery(function($) {
 
   $(".jqButton:not(.jqInitedButton)").livequery(function() {
-    var $this = $(this), form;
+    var $this = $(this), $form;
 
     $this.addClass("jqInitedButton")
       .on("mouseenter", function() {
         $this.addClass("jqButtonHover");
       }).on("mouseleave", function() {
         $this.removeClass("jqButtonHover");
-      });
+      })
+      .on("click", function() {
+        $form = $this.closest("form");
 
-    if ($this.is(".jqButtonDisabled")) {
-      $this.on("click", function() {
-        return false;
-      });
-    } else {
-      // submit button
-      if ($this.is(".jqSubmitButton")) {
-        $this.on("click", function() {
-          $this.closest("form").trigger("submit");
+        if ($this.is(".jqButtonDisabled")) {
           return false;
-        });
-      } 
+        } 
+        // submit button
+        if ($this.is(".jqSubmitButton")) {
+          $form.trigger("submit");
+          return false;
+        } 
 
-      // save button 
-      else if ($this.is(".jqSaveButton")) {
-        $this.on("click", function() {
-          form = $this.closest("form");
+        // save button 
+        if ($this.is(".jqSaveButton")) {
           if(typeof(foswikiStrikeOne) == "function") {
-            foswikiStrikeOne(form[0]); 
+            foswikiStrikeOne($form[0]); 
           }
-          form.trigger("submit");
+          $form.trigger("submit");
           return false;
-        });
-      } 
+        } 
 
-      // reset button
-      else if ($this.is(".jqResetButton")) {
-        $this.on("click", function() {
-          $this.closest("form").resetForm();
+        // reset button
+        if ($this.is(".jqResetButton")) {
+          $form.resetForm();
           return false;
-        });
-      }
+        }
 
-      // clear button
-      else if ($this.is(".jqClearButton")) {
-        $this.on("click", function() {
-          $this.closest("form").clearForm();
+        // clear button
+        if ($this.is(".jqClearButton")) {
+          $form.clearForm();
           return false;
-        });
-      }
-    }
-
+        }
+      });
   });
 });
 
