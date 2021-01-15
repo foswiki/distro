3eae0bd724a86d5b535f48ea40d87596169b1679
MichaelDaum <daum@michaeldaumconsulting.com>
2021-01-15 15:59:22 +0100

    Item15008: bring back support for "dontnotify"
    ... as well as support for RevCommentPlugin

--- NatEditPlugin/lib/Foswiki/Plugins/NatEditPlugin.pm
+++ NatEditPlugin/lib/Foswiki/Plugins/NatEditPlugin.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2007-2020 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2007-2021 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -30,8 +30,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '9.30';
-our $RELEASE           = '28 Sep 2020';
+our $VERSION           = '9.40';
+our $RELEASE           = '15 Jan 2021';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION  = 'A Wikiwyg Editor';
 our $baseWeb;
