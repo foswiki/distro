cc0f53ba60c68ff3b9655628e161f54cb7ef2eef
MichaelDaum <daum@michaeldaumconsulting.com>
2021-03-01 17:42:50 +0100

    Item15012: remove time tokens in attach format
    this hurts more than it helps: wiki apps in ATTACHEDFILELINKFORMAT are unconditionally
    piped through formatTime() causing all sorts of unintentional changes

--- core/lib/Foswiki/Attach.pm
+++ core/lib/Foswiki/Attach.pm
@@ -413,11 +413,12 @@ sub getAttachmentLink {
 
     # I18N: Site specified %ATTACHEDIMAGEFORMAT% or %ATTACHEDFILELINKFORMAT%,
     # ensure that filename is URL encoded - first $name must be URL.
-    $fileLink =~ s/\$name/$attName/;        # deprecated
-    $fileLink =~ s/\$name/$attName/;        # deprecated, see Item1814
+    $fileLink =~ s/\$name/$attName/g;        # deprecated
+    $fileLink =~ s/\$name/$attName/g;        # deprecated, see Item1814
     $fileLink =~ s/\$filename/$attName/g;
     $fileLink =~ s/\$fileurl/$attName/g;
-    $fileLink =~ s/\$fileext/$fileExt/;
+    $fileLink =~ s/\$fileext/$fileExt/g;
+    $fileLink =~ s/\$filetime/$fileTime/g;
 
     # Expand \t and \n early (only in the format, not
     # in the comment) - TWikibug:Item4581
@@ -426,9 +427,6 @@ sub getAttachmentLink {
     $fileLink =~ s/\$comment/$fileComment/g;
     $fileLink =~ s/\$size/$imgSize/g;
     $fileLink =~ s/([^\n])$/$1\n/;
-
-    require Foswiki::Time;
-    $fileLink = Foswiki::Time::formatTime( $fileTime, $fileLink );
     $fileLink = Foswiki::expandStandardEscapes($fileLink);
 
     return $fileLink;
