a9e59ba7674692f912386fd1dea0c6238519ba38
MichaelDaum <daum@michaeldaumconsulting.com>
2021-07-09 13:18:24 +0200

    Item15033: update jquery.validate

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VALIDATE.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VALIDATE.pm
@@ -28,7 +28,7 @@ sub new {
     my $this = bless(
         $class->SUPER::new(
             name         => 'Validate',
-            version      => '1.19.3',
+            version      => '1.19.4',
             author       => 'Joern Zaefferer',
             homepage     => 'http://jqueryvalidation.org/',
             javascript   => ['pkg.js'],
@@ -76,7 +76,7 @@ sub init {
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2010-2020 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2010-2021 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
--- JQueryPlugin/pub/System/JQueryPlugin/plugins/validate/pkg.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/validate/pkg.uncompressed.js
@@ -1,9 +1,9 @@
 /*!
- * jQuery Validation Plugin v1.19.3-pre
+ * jQuery Validation Plugin v1.19.4-pre
  *
  * https://jqueryvalidation.org/
  *
- * Copyright (c) 2020 Jörn Zaefferer
+ * Copyright (c) 2021 Jörn Zaefferer
  * Released under the MIT license
  */
 (function( factory ) {
@@ -1430,7 +1430,7 @@ $.extend( $.validator, {
 			// https://gist.github.com/dperini/729294
 			// see also https://mathiasbynens.be/demo/url-regex
 			// modified to allow protocol-relative URLs
-			return this.optional( element ) || /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( value );
+			return this.optional( element ) || /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( value );
 		},
 
 		// https://jqueryvalidation.org/date-method/
@@ -1655,11 +1655,11 @@ if ( $.ajaxPrefilter ) {
 }
 return $;
 }));/*!
- * jQuery Validation Plugin v1.19.3-pre
+ * jQuery Validation Plugin v1.19.4-pre
  *
  * https://jqueryvalidation.org/
  *
- * Copyright (c) 2020 Jörn Zaefferer
+ * Copyright (c) 2021 Jörn Zaefferer
  * Released under the MIT license
  */
 (function( factory ) {
