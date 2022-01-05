ac0104972d06fc23428f2e45461a58d323be70e7
MichaelDaum <daum@michaeldaumconsulting.com>
2022-01-05 18:16:22 +0100

    Item15060: add validation rule foswikiMandatory

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/validate/jquery.validate.init.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/validate/jquery.validate.init.uncompressed.js
@@ -1,5 +1,10 @@
 "use strict";
 jQuery(function($) {
+
+  $.validator.addClassRules("foswikiMandatory", {
+    required: true
+  });
+
   $(".jqValidate:not(.jqInitedValidate)").livequery(function() {
     var $this = $(this), 
         options = $.extend({}, $this.data(), $this.metadata());
