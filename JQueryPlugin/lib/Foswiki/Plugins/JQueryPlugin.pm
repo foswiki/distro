3204790ffe295f1ca60b9270f062d8d9303890fe
MichaelDaum <daum@michaeldaumconsulting.com>
2023-04-26 09:24:18 +0200

    Item15190: backported fix for jQuery-2.2.4

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin.pm
@@ -25,8 +25,8 @@ BEGIN {
     }
 }
 
-our $VERSION           = '10.50';
-our $RELEASE           = '19 Jan 2023';
+our $VERSION           = '10.51';
+our $RELEASE           = '25 Apr 2023';
 our $SHORTDESCRIPTION  = 'jQuery <nop>JavaScript library for Foswiki';
 our $NO_PREFS_IN_TOPIC = 1;
 our $iconService;
