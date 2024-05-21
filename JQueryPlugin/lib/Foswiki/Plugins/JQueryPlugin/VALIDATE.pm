c60dde85a7650e0537c2d55e9f4534501651d7d5
MichaelDaum <daum@michaeldaumconsulting.com>
2024-05-21 13:46:38 +0200

    Item15330: update form validation to latest upstream version

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VALIDATE.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/VALIDATE.pm
@@ -28,7 +28,7 @@ sub new {
     my $this = bless(
         $class->SUPER::new(
             name         => 'Validate',
-            version      => '1.19.6_1',
+            version      => '1.20.0',
             author       => 'Joern Zaefferer',
             homepage     => 'http://jqueryvalidation.org/',
             javascript   => ['pkg.js'],
@@ -76,7 +76,7 @@ sub init {
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2010-2022 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2010-2024 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
