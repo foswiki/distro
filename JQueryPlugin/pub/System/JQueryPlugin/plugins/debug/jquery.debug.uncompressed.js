f1ca843d412f09f335e7a9ad48dbb5ebdddac2e8
MichaelDaum <daum@michaeldaumconsulting.com>
2024-09-24 11:17:18 +0200

    Item15344: deprecated jquery.debug
    ... while stripping off IE11 support

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/debug/jquery.debug.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/debug/jquery.debug.uncompressed.js
@@ -1,34 +1,12 @@
 /* simple jQuery logger */
 (function($) {
-  var $container, 
-      $list,
-      userAgent = navigator.userAgent.toLowerCase(),
-      isIE = /msie/.test(userAgent) && !/opera/.test(userAgent); 
-
   function log() {
-    var args = arguments, msg;
-
-    if (isIE) {
-      if (typeof($container) === 'undefined') {
-        $container = $('<div id="DEBUG"></div>').appendTo("body");
-        $list = $("<ol></ol>").appendTo($container);
-      }
-      
-      msg = Array.prototype.slice.call(args);
-      if (msg.length == 1 && typeof(msg[0]) === 'string') {
-        msg = msg[0];
-      } else {
-        msg = msg.join(" ");
-      }
-      $list.append( '<li>' + msg + '</li>' ); 
-      $container.scrollTop($container[0].scrollHeight); 
-    } else {
-      console.log.apply(console, args);
-    }
+    var args = arguments;
+    console.log.apply(console, args);
   }
   
   /* export */
-  $.log = log;
+  $.log = foswiki.log = log;
   $.fn.debug = function() {
     return this.each(function(){
       $.log(this);
