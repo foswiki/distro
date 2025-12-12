ab8ae11dc20b1c3aeb685d528fb625d52f2292fa
MichaelDaum <daum@michaeldaumconsulting.com>
2025-12-12 10:02:53 +0100

    Item15583: fixed textboxlist's DeleteValue event

--- JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
+++ JQueryPlugin/lib/Foswiki/Plugins/JQueryPlugin/TEXTBOXLIST.pm
@@ -28,7 +28,7 @@ sub new {
     my $this = bless(
         $class->SUPER::new(
             name         => 'TextboxList',
-            version      => '2.33',
+            version      => '2.40',
             author       => 'Michael Daum',
             homepage     => 'http://foswiki.org/Extensions/JQueryPlugin',
             css          => ['jquery.textboxlist.css'],
