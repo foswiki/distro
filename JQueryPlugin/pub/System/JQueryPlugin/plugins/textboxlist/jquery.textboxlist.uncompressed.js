c471a455a9bc5b1bb8047c4be699e45a3b76b228
MichaelDaum <daum@michaeldaumconsulting.com>
2026-01-12 17:08:09 +0100

    Item15604: ignore duplicates in textboxlist

--- JQueryPlugin/pub/System/JQueryPlugin/plugins/textboxlist/jquery.textboxlist.uncompressed.js
+++ JQueryPlugin/pub/System/JQueryPlugin/plugins/textboxlist/jquery.textboxlist.uncompressed.js
@@ -1,7 +1,7 @@
 /*
- * jQuery textbox list plugin 2.40
+ * jQuery textbox list plugin 2.50
  *
- * Copyright (c) 2009-2025 Foswiki Contributors http://foswiki.org
+ * Copyright (c) 2009-2026 Foswiki Contributors http://foswiki.org
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
