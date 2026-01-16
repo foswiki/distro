82410b2e7ef1a5f340c4919df9ba0643f5d60ec4
MichaelDaum <daum@michaeldaumconsulting.com>
2026-01-16 10:58:49 +0100

    Item15606: use double quotes for attributes
    ... to match CGI's html renderers

--- core/lib/Foswiki/Render.pm
+++ core/lib/Foswiki/Render.pm
@@ -656,7 +656,7 @@ sub start_html {
 
             # This is what CGI encodes, so....
             $v =~ s/([&<>\x8b\x9b'])/'&#'.ord($1).';'/ge;
-            $html .= " $k='$v'";
+            $html .= " $k=\"$v\"";
         }
     }
     return "$html>";
