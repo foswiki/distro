f1d8d87f4eae8ab9963dd575cb0a9c614589ae37
MichaelDaum <daum@michaeldaumconsulting.com>
2026-01-16 10:06:37 +0100

    Item15606: remove dependency on CGI

--- TablePlugin/test/unit/TablePlugin/TablePluginSuite.pm
+++ TablePlugin/test/unit/TablePlugin/TablePluginSuite.pm
@@ -12,7 +12,7 @@ sub include_tests { return 'TablePluginTests' }
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
-Copyright (C) 2008-2010 Foswiki Contributors. Foswiki Contributors
+Copyright (C) 2008-2026 Foswiki Contributors. Foswiki Contributors
 are listed in the AUTHORS file in the root of this distribution.
 NOTE: Please extend that file, not this notice.
 
